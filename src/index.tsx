013231b78541ad027ac3def2d856184e3c3d62b9
hkkang <hkkang@nextree.io>
2019-12-11 15:59:34 +0900

    ie 폴리필 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import 'semantic-ui-less/semantic.less';
 import './style/css/main.76b94a4e.chunk.css';
 import './style/css/2.2e859a88.chunk.css';
 
-import 'react-app-polyfill/ie10';
+import 'react-app-polyfill/ie9';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
