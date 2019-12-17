c030dedc4b2a3e4f99dfbb74cce750c6722b64cd
jsseo <jsseo@nextree.co.kr>
2019-12-17 11:01:02 +0900

    context menu

---
+++ src/shared/layout/ContentMenu/ui/logic/ContentMenuContainer.tsx
@@ -0,0 +1,53 @@
+
+import React, { Component } from 'react';
+import { reactAutobind } from '@nara.platform/accent';
+import { Menu as SemanticMenu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+interface Menu {
+  name: string,
+  count?: number
+  path: string,
+}
+
+interface Props {
+  menus: Menu[]
+  activeIndex: number
+}
+
+
+@reactAutobind
+class ContentMenuContainer extends Component<Props> {
+  //
+  static Menu: Menu;
+
+  render() {
+    //
+    const { menus, activeIndex } = this.props;
+
+    if (!menus || !menus.length) return null;
+
+    return (
+      <div className="cont-inner">
+        <SemanticMenu className="sku">
+          {
+            menus.map((menu: Menu, index: number) => (
+              <SemanticMenu.Item
+                name={menu.name}
+                active={index === activeIndex}
+                as={Link}
+                to={menu.path}
+                key={`menu_${menu.name}`}
+              >
+                {menu.name}
+                { menu.count && <span className="count">+{menu.count}</span> || null }
+              </SemanticMenu.Item>
+            )) || null
+          }
+        </SemanticMenu>
+      </div>
+    );
+  }
+}
+
+export default ContentMenuContainer;
