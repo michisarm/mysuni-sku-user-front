595230f4a8d6fe40c7b47516722c4bf69c503f32
kimminjun <wqpkfalcy@gmail.com>
2019-12-26 22:01:47 +0900

    polyfill 교체

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 
+// import '@babel/polyfill';
+import 'babel-polyfill';
 import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/stable';
+// import 'react-app-polyfill/stable';
 
 import 'semantic-ui-less/semantic.less';
 import './style/css/main.84b0330d.chunk.css';
