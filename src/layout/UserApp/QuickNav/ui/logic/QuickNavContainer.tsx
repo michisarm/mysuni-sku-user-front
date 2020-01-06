7a96119a1641890059218612dc252c59773f9bdc
parksanghyun <shpark@nextree.co.kr>
2020-01-06 18:03:43 +0900

    sockjs basurl 변경

--- src/layout/UserApp/QuickNav/ui/logic/QuickNavContainer.tsx
+++ src/layout/UserApp/QuickNav/ui/logic/QuickNavContainer.tsx
@@ -38,7 +38,7 @@ class QuickNavContainer extends Component<Props, State> {
     feedType: '',
   };
 
-  baseUrl = 'https://mysuni.sk.com/api/pigeon';
+  baseUrl = '/api/pigeon';
   // baseUrl = 'http://pigeon/api/pigeon';
 
   // transport = ['xdr-streaming',
