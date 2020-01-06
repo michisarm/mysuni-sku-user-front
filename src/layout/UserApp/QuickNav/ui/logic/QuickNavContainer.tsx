d5c059d98eb180ef555b503f992e86e91ae8d18d
parksanghyun <shpark@nextree.co.kr>
2020-01-06 14:29:51 +0900

    오른쪽 하단 메뉴 Feed 추가(Learning, Community)

--- src/layout/UserApp/QuickNav/ui/logic/QuickNavContainer.tsx
+++ src/layout/UserApp/QuickNav/ui/logic/QuickNavContainer.tsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { reactAutobind, WorkSpace, getCookie } from '@nara.platform/accent';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
+import SockJs from 'sockjs-client';
 
 //import { tenantInfo } from '@nara.platform/dock';
 import SiteMapModalContainer from '../../../QuickNav/ui/logic/SiteMapModalContainer';
@@ -9,13 +10,14 @@ import QuickNavWrapperView from '../view/QuickNavWrapperView';
 import {
   MenuWrapperView, TopMenuItemView, BottomMenuItemView,
 } from '../view/QuickNavElementsView';
-
+import FeedEventRdo from '../model/FeedEventRdo';
 
 interface Props extends RouteComponentProps {
 }
 
 interface State {
   active: boolean,
+  feedType: string
 }
 
 
@@ -26,10 +28,49 @@ class QuickNavContainer extends Component<Props, State> {
 
   state = {
     active: false,
+    feedType: '',
   };
 
+  baseUrl = 'http://pigeon:8092/api/pigeon';
+  // baseUrl = 'http://pigeon/api/pigeon';
+
+  // transport = ['xdr-streaming',
+  //   'xhr-streaming',
+  //   'eventsource',
+  //   'iframe-eventsource',
+  //   'htmlfile',
+  //   'iframe-htmlfile',
+  //   'xdr-polling',
+  //   'xhr-polling',
+  //   'iframe-xhr-polling',
+  //   'jsonp-polling'];
 
   componentDidMount() {
+    //
+    // const sockjs = new SockJs('http://127.0.0.1:8092/api/pigeon/pigeon', null, { transports: this.transport });
+    const sockjs = new SockJs(this.baseUrl + '/pigeon');
+    //const sockjs = new SockJs('http://pigeon:8080/api/pigeon/pigeon');
+
+    sockjs.onopen = () => {
+      sockjs.send('Attempt to connect socket..');
+    };
+
+    sockjs.onmessage = (event: any) => {
+      //
+      const feedEvent:FeedEventRdo = JSON.parse(event.data);
+
+      const cookieCitizenKey:string = this.genCitizenKey(getCookie('audienceId'));
+
+      if (cookieCitizenKey === feedEvent.citizenId) {
+        this.setState( { feedType: feedEvent.feedType } );
+      }
+
+      console.log('COOKIE: ' + cookieCitizenKey);
+      console.log('CITIZENID: ' + feedEvent.citizenId);
+      console.log('FEEDTYPE: ' + feedEvent.feedType);
+      //this.setState({feed: true});
+    };
+
     window.addEventListener('click', this.deactive);
   }
 
@@ -41,6 +82,15 @@ class QuickNavContainer extends Component<Props, State> {
     // if (prevState.active !== this.state.active && this.state.active) {
     //   window.addEventListener('click', this.deactive);
     // }
+  }
+
+  genCitizenKey(tenantId:string) {
+    //
+    const splitWholeId:string[] = tenantId.split('@');
+    const pavilions:string[] = splitWholeId[0].split('-');
+    const cinerooms:string[] = splitWholeId[1].split('-');
+
+    return pavilions[0] + '@' + cinerooms[0] + '-' + cinerooms[1];
   }
 
   deactive() {
@@ -66,16 +116,19 @@ class QuickNavContainer extends Component<Props, State> {
 
   onClickLearning() {
     //
+    this.setState( { feedType: '' } );
     this.routeNav('/my-training');
   }
 
   onClickCommunity() {
     //
+    this.setState( { feedType: '' } );
     this.routeNav('/community');
   }
 
   onClickSupport() {
     //
+    this.setState( { feedType: '' } );
     this.routeNav('/board/support/Notice');
   }
 
@@ -124,9 +177,9 @@ class QuickNavContainer extends Component<Props, State> {
         <MenuWrapperView
           topButtons={
             <>
-              <TopMenuItemView iconName="learning32" text="Learning" onClick={this.onClickLearning} />
-              <TopMenuItemView iconName="community32" text="Community" onClick={this.onClickCommunity} />
-              <TopMenuItemView iconName="support32" text="Support" onClick={this.onClickSupport} />
+              <TopMenuItemView iconName="learning32" feedType={this.state.feedType} text="Learning" onClick={this.onClickLearning} />
+              <TopMenuItemView iconName="community32" feedType={this.state.feedType} text="Community" onClick={this.onClickCommunity} />
+              <TopMenuItemView iconName="support32" feedType={this.state.feedType} text="Support" onClick={this.onClickSupport} />
             </>
           }
           bottomButtons={
