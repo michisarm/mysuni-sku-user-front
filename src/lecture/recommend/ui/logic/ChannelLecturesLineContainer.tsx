9f50f8297ad6a599118adef112c03372ae8b2a5f
jmpark <jmpark@nextree.co.kr>
2020-01-15 07:45:33 +0900

    modify temporary

--- src/lecture/recommend/ui/logic/ChannelLecturesLineContainer.tsx
+++ src/lecture/recommend/ui/logic/ChannelLecturesLineContainer.tsx
@@ -111,11 +111,12 @@ class ChannelLecturesLineContainer extends Component<Props> {
                 results.map((lecture: LectureModel, index: number) => {
                   const inMyLecture = inMyLectureMap.get(lecture.serviceId) || undefined;
 
+                  // rating={lecture.reviewSummary.average}
                   return (
                     <Lecture
                       key={`lecture-${index}`}
                       model={lecture}
-                      rating={lecture.reviewSummary.average}
+                      rating={3}
                       // thumbnailImage="http://placehold.it/60x60"
                       action={inMyLecture ? Lecture.ActionType.Remove : Lecture.ActionType.Add}
                       onAction={() => this.onActionLecture(inMyLecture || lecture)}
