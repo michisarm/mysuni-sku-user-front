b7a30eca6abbac3d63c5e5c60c3d9571fd674e85
hkkang <hkkang@nextree.io>
2020-01-14 10:51:34 +0900

    reactAlert 추가 개선

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-console.log('hello');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-console.log('test2');
