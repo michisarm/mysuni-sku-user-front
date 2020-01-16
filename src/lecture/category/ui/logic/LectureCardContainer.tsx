fb797492c72ce091fb36de782dd738ab6d8691a1
hkkang <hkkang@nextree.io>
2020-01-17 02:45:52 +0900

    프로필 수정 기능 제거

--- src/lecture/category/ui/logic/LectureCardContainer.tsx
+++ src/lecture/category/ui/logic/LectureCardContainer.tsx
@@ -1,7 +1,7 @@
+
 import React, { Component } from 'react';
 import { mobxHelper, reactAlert, reactAutobind } from '@nara.platform/accent';
 import { inject, observer } from 'mobx-react';
-import moment from 'moment';
 
 import depot from '@nara.drama/depot';
 import { CubeType, LearningState, ProposalState } from 'shared';
