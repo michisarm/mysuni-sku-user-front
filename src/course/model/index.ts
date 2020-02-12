4c8456b3d5c31a1e59e4edd1ccece05f4613f8c3
hkkang <hkkang@nextree.io>
2020-02-12 11:21:29 +0900

    shared 리팩토링

--- src/course/model/index.ts
+++ src/course/model/index.ts
@@ -2,5 +2,6 @@
 export { CoursePlanModel } from './CoursePlanModel';
 export { CoursePlanContentsModel } from './CoursePlanContentsModel';
 export { CoursePlanRdoModel } from './CoursePlanRdoModel';
+export { CoursePlanFlowCdoModel } from './CoursePlanFlowCdoModel';
 export { CourseSetType } from './CourseSetType';
 export { CourseSetModel } from './CourseSetModel';
