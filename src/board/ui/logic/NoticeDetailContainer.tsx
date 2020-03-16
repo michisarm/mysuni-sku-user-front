ce63cd4db48606c7a6e46d6fc56b0a00fba1d841
백동주 <djpaek@sk.com>
2020-03-16 18:22:01 +0900

    attachment + newline fix

--- src/board/ui/logic/NoticeDetailContainer.tsx
+++ src/board/ui/logic/NoticeDetailContainer.tsx
@@ -114,7 +114,7 @@ class NoticeDetailContainer extends React.Component<Props, State> {
                     <p>
                       <a href="#" className="link" key={index}>
                         <span className="ellipsis" onClick={() => depot.downloadDepotFile(foundedFile.id)}>
-                          \n {foundedFile.name}
+                          {foundedFile.name}
                         </span>
                       </a>
                       <br/>
