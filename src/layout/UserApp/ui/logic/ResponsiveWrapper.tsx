6ab15d06afb1a74852e866e23e9fd865cace59bc
MSJ <ouk@sm.com>
2020-05-22 19:16:19 +0900

    1. ipad 화면 전환 수정

--- src/layout/UserApp/ui/logic/ResponsiveWrapper.tsx
+++ src/layout/UserApp/ui/logic/ResponsiveWrapper.tsx
@@ -12,7 +12,7 @@ interface ResponsiveWrapperProps {
 class ResponsiveWrapper extends Component<ResponsiveWrapperProps> {
 
   isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    return /Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
   render() {
