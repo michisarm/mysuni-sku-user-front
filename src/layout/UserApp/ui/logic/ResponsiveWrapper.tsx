22176713545c6ab46bd996b04d04de541f86641d
PSJ <ouk@sk.com>
2020-05-22 10:23:43 +0900

    브라우저 크기 변경 시 모달팝업 종료 개선

--- src/layout/UserApp/ui/logic/ResponsiveWrapper.tsx
+++ src/layout/UserApp/ui/logic/ResponsiveWrapper.tsx
@@ -10,16 +10,30 @@ interface ResponsiveWrapperProps {
 
 @reactAutobind
 class ResponsiveWrapper extends Component<ResponsiveWrapperProps> {
+
+  state = { isMobile : false };
   //
   render() {
     //
     const { children } = this.props;
+    const filter = 'win16|win32|win64|mac|macintel';
+    if ( filter.indexOf(navigator.platform.toLowerCase()) < 0 ) {
+      this.setState({ isMobile : true });
+    }
 
     return (
       <>
-        <ResponsiveEnvWrapper responsive={Responsive.onlyComputer}>{children}</ResponsiveEnvWrapper>
-        <ResponsiveEnvWrapper responsive={Responsive.onlyTablet}>{children}</ResponsiveEnvWrapper>
-        <ResponsiveEnvWrapper responsive={Responsive.onlyMobile}>{children}</ResponsiveEnvWrapper>
+        { this.state.isMobile ?
+          <>
+            <ResponsiveEnvWrapper responsive={Responsive.onlyComputer}>{children}</ResponsiveEnvWrapper>
+            <ResponsiveEnvWrapper responsive={Responsive.onlyTablet}>{children}</ResponsiveEnvWrapper>
+            <ResponsiveEnvWrapper responsive={Responsive.onlyMobile}>{children}</ResponsiveEnvWrapper>
+          </>
+          :
+          <>
+            <ResponsiveEnvWrapper responsive={Responsive.contextType}>{children}</ResponsiveEnvWrapper>
+          </>
+        }
       </>
     );
   }
