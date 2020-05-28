97f9dd8e34fe951edb29ce1d56a900e0fd7645b0
skunivunion <64876113+skunivunion@users.noreply.github.com>
2020-05-29 05:40:32 +0900

    삭제 버튼 삭제 및 상세 페이지 화면 에서 리스트 클릭 수정

--- src/lecture/category/ui/logic/LectureCardContainer.tsx
+++ src/lecture/category/ui/logic/LectureCardContainer.tsx
@@ -875,14 +875,21 @@ class LectureCardContainer extends Component<Props, State> {
         />
 
         {
-          ((cubeType === CubeType.ClassRoomLecture || cubeType === CubeType.ELearning) && (enrollingAvailableChk === true) && (freeOfChargeChk === false) && (approvalProcessChk === true) &&
-            <ApplyReferenceModalApproval ref={applyReferenceModelApproval => this.applyReferenceModelApproval = applyReferenceModelApproval} handleOk={this.onClickApplyReferentOkApproval} />
+          (cubeType === CubeType.ClassRoomLecture || cubeType === CubeType.ELearning) && (enrollingAvailableChk === true) && (freeOfChargeChk === false) && (approvalProcessChk === true) && (
+            <ApplyReferenceModalApproval
+              ref={applyReferenceModelApproval => this.applyReferenceModelApproval = applyReferenceModelApproval}
+              handleOk={this.onClickApplyReferentOkApproval}
+            />
           )
         }
 
         {
-          ((cubeType === CubeType.ClassRoomLecture || cubeType === CubeType.ELearning) &&
-            <ApplyReferenceModal ref={applyReferenceModel => this.applyReferenceModel = applyReferenceModel} classrooms={typeViewObject.classrooms} handleOk={this.onClickApplyReferentOk} />
+          (cubeType === CubeType.ClassRoomLecture || cubeType === CubeType.ELearning) && (
+            <ApplyReferenceModal
+              ref={applyReferenceModel => this.applyReferenceModel = applyReferenceModel}
+              classrooms={typeViewObject.classrooms}
+              handleOk={this.onClickApplyReferentOk}
+            />
           )
         }
 
