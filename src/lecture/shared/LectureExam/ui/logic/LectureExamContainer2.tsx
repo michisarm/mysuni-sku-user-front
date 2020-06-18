09cd08d9dbe42dc9e1e5e14d57372b94f45d4757
avedpark <avedpark@gmail.com>
2020-06-18 15:07:29 +0900

    20200618 코스고도화 222

---
+++ src/lecture/shared/LectureExam/ui/logic/LectureExamContainer2.tsx
@@ -0,0 +1,63 @@
+
+import React, { Component } from 'react';
+import { reactAutobind } from '@nara.platform/accent';
+import { Report, Test, Survey } from '../view/LectureExamElementsView2';
+
+interface Props {
+  /** 과제 액션*/
+  onReport?: () => void
+  /** 과 비활성화 시 */
+  onReportNotReady?: () => void
+  /** 테스트제 액션 */
+  onTest?: () => void
+  /** 테스트 비활성화 시 */
+  onTestNotReady?: () => void
+  /** 설문 액션*/
+  onSurvey?: () => void
+  /** 설문 비활성화 시 **/
+  OnSurveyNotReady?: () => void
+
+  viewObject?: any
+  passedState: boolean
+  type?: string
+  name?: string
+}
+
+@reactAutobind
+class LectureExamContainer extends Component<Props> {
+  //
+  render() {
+    //
+    const { onReport, onReportNotReady, onTest, onTestNotReady, onSurvey, OnSurveyNotReady, viewObject, passedState, type, name } = this.props;
+
+    return (
+      <div className="contents trs-box-wrap non-height">
+        <Report
+          OnReport={onReport}
+          onReportNotReady={onReportNotReady}
+          viewObject={viewObject}
+          passedState={passedState}
+          type={type}
+          name={name}
+        />
+        <Test
+          OnTest={onTest}
+          OnTestNotReady={onTestNotReady}
+          viewObject={viewObject}
+          type={type}
+          name={name}
+        />
+        <Survey
+          onSurvey={onSurvey}
+          OnSurveyNotReady={OnSurveyNotReady}
+          viewObject={viewObject}
+          type={type}
+          name={name}
+        />
+      </div>
+    );
+  }
+}
+
+export default LectureExamContainer;
+
---
+++ src/lecture/shared/LectureExam/ui/view/LectureExamElementsView2.tsx
@@ -0,0 +1,276 @@
+
+import React from 'react';
+
+interface ReportProps {
+  OnReport?: () => void
+  onReportNotReady?: () => void
+  viewObject?: any
+  passedState?: boolean
+  type?: string
+  name?: string
+}
+
+export const Report = ({ OnReport, onReportNotReady, viewObject, passedState, type, name }: ReportProps) => {
+  //
+  if (!OnReport) return null;
+  // console.log('Report viewObject : ', viewObject);
+  // if (passedState !== undefined) {
+  //   alert(passedState);
+  // }
+
+  return (
+    <div className="trs-box">
+      <div className="thumbnail">
+        <img src={`${process.env.PUBLIC_URL}/images/all/report.svg`} alt="Report" />
+      </div>
+      <div className="desc">
+        <div className="t-copy">Report</div>
+        {/*<div className="s-copy">Front-End Machine Learning UI/UX Guide Director</div>*/}
+      </div>
+      <div className="btn-area">
+
+        {
+          passedState && (
+            <span className="state">제출완료</span>
+            // <button className="ui button trs" onClick={OnReport}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '0' && (
+            <button className="ui button trs" onClick={OnReport}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '1' && (
+            <button className="ui button trs" style={{ opacity: 0.3 }} onClick={onReportNotReady}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '2' && (
+            <button className="ui button trs" onClick={OnReport}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '3' && (
+            <button className="ui button trs" style={{ opacity: 0.3 }} onClick={onReportNotReady}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '4' && (
+            <button className="ui button trs" onClick={OnReport}><span>과제제출</span></button>
+          )
+        }
+
+        {
+          !passedState && type === '5' && (
+            <button className="ui button trs" onClick={OnReport}><span>과제제출</span></button>
+          )
+        }
+
+      </div>
+    </div>
+  );
+};
+
+interface TestProps {
+  OnTest?: () => void
+  OnTestNotReady?: () => void
+  viewObject?: any
+  type?: string
+  name?: string
+}
+
+export const Test = ({ OnTest, OnTestNotReady, viewObject, type, name }: TestProps) => {
+  //
+  if (!OnTest) return null;
+  // console.log('Test viewObject : ', viewObject);
+  // console.log('type : {0}, name : {1}', type, name);
+  return (
+    <>
+      <li className="step2">
+        <div className="tit trs">
+          <i className="icon icon-test24" />
+          <span>Test</span>
+        </div>
+        <div className="right">
+          <a href="#" className="btn-play black">
+            <span className="text">Test</span>
+            <i className="icon play-black24" />
+          </a>
+        </div>
+      </li>
+    </>
+    // <div className="trs-box">
+    //   <div className="thumbnail">
+    //     <img src={`${process.env.PUBLIC_URL}/images/all/test.svg`} alt="Test" />
+    //   </div>
+    //   <div className="desc">
+    //     <div className="t-copy">Test</div>
+    //     <div className="s-copy">{viewObject.examTitle}</div>
+    //   </div>
+    //   <div className="btn-area">
+    //     {
+    //       type === '0' && (
+    //         <button className="ui button trs" onClick={OnTest}><span>{name}</span></button>
+    //       )
+    //     }
+    //
+    //     {
+    //       type === '1' && (
+    //         <button className="ui button trs" style={{ opacity: 0.3 }} onClick={OnTestNotReady}><span>{name}</span></button>
+    //       )
+    //     }
+    //
+    //     {
+    //       type === '2' && (
+    //         <button className="ui button trs" onClick={OnTest}><span>{name}</span></button>
+    //       )
+    //     }
+    //
+    //     {
+    //       type === '3' && (
+    //         <button className="ui button trs" style={{ opacity: 0.3 }} onClick={OnTestNotReady}><span>{name}</span></button>
+    //       )
+    //     }
+    //
+    //     {
+    //       type === '4' && (
+    //         <span className="state">{name}</span>
+    //       )
+    //     }
+    //
+    //     {
+    //       type === '5' && (
+    //         <span className="state">{name}</span>
+    //       )
+    //     }
+    //
+    //   </div>
+    // </div>
+  );
+};
+
+interface SurveyProps {
+  onSurvey?: () => void
+  OnSurveyNotReady?: () => void
+  viewObject?: any
+  type?: string
+  name?: string
+}
+
+export const Survey = ({ onSurvey, OnSurveyNotReady, viewObject, type, name }: SurveyProps) => {
+  //
+  if (!onSurvey) return null;
+  // console.log('Survey viewObject : ', viewObject);
+  return (
+    <li className="step2">
+      <div className="tit trs">
+        <i className="icon icon-survey24" />
+        <span>Survey</span>
+      </div>
+      <div className="right">
+        <a href="#" className="btn-play black">
+          <span className="text">설문하기</span>
+          <i className="icon play-black24" />
+        </a>
+      </div>
+    </li>
+    // <div className="trs-box">
+    //   <div className="thumbnail">
+    //     <img src={`${process.env.PUBLIC_URL}/images/all/survey.svg`} alt="Survey" />
+    //   </div>
+    //   <div className="desc">
+    //     <div className="t-copy">Survey</div>
+    //     <div className="s-copy">{viewObject.surveyTitle}</div>
+    //   </div>
+    //   <div className="btn-area">
+    //     {
+    //       viewObject.surveyState && (
+    //         <span className="state">참여완료</span>
+    //       )
+    //     }
+    //
+    //     {
+    //       !viewObject.surveyState && (viewObject.state !== undefined || viewObject.state === 'Completed' || viewObject.state === 'InProgress' ||
+    //         viewObject.state === 'Waiting' || viewObject.state === 'Missed') && (
+    //         <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>
+    //       )
+    //     }
+    //
+    //     {
+    //       !viewObject.surveyState && viewObject.state === undefined && viewObject.state !== 'Completed' && viewObject.state !== 'InProgress' && viewObject.state !== 'Waiting' && viewObject.state !== 'Missed' && (
+    //         <button className="ui button trs" style={{ opacity: 0.3 }} onClick={OnSurveyNotReady}><span>설문하기</span></button>
+    //       )
+    //     }
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type === '0' && (*/}
+    //     {/*    <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type === '2' && (*/}
+    //     {/*    <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type === '1' && (*/}
+    //     {/*    <button className="ui button trs" style={{ opacity: 0.3 }} onClick={OnSurveyNotReady}><span>설문하기</span></button>*/}
+    //     {/*    // <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type === '3' && (*/}
+    //     {/*    <button className="ui button trs" style={{ opacity: 0.3 }} onClick={OnSurveyNotReady}><span>설문하기</span></button>*/}
+    //     {/*    // <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  type === '4' && (*/}
+    //     {/*    <span className="state">{name}</span>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type === '5' && (*/}
+    //     {/*    <span className="state">참여완료</span>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  !viewObject.surveyState && type !== '1' && type !== '3' && (*/}
+    //     {/*    <button className="ui button trs" onClick={onSurvey}><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  viewObject.surveyState && (*/}
+    //     {/*    <span className="state">참여완료</span>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  type === '1' && (*/}
+    //     {/*    <button className="ui button trs" disabled><span>설문하기</span></button>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //     {/*{*/}
+    //     {/*  viewObject.surveyState && (*/}
+    //     {/*    <span className="state">참여완료</span>*/}
+    //     {/*  )*/}
+    //     {/*}*/}
+    //
+    //   </div>
+    // </div>
+  );
+};
