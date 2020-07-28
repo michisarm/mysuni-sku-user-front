ae3c1e28e6bc0d11c34f4393f343d92bae707c0e
sspark <ss.park@sk.com>
2020-07-28 09:54:43 +0900

    중복시청 메세지 수정

--- src/lecture/category/ui/logic/CourseContainer.tsx
+++ src/lecture/category/ui/logic/CourseContainer.tsx
@@ -283,7 +283,7 @@ class CourseContainer extends Component<Props, State> {
     if (this.handleMultiVideo(lectureView)) {
       reactAlert({
         title: '알림',
-        message: '먼저 시작한 학습을 완료 후 시청할 수 있습니다.',
+        message: '동일 시간대에 1개의 학습만 진행해 주십시오.',
         // onClose: () => this.playVideo(videoUrl, studentCdo, lectureView),
       });
     } else {
