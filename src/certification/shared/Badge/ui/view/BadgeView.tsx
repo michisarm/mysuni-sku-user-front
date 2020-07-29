d57bde91ed9ef754eec1c0ad6b70c1a83511ab0a
kwpark <nekorbb@gmail.com>
2020-07-29 17:30:07 +0900

    ma 사이트에서 이미지 노출 안되는 문제 수정

--- src/certification/shared/Badge/ui/view/BadgeView.tsx
+++ src/certification/shared/Badge/ui/view/BadgeView.tsx
@@ -1,59 +1,73 @@
-
-import React, {FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import classNames from 'classnames';
+import { getPublicUrl } from 'shared/helper/envHelper';
 
-const blankImage = '/images/all/icon-chanel-64-px.svg';
-
+const blankImage = `${getPublicUrl()}/images/all/icon-chanel-64-px.svg`;
 
 interface BadgeContentWrapperProps {
-  onViewDetail?: () => void,
-  badgeLevel: string,
-  badgeStyle: string,
-  badgeSize: string,
+  onViewDetail?: () => void;
+  badgeLevel: string;
+  badgeStyle: string;
+  badgeSize: string;
 }
 
-export const BadgeContentWrapper: FunctionComponent<BadgeContentWrapperProps> = ({ badgeLevel, badgeStyle, badgeSize, children, onViewDetail }) => (
+export const BadgeContentWrapper: FunctionComponent<BadgeContentWrapperProps> = ({
+  badgeLevel,
+  badgeStyle,
+  badgeSize,
+  children,
+  onViewDetail,
+}) => (
   <>
-    { badgeStyle === 'List' ? (
-      <a className={ classNames('badge', badgeLevel, badgeSize)} onClick={onViewDetail}>
+    {badgeStyle === 'List' ? (
+      <a
+        className={classNames('badge', badgeLevel, badgeSize)}
+        onClick={onViewDetail}
+      >
         {children}
       </a>
     ) : (
-      <div className={ classNames('badge', badgeLevel, badgeSize)}>
+      <div className={classNames('badge', badgeLevel, badgeSize)}>
         {children}
       </div>
     )}
   </>
 );
 
-
 interface CertificationOrgProps {
-  certiAdminCategoryIcon: string,
-  certiAdminCategoryName: string,
+  certiAdminCategoryIcon: string;
+  certiAdminCategoryName: string;
 }
 
-export const CertificationOrg: FunctionComponent<CertificationOrgProps> = ({ certiAdminCategoryIcon, certiAdminCategoryName }) => (
+export const CertificationOrg: FunctionComponent<CertificationOrgProps> = ({
+  certiAdminCategoryIcon,
+  certiAdminCategoryName,
+}) => (
   <span className="issuing">
-    <img src={certiAdminCategoryIcon} alt={`발급기관: ${certiAdminCategoryName}`}/>
+    <img
+      src={`${getPublicUrl()}${certiAdminCategoryIcon}`}
+      alt={`발급기관: ${certiAdminCategoryName}`}
+    />
   </span>
 );
 
-
 interface CollegeProps {
-  iconUrl: string,
-  mainCategory: string,
+  iconUrl: string;
+  mainCategory: string;
 }
 
-export const College: FunctionComponent<CollegeProps> = ({ iconUrl, mainCategory }) => (
+export const College: FunctionComponent<CollegeProps> = ({
+  iconUrl,
+  mainCategory,
+}) => (
   <span className="college">
-    <img src={ iconUrl || blankImage} alt="" />
+    <img src={iconUrl || blankImage} alt="" />
     <span>{mainCategory}</span>
   </span>
 );
 
-
 interface TitleProps {
-  name: string
+  name: string;
 }
 
 export const Title: FunctionComponent<TitleProps> = ({ name }) => (
