b5c26ce7eb0cf49d38dc2d8ffa11bd0833511975
avedpark <avedpark@gmail.com>
2020-07-31 21:23:06 +0900

    20200731 코스모델추가

---
+++ src/course/model/CoursePlanCustomModel.ts
@@ -0,0 +1,26 @@
+import {CommentCountRdoModel, ReviewSummaryModel} from '@nara.drama/feedback';
+import {CoursePlanModel} from './CoursePlanModel';
+import {CoursePlanContentsModel} from './CoursePlanContentsModel';
+import SurveyCaseModel from '../../survey/event/model/SurveyCaseModel';
+import CourseLectureModel from '../../lecture/model/CourseLectureModel';
+import AnswerSheetModel from '../../survey/answer/model/AnswerSheetModel';
+import LectureViewModel from '../../lecture/model/LectureViewModel';
+import SubLectureViewModel from '../../lecture/model/SubLectureViewModel';
+import ProgramLectureModel from '../../lecture/model/ProgramLectureModel';
+import {SurveyFormModel} from '../../survey/form/model/SurveyFormModel';
+
+export interface CoursePlanCustomModel {
+  coursePlan: CoursePlanModel
+  coursePlanContents: CoursePlanContentsModel
+  answerSheet: AnswerSheetModel
+  // surveyCase: SurveyCaseModel
+  surveyForm: SurveyFormModel
+  courseLecture: CourseLectureModel
+  programLecture: ProgramLectureModel
+  lectureViews: LectureViewModel[]
+  reviewSummary: ReviewSummaryModel
+  commentCountRdo: CommentCountRdoModel
+  subLectureViews:  SubLectureViewModel[]
+  // reviewSummary:
+  // commentCountRdo:
+}
