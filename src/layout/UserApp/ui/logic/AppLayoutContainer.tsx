f5ce5b27159566a78f0a7a971c19afaeb98eb4cd
dudrus <dudrus@naver.com>
2020-08-31 17:34:26 +0900

    add 재동의

--- src/layout/UserApp/ui/logic/AppLayoutContainer.tsx
+++ src/layout/UserApp/ui/logic/AppLayoutContainer.tsx
@@ -28,7 +28,7 @@ class AppLayoutContainer extends Component<Props> {
     // 재동의
     function compareDate(signDate: any) {
       let rtn = false;
-      const reAgreeDate = new Date('2020-08-30').getTime();
+      const reAgreeDate = new Date('2020-08-31').getTime();
       const agreeDate = new Date(signDate).getTime();
       if (reAgreeDate > agreeDate) {
         rtn = true;
