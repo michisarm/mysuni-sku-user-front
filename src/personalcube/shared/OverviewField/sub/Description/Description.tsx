fcf0a18a6d7f0a3ca36c02fc7ce5b475ab66bc20
wagno <wagno@ucom.kr>
2020-09-08 10:31:34 +0900

    Overview 접김 기능 추가

--- src/personalcube/shared/OverviewField/sub/Description/Description.tsx
+++ src/personalcube/shared/OverviewField/sub/Description/Description.tsx
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { reactAutobind } from '@nara.platform/accent';
 import { Button, Icon } from 'semantic-ui-react';
 import classNames from 'classnames';
+import './Description.css'
 
 interface Props {
   description: string;
@@ -48,44 +49,30 @@ class Description extends Component<Props, DescriptionState, any> {
     const { descriptionOpen, showMoreButton } = this.state || {};
 
     return (
-      <div className="class-guide-txt fn-parents" dangerouslySetInnerHTML={{ __html: description }} >
+      <div className="class-guide-txt fn-parents">
+        <div
+          className={`${descriptionOpen ? '' : 'text'} description`}
+          dangerouslySetInnerHTML={{ __html: description }}
+          ref={this.textContainerRef}
+        />
+        {showMoreButton === true && (
+          <Button
+            icon
+            className={classNames('right btn-blue fn-more-toggle')}
+            onClick={this.toggleMore}
+          >
+            {descriptionOpen === true ? 'hide' : 'more'}{' '}
+            <Icon
+              className={classNames({
+                more2: descriptionOpen !== true,
+                hide2: descriptionOpen === true,
+              })}
+            />
+          </Button>
+        )}
       </div>
     );
   }
 }
 
 export default Description;
-
-
-// 일정 크기 이상의 영역을 숨기고, 버튼을 표시하는 코드
-// CSS 문제로 해당 변경사항을 Rollback 하고 기록함
-// render() {
-//   //
-//   const { description } = this.props;
-//   const { descriptionOpen, showMoreButton } = this.state || {};
-
-//   return (
-//     <div className="class-guide-txt fn-parents">
-//       <div
-//         className={`${descriptionOpen ? '' : 'text'} description`}
-//         dangerouslySetInnerHTML={{ __html: description }}
-//         ref={this.textContainerRef}
-//       />
-//       {showMoreButton === true && (
-//         <Button
-//           icon
-//           className={classNames('right btn-blue fn-more-toggle')}
-//           onClick={this.toggleMore}
-//         >
-//           {descriptionOpen === true ? 'hide' : 'more'}{' '}
-//           <Icon
-//             className={classNames({
-//               more2: descriptionOpen !== true,
-//               hide2: descriptionOpen === true,
-//             })}
-//           />
-//         </Button>
-//       )}
-//     </div>
-//   );
-// }
