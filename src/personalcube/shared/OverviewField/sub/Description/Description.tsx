22c095da7d1e8b98ccb6c39a8f1c4f008b7bc30a
wagno <wagno@ucom.kr>
2020-09-08 10:23:23 +0900

    CSS 이슈로 Overview 영역 접힘 기능을 제거하고, 이후 사용을 위해 코드 하단에 주석 처리함.

--- src/personalcube/shared/OverviewField/sub/Description/Description.tsx
+++ src/personalcube/shared/OverviewField/sub/Description/Description.tsx
@@ -48,30 +48,44 @@ class Description extends Component<Props, DescriptionState, any> {
     const { descriptionOpen, showMoreButton } = this.state || {};
 
     return (
-      <div className="class-guide-txt fn-parents">
-        <div
-          className={`${descriptionOpen ? '' : 'text'} description`}
-          dangerouslySetInnerHTML={{ __html: description }}
-          ref={this.textContainerRef}
-        />
-        {showMoreButton === true && (
-          <Button
-            icon
-            className={classNames('right btn-blue fn-more-toggle')}
-            onClick={this.toggleMore}
-          >
-            {descriptionOpen === true ? 'hide' : 'more'}{' '}
-            <Icon
-              className={classNames({
-                more2: descriptionOpen !== true,
-                hide2: descriptionOpen === true,
-              })}
-            />
-          </Button>
-        )}
+      <div className="class-guide-txt fn-parents" dangerouslySetInnerHTML={{ __html: description }} >
       </div>
     );
   }
 }
 
 export default Description;
+
+
+// 일정 크기 이상의 영역을 숨기고, 버튼을 표시하는 코드
+// CSS 문제로 해당 변경사항을 Rollback 하고 기록함
+// render() {
+//   //
+//   const { description } = this.props;
+//   const { descriptionOpen, showMoreButton } = this.state || {};
+
+//   return (
+//     <div className="class-guide-txt fn-parents">
+//       <div
+//         className={`${descriptionOpen ? '' : 'text'} description`}
+//         dangerouslySetInnerHTML={{ __html: description }}
+//         ref={this.textContainerRef}
+//       />
+//       {showMoreButton === true && (
+//         <Button
+//           icon
+//           className={classNames('right btn-blue fn-more-toggle')}
+//           onClick={this.toggleMore}
+//         >
+//           {descriptionOpen === true ? 'hide' : 'more'}{' '}
+//           <Icon
+//             className={classNames({
+//               more2: descriptionOpen !== true,
+//               hide2: descriptionOpen === true,
+//             })}
+//           />
+//         </Button>
+//       )}
+//     </div>
+//   );
+// }
