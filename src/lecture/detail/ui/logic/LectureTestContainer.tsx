0fbb8cad6f14dd2294909b07db14a5c965e62cd8
곽은옥 <keunok@ucom.kr>
2020-10-22 12:09:49 +0900

    Revert "findExamination 파라메터 id 수정, findExamPaperForm 호출"
    This reverts commit ccdd8d08c62cd6414b5b571c7a77a0d1759b853d.

--- src/lecture/detail/ui/logic/LectureTestContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLectureStructure } from '../../service/useLectureStructure';
-
-function LectureTestContainer() {
-  useLectureStructure();
-  return null;
-}
-
-export default LectureTestContainer;
