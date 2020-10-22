ccdd8d08c62cd6414b5b571c7a77a0d1759b853d
곽은옥 <keunok@ucom.kr>
2020-10-22 12:08:36 +0900

    findExamination 파라메터 id 수정, findExamPaperForm 호출

---
+++ src/lecture/detail/ui/logic/LectureTestContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { useLectureStructure } from '../../service/useLectureStructure';
+
+function LectureTestContainer() {
+  useLectureStructure();
+  return null;
+}
+
+export default LectureTestContainer;
