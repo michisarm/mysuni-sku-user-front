f926c5ed00ab50534ff2bea8e7e8ed9e860ad486
김승호 <>
2020-10-26 18:34:28 +0900

    css 청크파일 수정

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import 'semantic-ui-less/semantic.less';
 
 import '@nara.drama/approval/lib/snap.css';
 
-import './style/css/main.09f96124.chunk.css';
+import './style/css/main.e7b202c9.chunk.css';
 // 0721 badge layout 변경
 // import './style/css/2.6a3c6d3d.chunk.css';
 // 0724 badge layout 변경 - 발급기관로고 분리
 //import './style/css/2.b6563bb3.chunk.css';
 // 0810 PSJ 뱃지 디자인 변경
 //import './style/css/2.2e9b8fef.chunk.css';
-import './style/css/2.996508f7.chunk.css';
-
+// import './style/css/2.996508f7.chunk.css';
+import './style/css/2.19382c8f.chunk.css';
 
 import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
