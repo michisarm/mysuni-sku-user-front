349b619afe48f591d41d674df58d72039310b709
박지훈 <parkjihoon.com@gmail.com>
2020-10-30 11:15:09 +0900

    video contents 개발 #3 - 오류 수정

--- src/lecture/detail/ui/view/LectureVideoView/LectureVideoView.tsx
+++ src/lecture/detail/ui/view/LectureVideoView/LectureVideoView.tsx
@@ -47,23 +47,37 @@ const LectureVideoView: React.FC<LectureVideoViewProps> = function LectureVideoV
               </div>
 
               <div className="course-video">
-                {getLectureMedia() && (
-                  <SkuVideoPlayer
-                    cubeId=""
-                    //deliveryId="2081ffc6-d685-4f12-9e45-ac2a0019e0b3"
-                    deliveryId={
-                      getLectureMedia().mediaContents.internalMedias[0]
-                        .panoptoSessionId
-                    }
-                    //userId="djpaek@sk.com"
-                    userId={patronInfo.getPatronId() || ''}
-                    useCaption={true}
-                    captionInfos={captionInfos}
-                    hasNext={true}
-                    nextInfos={null}
-                  />
-                )}
+                {getLectureMedia() &&
+                  (getLectureMedia().mediaType == 'LinkMedia' ||
+                    getLectureMedia().mediaType == 'ContentsProviderMedia') && (
+                    <video controls>
+                      <source
+                        src={getLectureMedia().mediaContents.linkMediaUrl}
+                      ></source>
+                    </video>
+                  )}
+                {getLectureMedia() &&
+                  (getLectureMedia().mediaType == 'InternalMedia' ||
+                    getLectureMedia().mediaType == 'InternalMediaUpload') && (
+                    <SkuVideoPlayer
+                      cubeId=""
+                      //deliveryId="2081ffc6-d685-4f12-9e45-ac2a0019e0b3"
+                      deliveryId={
+                        getLectureMedia().mediaContents.internalMedias[0]
+                          .panoptoSessionId
+                      }
+                      //userId="djpaek@sk.com"
+                      userId={patronInfo.getPatronId() || ''}
+                      useCaption={true}
+                      captionInfos={captionInfos}
+                      hasNext={true}
+                      nextInfos={null}
+                    />
+                  )}
                 {getLectureTranscripts() &&
+                  getLectureMedia() &&
+                  (getLectureMedia().mediaType == 'InternalMedia' ||
+                    getLectureMedia().mediaType == 'InternalMediaUpload') &&
                   getLectureTranscripts().length > 0 &&
                   displayTranscript && (
                     <>
