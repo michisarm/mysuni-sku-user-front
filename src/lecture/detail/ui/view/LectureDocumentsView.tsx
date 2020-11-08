4e58c5eb4d751b9bfe2211fd51291d379cce5ffe
박영일 <baramboy@gmail.com>
2020-11-08 22:03:01 +0900

    Panopto Embed API 를 Video, Audio 측에 반영. PDF 뷰어 적용 및 스타일 작업.

--- src/lecture/detail/ui/view/LectureDocumentsView.tsx
+++ src/lecture/detail/ui/view/LectureDocumentsView.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LectureWebpage from '../../viewModel/LectureWebpage';
+import { onLectureMedia } from 'lecture/detail/store/LectureMediaStore';
+import { Document, Page } from 'react-pdf';
 
+// http://localhost:3000/lecture/cineroom/ne1-m2-c2/college/CLG0001c/cube/CUBE-2ls/lecture-card/LECTURE-CARD-29d
 const LectureDocumentsView: React.FC<LectureWebpage> = function LectureDocumentsView({
   fileBoxId,
 }) {
+  const [numPages, setNumPages] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = (pdf:any) => {
+    setNumPages(pdf.numPages);
+  }
+
+  const prev = () => {
+    if (pageNumber > 1) {
+      setPageNumber(pageNumber - 1);
+    }
+  }
+
+  const next = () => {
+    if (pageNumber < numPages) {
+      setPageNumber(pageNumber + 1);
+    }
+  }
+
+  useEffect(()=>{    
+    onLectureMedia(lectureMedia=>{
+      console.log('-----file contents...');
+      console.log(lectureMedia);
+    }, 'LectureDocumentsView');
+  });
+
   return (
-    <div
-      style={{
-        width: '100%',
-        maxWidth: 950,
-        height: 684,
-        backgroundColor: '#a8a8a8',
-      }}
-    />
+    <div className="documents-viewer">
+      <Document
+        renderMode="svg"
+        file="/assets/docs/sample-pdf.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div className="pagination">
+        <button disabled={pageNumber === 1} type="button" onClick={prev}>‹</button>
+        <span>{pageNumber} of {numPages}</span>
+        <button disabled={pageNumber >= numPages} type="button" onClick={next}>›</button>
+      </div>        
+    </div>
   );
 };
 
--- src/lecture/detail/ui/view/LectureVideoView/LectureVideoView.tsx
+++ src/lecture/detail/ui/view/LectureVideoView/LectureVideoView.tsx
@@ -2,7 +2,7 @@
 
 import React, { useCallback, useEffect, useState } from 'react';
 import { getLectureTranscripts } from 'lecture/detail/store/LectureTranscriptStore';
-import { getLectureMedia } from 'lecture/detail/store/LectureMediaStore';
+import { getLectureMedia, onLectureMedia } from 'lecture/detail/store/LectureMediaStore';
 import SkuVideoPlayer from 'lecture/detail/service/useLectureMedia/components/SkuPlayer/SkuVideoPlayer';
 import { patronInfo } from '@nara.platform/dock';
 
@@ -12,7 +12,14 @@ import { patronInfo } from '@nara.platform/dock';
 
 interface LectureVideoViewProps {}
 
-const LectureVideoView: React.FC<LectureVideoViewProps> = function LectureVideoView({}) {
+//FIXME SSO 로그인된 상태가 아니면 동작 안 함. 
+const LectureVideoView: React.FC<LectureVideoViewProps> = function LectureVideoView({}) {  
+
+  const [embedApi, setEmbedApi] = useState({
+    pauseVideo: () => {},
+    seekTo: (index: number) => {},
+  });
+
   const toHHMM = useCallback((idx: number) => {
     const time = idx;
     const hours = Math.floor(time / 60);
@@ -21,7 +28,7 @@ const LectureVideoView: React.FC<LectureVideoViewProps> = function LectureVideoV
     let sHours = '';
     let sMinutes = '';
     sHours = String(hours.toString()).padStart(2, '0');
-    sMinutes = String(minutes.toString()).padStart(2, '0');
+    sMinutes = String(minutes.toString()).padStart(2, '0');    
 
     return sHours + ':' + sMinutes;
   }, []);
@@ -33,15 +40,124 @@ const LectureVideoView: React.FC<LectureVideoViewProps> = function LectureVideoV
       { lang: 'en', url: 'test_en.srt', isDefault: true },
     ],
   };
+
+  const onPanoptoIframeReady = () => {
+    // The iframe is ready and the video is not yet loaded (on the splash screen)
+    // Load video will begin playback
+    console.log('panopto iframe loaded...');
+    //embedApi.loadVideo();//페이지 로드 시 자동 실행됩니다.
+  };
+
+  const onPanoptoLoginShown = () => {
+    console.log('------로그인이 안 되어 있어요....!!!!!!!!!');
+    //로그인 페이지가 안 떠 있는 경우 바로 Clear 시켜서 반복 호출을 막아야 합니다.
+    cleanUpPanoptoIframe();
+  };
+
+  const cleanUpPanoptoIframe = () => {
+    let playerEl = document.getElementById('panopto-embed-player');
+    if (playerEl)
+      playerEl.innerHTML = '';
+  };
+
+  const seekByIndex = (index: number) => {
+    console.log('transcript index: ' + index); 
+    if (embedApi && index >= 0) {
+      //TODO current state 를 찾아서 Play 이 
+      embedApi.seekTo(index);
+    }
+  };
+
+  useEffect(()=>{    
+    onLectureMedia(lectureMedia=>{
+      cleanUpPanoptoIframe();//기존에 어떤 상태이건 초기화
+      if (typeof lectureMedia === 'undefined') {
+      }
+      else {
+        let currentPaonoptoSessionId = lectureMedia.mediaContents.internalMedias[0].panoptoSessionId || '';
+        let embedApi = new window.EmbedApi("panopto-embed-player", {
+          width: "750",
+          height: "422",
+          //This is the URL of your Panopto site
+          //https://sku.ap.panopto.com/Panopto/Pages/Auth/Login.aspx?support=true
+          serverName: "sku.ap.panopto.com",
+          sessionId: currentPaonoptoSessionId,
+          videoParams: { // Optional parameters
+            //interactivity parameter controls whether the user will see table of contents, discussions, notes, and in-video search
+            "interactivity": "none",
+            "showtitle": "false",
+            "showBrand": "true",
+            "offerviewer": "false",
+          },
+          events: {
+            "onIframeReady": onPanoptoIframeReady,
+            "onLoginShown": onPanoptoLoginShown,
+            //"onReady": onPanoptoVideoReady,
+            //"onStateChange": onPanoptoStateUpdate
+          }
+        });
+        setEmbedApi(embedApi);
+      }
+    
+    },'LectureVideoView');
+
+    return () => {
+      cleanUpPanoptoIframe();
+    };
+  }, []);
+
   return (
-    <div
-      style={{
-        width: '100%',
-        maxWidth: 950,
-        height: 535,
-        backgroundColor: '#a8a8a8',
-      }}
-    />
+    <div className="course-info-detail responsive-course">
+      <div className="course-detail-center">
+        <div className="main-wrap">
+          <div className="scrolling-area area2">
+            <div className="ui segment full">
+              <div className="course-video">
+                <div id="panopto-embed-player"></div>
+                {getLectureTranscripts() &&
+                  getLectureMedia() &&
+                  (getLectureMedia()?.mediaType == 'InternalMedia' ||
+                    getLectureMedia()?.mediaType == 'InternalMediaUpload') &&
+                  (getLectureTranscripts()?.length || 0) > 0 &&
+                  displayTranscript && (
+                    <>
+                      <button
+                        className="ui icon button right btn-blue"
+                        onClick={() => setDisplayTranscript(false)}
+                      >
+                        Close Transcript
+                        <i aria-hidden="true" className="icon icon morelink" />
+                      </button>
+
+                      <div className="course-video-tanscript">
+                        <div className="course-video-scroll">
+                          {getLectureTranscripts()?.map(lectureTranscript => {
+                            return (
+                              <>
+                                <strong style={{cursor: 'pointer'}} onClick={()=>{seekByIndex(lectureTranscript.idx)}}>{toHHMM(lectureTranscript.idx)}</strong>
+                                <p>{lectureTranscript.text}</p>
+                              </>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    </>
+                  )}
+                {getLectureTranscripts() && !displayTranscript && (
+                  <button
+                    className="ui icon button right btn-blue"
+                    onClick={() => setDisplayTranscript(true)}
+                  >
+                    View Transcript
+                    <i aria-hidden="true" className="icon icon morelink" />
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
