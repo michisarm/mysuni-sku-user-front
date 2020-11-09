8ce1b548cf05d2cd5d203f8b0b1cedf9c2fef8c5
ahnheetae <ahnheetae@ucomp.co.kr>
2020-11-09 15:04:58 +0900

    2020-11-09 MyCommunityPostListContainer.tsx 수정

--- src/community/ui/logic/MyCommunityIntro/MyCommunityPostListContainer.tsx
+++ src/community/ui/logic/MyCommunityIntro/MyCommunityPostListContainer.tsx
@@ -12,49 +12,51 @@ const PostItemView: React.FC<PostItem> = function CommunityItemView({
   contents,
 }) {
   return (
-    <div className="comment-area community-main-card">
-      {/* comments */}
-      <Comment.Group className="base">
-        {/*comment : 2줄이상 말줄임, 대댓글*/}
-        <Comment>
-          <Comment.Avatar src={profileImage} />
-          <Comment.Content>
-            <Comment.Author as="a">{communityName}</Comment.Author>
-            <Comment.Text>
-              <div className="ellipsis">
-                <span className="id">{profileId}</span>
-                <span className="date">{createTime}</span>
-              </div>
-              {/* <Button>+ View more</Button> */}
-            </Comment.Text>
-            <Comment.Actions>
-              <div className="right top">
-                <Button icon className="img-icon">
-                  <Icon className="bookmark2" />
-                  <span className="blind">북마크</span>
-                </Button>
-                <Button icon className="img-icon">
-                  <Icon className="share2" />
-                  <span className="blind">공유</span>
-                </Button>
-              </div>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-        <div className="card-bottom">
-          <h3>
-            <span className="ico_feed board">게시물</span>
-            {name}
-          </h3>
-          <div dangerouslySetInnerHTML={{ __html: contents }} />
-          <div className="text-right">
-            <button className="ui icon button right btn-blue btn-more">
-              more
-              <i aria-hidden="true" className="icon more2" />
-            </button>
+    <div className="sub-info-box">
+      <div className="comment-area community-main-card">
+        {/* comments */}
+        <Comment.Group className="base">
+          {/*comment : 2줄이상 말줄임, 대댓글*/}
+          <Comment>
+            <Comment.Avatar src={profileImage} />
+            <Comment.Content>
+              <Comment.Author as="a">{communityName}</Comment.Author>
+              <Comment.Text>
+                <div className="ellipsis">
+                  <span className="id">{profileId}</span>
+                  <span className="date">{createTime}</span>
+                </div>
+                {/* <Button>+ View more</Button> */}
+              </Comment.Text>
+              <Comment.Actions>
+                <div className="right top">
+                  <Button icon className="img-icon">
+                    <Icon className="bookmark2" />
+                    <span className="blind">북마크</span>
+                  </Button>
+                  <Button icon className="img-icon">
+                    <Icon className="share2" />
+                    <span className="blind">공유</span>
+                  </Button>
+                </div>
+              </Comment.Actions>
+            </Comment.Content>
+          </Comment>
+          <div className="card-bottom">
+            <h3>
+              <span className="ico_feed board">게시물</span>
+              {name}
+            </h3>
+            <div dangerouslySetInnerHTML={{ __html: contents }} />
+            <div className="text-right">
+              <button className="ui icon button right btn-blue btn-more">
+                more
+                <i aria-hidden="true" className="icon more2" />
+              </button>
+            </div>
           </div>
-        </div>
-      </Comment.Group>
+        </Comment.Group>
+      </div>
     </div>
   );
 };
