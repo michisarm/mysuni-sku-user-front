624b7585c9bcc5ae64dc6695b33eee5fd19aeca9
박지훈 <parkjihoon.com@gmail.com>
2020-11-14 17:58:58 +0900

    media 기능 수정 진행 #4

--- src/lecture/detail/service/useLectureMedia/utility/checkStudent.ts
+++ src/lecture/detail/service/useLectureMedia/utility/checkStudent.ts
@@ -1,35 +1,11 @@
 /* eslint-disable consistent-return */
 import LectureRouterParams from 'lecture/detail/viewModel/LectureRouterParams';
 import {
-  findIsJsonStudentByCube, joinCommunity,
+  findIsJsonStudentByCube, joinCommunity, registerStudent,
 } from 'lecture/detail/api/lectureApi';
 import SkProfileService from 'profile/present/logic/SkProfileService';
 import StudentCdo from 'lecture/detail/model/StudentCdo';
 import { getStateFromCube } from '../../useLectureState/utility/getStateFromCube';
-
-export async function registerStudent(
-  rollBookId:string
-): Promise<void> {
-
-  const {
-    skProfile: { member },
-  } = SkProfileService.instance;
-  const nextStudentCdo: StudentCdo = {
-    rollBookId,
-    name: member.name,
-    email: member.email,
-    company: member.company,
-    department: member.department,
-    proposalState: 'Submitted',
-    programLectureUsid: '',
-    courseLectureUsid: '',
-    leaderEmails: [],
-    url: '',
-    classroomId: '',
-    approvalProcess: false,
-  };
-  await joinCommunity(nextStudentCdo);
-}
 
 export async function checkStudent(
   params: LectureRouterParams
@@ -40,7 +16,26 @@ export async function checkStudent(
     const studentJoins = await findIsJsonStudentByCube(lectureId);
     // 미디어 플레이 시점에 student 데이터가 없으면 생성 후 우측 상단 학습중 표시
     if (studentJoins.length > 0 && !studentJoins[0].join) {
-      await registerStudent(studentJoins[0].rollBookId);
+
+      const {
+        skProfile: { member },
+      } = SkProfileService.instance;
+      const nextStudentCdo: StudentCdo = {
+        rollBookId : studentJoins[0].rollBookId,
+        name: member.name,
+        email: member.email,
+        company: member.company,
+        department: member.department,
+        proposalState: 'Approved',
+        programLectureUsid: '',
+        courseLectureUsid: '',
+        leaderEmails: [],
+        url: '',
+        classroomId: '',
+        approvalProcess: false,
+      };
+
+      await registerStudent(nextStudentCdo);
       getStateFromCube(params);
     }
   }
