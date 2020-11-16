c73862e5974b53fc0c1e815790c066c048574010
ahnheetae <ahnheetae@ucomp.co.kr>
2020-11-16 17:04:23 +0900

    add chunk.css & 토론하기 스타일

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import 'react-quill/dist/quill.snow.css';
 
 //2020-11-05
 import './style/css/2.1c5db5b6.chunk.css';
-import './style/css/main.77ada192.chunk.css';
+import './style/css/main.32b4dfa7.chunk.css';
 
 initializeBody();
 
