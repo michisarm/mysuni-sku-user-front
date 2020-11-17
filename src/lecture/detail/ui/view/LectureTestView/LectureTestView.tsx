f1f7f6c4aff178299b11e8e80b9989ea0e7b73f2
박은호 <wagno@ucom.kr>
2020-11-17 15:36:18 +0900

    테스트 페이지 UI 오류 수정

--- src/lecture/detail/ui/view/LectureTestView/LectureTestView.tsx
+++ src/lecture/detail/ui/view/LectureTestView/LectureTestView.tsx
@@ -79,161 +79,142 @@ const LectureTestView: React.FC<LectureTestViewProps> = function LectureTestView
     testClassName += ' test-complete ';
   }
   return (
-    <>
-      <div className="course-detail-center">
-        <div className="main-wrap">
-          <div className="scrolling-area area2 ">
-            <div className={testClassName}>
-              {testItem && (
-                <>
-                  <div className="course-info-header">
-                    <div className="survey-header">
-                      <div className="survey-header-left">
-                        {testItem.name}
-                      </div>
-                      <div className="survey-header-right">
-                        {!testStudentItem ||
-                          !testStudentItem.learningState ||
-                          (testStudentItem.learningState !== 'Failed' &&
-                            testStudentItem.learningState !== 'Missed' &&
-                            testStudentItem.learningState !== 'TestWaiting' &&
-                            testStudentItem.learningState !== 'Passed' )}
-                        {testStudentItem &&
-                          testStudentItem.learningState &&
-                          (testStudentItem.learningState === 'Failed' ||
-                            testStudentItem.learningState === 'Missed') && (
-                            <button className="ui button free proceeding p18">
-                              미이수
-                            </button>
-                          )}
-                        {testStudentItem &&
-                          testStudentItem.learningState &&
-                          testStudentItem.learningState === 'TestWaiting' && (
-                            <button className="ui button free proceeding p18">
-                              검수중
-                            </button>
-                          )}
-                        {testStudentItem &&
-                          testStudentItem.learningState &&
-                          (testStudentItem.learningState === 'Passed' ||
-                            testStudentItem.learningState ===
-                            'TestPassed') && (
-                            <button className="ui button free proceeding p18">
-                              이수
-                            </button>
-                          )}
-                      </div>
-                      <div className="test-text">
-                        <div className="test-text-box">
-                          <span>합격점</span>
-                          <span>{testItem.successPoint}점</span>
-                        </div>
-                        <div className="test-text-box">
-                          <span>총점</span>
-                          <span>{testItem.totalPoint}점</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {testItem &&
-                    testItem.questions &&
-                    testItem.questions.map(question => {
-                      let answer: string = '';
-                      let answerResult: boolean = false;
-                      if (answerItem !== undefined) {
-                        answerItem.answers.forEach(result => {
-                          if (result.questionNo === question.questionNo) {
-                            answer = result.answer;
-                          }
-                        });
+    <div className={testClassName}>
+      {testItem && (
+        <>
+          <div className="course-info-header">
+            <div className="survey-header">
+              <div className="survey-header-left">{testItem.name}</div>
+              <div className="survey-header-right">
+                {!testStudentItem ||
+                  !testStudentItem.learningState ||
+                  (testStudentItem.learningState !== 'Failed' &&
+                    testStudentItem.learningState !== 'Missed' &&
+                    testStudentItem.learningState !== 'TestWaiting' &&
+                    testStudentItem.learningState !== 'Passed')}
+                {testStudentItem &&
+                  testStudentItem.learningState &&
+                  (testStudentItem.learningState === 'Failed' ||
+                    testStudentItem.learningState === 'Missed') && (
+                    <button className="ui button free proceeding p18">
+                      미이수
+                    </button>
+                  )}
+                {testStudentItem &&
+                  testStudentItem.learningState &&
+                  testStudentItem.learningState === 'TestWaiting' && (
+                    <button className="ui button free proceeding p18">
+                      검수중
+                    </button>
+                  )}
+                {testStudentItem &&
+                  testStudentItem.learningState &&
+                  (testStudentItem.learningState === 'Passed' ||
+                    testStudentItem.learningState === 'TestPassed') && (
+                    <button className="ui button free proceeding p18">
+                      이수
+                    </button>
+                  )}
+              </div>
+              <div className="test-text">
+                <div className="test-text-box">
+                  <span>합격점</span>
+                  <span>{testItem.successPoint}점</span>
+                </div>
+                <div className="test-text-box">
+                  <span>총점</span>
+                  <span>{testItem.totalPoint}점</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {testItem &&
+            testItem.questions &&
+            testItem.questions.map(question => {
+              let answer: string = '';
+              let answerResult: boolean = false;
+              if (answerItem !== undefined) {
+                answerItem.answers.forEach(result => {
+                  if (result.questionNo === question.questionNo) {
+                    answer = result.answer;
+                  }
+                });
 
-                        if (answerItem.submitted) {
-                          let submitAnswer = '';
-                          answerItem.submitAnswers.forEach(result => {
-                            if (result.questionNo === question.questionNo) {
-                              submitAnswer = result.answer;
-                            }
-                          });
-                          if (question.questionType === 'SingleChoice') {
-                            if (question.answer === submitAnswer) {
-                              answerResult = true;
-                            }
-                          }
-                          if (question.questionType === 'MultiChoice') {
-                            let answerChkArr = [];
+                if (answerItem.submitted) {
+                  let submitAnswer = '';
+                  answerItem.submitAnswers.forEach(result => {
+                    if (result.questionNo === question.questionNo) {
+                      submitAnswer = result.answer;
+                    }
+                  });
+                  if (question.questionType === 'SingleChoice') {
+                    if (question.answer === submitAnswer) {
+                      answerResult = true;
+                    }
+                  }
+                  if (question.questionType === 'MultiChoice') {
+                    let answerChkArr = [];
 
-                            // 문제지 정답
-                            answerChkArr = JSON.parse(question.answer);
-                            // 사용자 정답
-                            const answerMultiJson = submitAnswer.split(',');
-                            let checkCnt = 0;
+                    // 문제지 정답
+                    answerChkArr = JSON.parse(question.answer);
+                    // 사용자 정답
+                    const answerMultiJson = submitAnswer.split(',');
+                    let checkCnt = 0;
 
-                            // 자릿수 비교
-                            if (
-                              answerChkArr.length === answerMultiJson.length
-                            ) {
-                              // 정답지
-                              for (let i = 0; i < answerChkArr.length; i++) {
-                                // 사용자문제지
-                                for (
-                                  let j = 0;
-                                  j < answerMultiJson.length;
-                                  j++
-                                ) {
-                                  // 정답지 사용자 문제지 체크
-                                  if (
-                                    answerChkArr[i] === answerMultiJson[j]
-                                  ) {
-                                    checkCnt++;
-                                  }
-                                }
-                              }
-                            }
-
-                            // 정답지와 사용자 정답 갯수 체크
-                            if (answerChkArr.length === checkCnt) {
-                              answerResult = true;
-                            }
+                    // 자릿수 비교
+                    if (answerChkArr.length === answerMultiJson.length) {
+                      // 정답지
+                      for (let i = 0; i < answerChkArr.length; i++) {
+                        // 사용자문제지
+                        for (let j = 0; j < answerMultiJson.length; j++) {
+                          // 정답지 사용자 문제지 체크
+                          if (answerChkArr[i] === answerMultiJson[j]) {
+                            checkCnt++;
                           }
                         }
                       }
+                    }
 
-                      return (
-                        <TestQuestionView
-                          key={'question_' + question.questionNo}
-                          question={question}
-                          submitted={answerItem?.submitted}
-                          answer={answer}
-                          answerResult={answerResult}
-                          readOnly={readOnly}
-                        />
-                      );
-                    })}
-                  {!readOnly && (
-                    <div className="survey-preview">
-                      <p>
-                        <button
-                          className="ui button fix line"
-                          onClick={saveAnswerSheet}
-                        >
-                          저장
-                        </button>
-                        <button
-                          className="ui button fix bg"
-                          onClick={submitAnswerSheet}
-                        >
-                          제출
-                        </button>
-                      </p>
-                    </div>
-                  )}
-                </>
-              )}
+                    // 정답지와 사용자 정답 갯수 체크
+                    if (answerChkArr.length === checkCnt) {
+                      answerResult = true;
+                    }
+                  }
+                }
+              }
+
+              return (
+                <TestQuestionView
+                  key={'question_' + question.questionNo}
+                  question={question}
+                  submitted={answerItem?.submitted}
+                  answer={answer}
+                  answerResult={answerResult}
+                  readOnly={readOnly}
+                />
+              );
+            })}
+          {!readOnly && (
+            <div className="survey-preview">
+              <p>
+                <button
+                  className="ui button fix line"
+                  onClick={saveAnswerSheet}
+                >
+                  저장
+                </button>
+                <button
+                  className="ui button fix bg"
+                  onClick={submitAnswerSheet}
+                >
+                  제출
+                </button>
+              </p>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
+          )}
+        </>
+      )}
+    </div>
   );
 };
 
