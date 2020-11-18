f9c9150173fb9f652dacfaa548cc4fcff9fae774
ahnheetae <ahnheetae@ucomp.co.kr>
2020-11-18 17:25:17 +0900

    공통 컴포넌트 폴더 생성

---
+++ src/search/Components/ExpertCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExpertCard: React.FC = () => {
+  return <>ExpertCard</>;
+};
+
+export default ExpertCard;
--- src/search/HeaderArea/ContentsHeader.tsx
+++ src/search/HeaderArea/ContentsHeader.tsx
@@ -1,12 +1,10 @@
-import React, {useLayoutEffect, useState} from 'react'
-import {
-    Segment, Radio, Button,
-} from 'semantic-ui-react'
+import React, { useLayoutEffect, useState } from 'react';
+import { Segment, Radio, Button } from 'semantic-ui-react';
 import 'react-datepicker/dist/react-datepicker.css';
 import classNames from 'classnames';
 // import {SearchFilter} from '../../../../../components';
 
-const ContentsHeader:React.FC = () => {
+const ContentsHeader: React.FC = () => {
   //
   const [isOnFilter, setIsOnFilter] = useState<boolean>(false);
 
@@ -14,36 +12,43 @@ const ContentsHeader:React.FC = () => {
 
   // onClickFilter = () =>  this.setState((prevState) => ({isOnFilter: !prevState.isOnFilter}));
 
+  // Get quertID
   useLayoutEffect(() => {
-    const queryId: string = window.location.search.slice(window.location.search.indexOf('=') + 1, window.location.search.length);
+    const queryId: string = window.location.search.slice(
+      window.location.search.indexOf('=') + 1,
+      window.location.search.length
+    );
     setSearchValue(queryId);
-  },[]);
-
+  }, []);
 
   return (
-      <>
-        <div className="sort-condition">
-          <Segment className="full">
-            <div className="keyword">
-              <span>{`${searchValue}`}</span>에 대한 검색 결과 입니다.
+    <>
+      <div className="sort-condition">
+        <Segment className="full">
+          <div className="keyword">
+            <span>{`${searchValue}`}</span>에 대한 검색 결과 입니다.
+          </div>
+          <div className="right-area">
+            <div className="sort">
+              <Radio
+                className="base"
+                label="과정명으로 검색"
+                name="search01"
+                defaultChecked
+              />
+              <Radio className="base" label="상세 검색" name="search01" />
+              <Radio className="base" label="Tag 검색" name="search01" />
             </div>
-            <div className="right-area">
-              <div className="sort">
-                <Radio className="base" label="과정명으로 검색" name="search01" defaultChecked/>
-                <Radio className="base" label="상세 검색" name="search01"/>
-                <Radio className="base" label="Tag 검색" name="search01"/>
-              </div>
-                {/* <Button className={classNames('btn-filter-blue', isOnFilter ? 'on' : '')} onClick={this.onClickFilter}>
+            {/* <Button className={classNames('btn-filter-blue', isOnFilter ? 'on' : '')} onClick={this.onClickFilter}>
                   <span>Filter(05)</span>
                 </Button> */}
-            </div>
-            {/*Filter*/}
-            {/* <SearchFilter isOnFilter={isOnFilter}/> */}
-          </Segment>
-        </div>
-      </>
-  )
-}
-
+          </div>
+          {/*Filter*/}
+          {/* <SearchFilter isOnFilter={isOnFilter}/> */}
+        </Segment>
+      </div>
+    </>
+  );
+};
 
 export default ContentsHeader;
