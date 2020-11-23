9fcb7fafd3518342e1ebed433eb186a49866bbd7
박은호 <wagno@ucom.kr>
2020-11-23 16:57:59 +0900

    Task Cube 뒤로가기

--- src/lecture/detail/ui/logic/LectureTaskContainer.tsx
+++ src/lecture/detail/ui/logic/LectureTaskContainer.tsx
@@ -4,6 +4,7 @@ import LectureTaskView from '../view/LectureTaskView/LectureTaskView';
 import {
   getLectureTaskDetail,
   setLectureTaskDetail,
+  setLectureTaskItem,
   setLectureTaskOffset,
   setLectureTaskTab,
   setLectureTaskViewType,
@@ -31,8 +32,34 @@ import {
 import { reactConfirm } from '@nara.platform/accent';
 import { updateLectureTask } from 'lecture/detail/service/useLectureTask/utility/updateLectureTask';
 import { createLectureTask } from 'lecture/detail/service/useLectureTask/utility/createLectureTask';
+import { useHistory, useLocation } from 'react-router-dom';
+import { getTaskDetailCube } from '../../service/useLectureTask/utility/getTaskDetailCube';
 
 function LectureTaskContainer() {
+  const { pathname, hash } = useLocation();
+  const history = useHistory();
+  useEffect(() => {
+    return () => {
+      setLectureTaskTab('Overview');
+    };
+  }, [pathname]);
+  useEffect(() => {
+    if (hash === '#create') {
+      setLectureTaskViewType('create');
+      return;
+    } else if (hash === '#edit') {
+      setLectureTaskViewType('edit');
+      return;
+    } else if (hash === '#reply') {
+      setLectureTaskViewType('reply');
+      return;
+    } else if (hash === '#detail') {
+      setLectureTaskViewType('detail');
+      return;
+    }
+    setLectureTaskViewType('list');
+  }, [hash]);
+
   const [taskItem] = useLectureTask();
   const [taskDetail] = useLectureTaskDetail();
   const params = useLectureRouterParams();
@@ -53,41 +80,39 @@ function LectureTaskContainer() {
   }, []);
 
   const moveToDetail = useCallback((param: any) => {
-    setLectureTaskViewType(param);
+    getTaskDetailCube(param);
     setDetailTaskId(param.id);
     setDetailType(param.type);
+
     //게시글 부모인지 자식인지
   }, []);
 
   const onClickList = useCallback(() => {
-    setLectureTaskViewType('list');
+    history.goBack();
   }, []);
 
   const onHandleSave = useCallback(() => {
-    setLectureTaskViewType('list');
+    history.goBack();
   }, []);
 
   const onHandleReply = useCallback(() => {
-    setLectureTaskViewType('list');
+    history.goBack();
   }, []);
 
   const onClickModify = useCallback(() => {
     setCreate(true);
-    setLectureTaskViewType('edit');
+    history.goBack();
   }, []);
 
-  const onClickReplies = useCallback(() => {
-    setLectureTaskViewType('reply');
-  }, []);
+  const onClickReplies = useCallback(() => {}, []);
 
   const onClickDelete = useCallback((id: string, type: string) => {
-    setLectureTaskViewType('list');
     deletePost(id, type);
+    history.goBack();
   }, []);
 
   const listHashLink = useCallback((hash: string) => {
     setLectureTaskTab(hash);
-    setLectureTaskViewType('list');
     const element = document.getElementById(hash);
     if (element !== null) {
       element.scrollIntoView();
@@ -96,7 +121,6 @@ function LectureTaskContainer() {
 
   const overviewHashLink = useCallback((hash: string) => {
     setLectureTaskTab(hash);
-    setLectureTaskViewType('Overview');
     const element = document.getElementById(hash);
     if (element !== null) {
       element.scrollIntoView();
@@ -105,7 +129,6 @@ function LectureTaskContainer() {
 
   const handelClickCreateTask = useCallback(() => {
     setCreate(true);
-    setLectureTaskViewType('create');
   }, []);
 
   const onHandleChange = useCallback(
@@ -160,12 +183,11 @@ function LectureTaskContainer() {
               commentFeedbackId: '',
               notice: false,
             });
-  
-            setLectureTaskViewType('list');
-          })
+            history.goBack();
+          });
         } else {
           updateLectureTask(detailTaskId);
-          setLectureTaskViewType('list');
+          history.goBack();
         }
       },
     });
@@ -211,7 +233,7 @@ function LectureTaskContainer() {
           </ContentLayout>
         </div>
       )}
-      {viewType !== 'list' && viewType !== 'create' && viewType !== 'edit' && (
+      {viewType === 'detail' && (
         <>
           <LectureTaskDetailView
             taskId={detailTaskId}
@@ -259,21 +281,6 @@ function LectureTaskContainer() {
           handleOnClickList={onHandleReply}
           handleCloseClick={onClickList}
         />
-      )}
-      {viewType === 'Overview' && (
-        <>
-          <LectureCubeSummaryContainer />
-          <ContentLayout className="support">
-            <LectureTaskView
-              lectureDescription={lectureDescription}
-              lectureSubcategory={lectureSubcategory}
-              lectureTags={lectureTags}
-              lectureFile={lectureFile}
-              listHashLink={listHashLink}
-              overviewHashLink={overviewHashLink}
-            />
-          </ContentLayout>
-        </>
       )}
     </>
   );
--- src/lecture/detail/ui/view/LectureTaskView/LectureTaskDetailContentHeaderView.tsx
+++ src/lecture/detail/ui/view/LectureTaskView/LectureTaskDetailContentHeaderView.tsx
@@ -5,6 +5,7 @@ import { observer } from 'mobx-react';
 import moment from 'moment';
 import { Button, Icon } from 'semantic-ui-react';
 import { LectureTaskDetail } from 'lecture/detail/viewModel/LectureTaskDetail';
+import { useHistory } from 'react-router-dom';
 
 interface Props {
   taskDetail: LectureTaskDetail;
@@ -21,112 +22,105 @@ interface Props {
   onClickReplies: (id: string) => void;
 }
 
-@reactAutobind
-@observer
-class LectureTaskDetailContentHeaderView extends Component<Props> {
+const LectureTaskDetailContentHeaderView: React.FC<Props> = function LectureTaskDetailContentHeaderView({
+  title,
+  time,
+  subField,
+  deletable,
+  reply,
+  readCount,
+  name,
+  taskDetail,
+  onClickList,
+  onClickDelete,
+  onClickModify,
+  onClickReplies,
+}) {
   //
-  render() {
-    //
-    const {
-      title,
-      time,
-      subField,
-      deletable,
-      reply,
-      readCount,
-      name,
-      taskDetail,
-      onClickList,
-      onClickDelete,
-      onClickModify,
-      onClickReplies,
-    } = this.props;
+  const history = useHistory();
 
-    const handelClickModify = () => {
-      onClickModify(taskDetail.id);
-    };
+  const handelClickModify = () => {
+    onClickModify(taskDetail.id);
+  };
 
-    const handelClickReplies = () => {
-      onClickReplies(taskDetail.id);
-    };
+  const handelClickReplies = () => {
+    onClickReplies(taskDetail.id);
+  };
 
-    const handelClickDelete = () => {
-      onClickDelete(taskDetail.id);
-    };
+  const handelClickDelete = () => {
+    onClickDelete(taskDetail.id);
+  };
 
-    return (
-      <>
-        <div className="course-info-header">
-          <div className="survey-header">
-            <div className="survey-header-left debate-header-sub">
-              <div className="title">{title}</div>
-              <div className="survey-read-side mb0">
-                <div className="title-area">
-                  <div className="ui label onlytext">{name}</div>
-                  <div className="ui label onlytext">
-                    <span>
-                      {time && moment(time).format('YYYY.MM.DD HH:MM')}
-                    </span>
-                  </div>
-                  <div className="ui label onlytext">
-                    <span className="header-span-first">조회수</span>
-                    <span>{readCount}</span>
-                  </div>
+  return (
+    <>
+      <div className="course-info-header">
+        <div className="survey-header">
+          <div className="survey-header-left debate-header-sub">
+            <div className="title">{title}</div>
+            <div className="survey-read-side mb0">
+              <div className="title-area">
+                <div className="ui label onlytext">{name}</div>
+                <div className="ui label onlytext">
+                  <span>{time && moment(time).format('YYYY.MM.DD HH:MM')}</span>
                 </div>
-                <div className="right-area">
-                  <div className="ui onlytext">
-                    {onClickModify && (
-                      <Button
-                        icon
-                        className="left postset edit"
-                        onClick={handelClickModify}
-                      >
-                        <Icon name="edit" />
-                        Edit
-                      </Button>
-                    )}
-                    {deletable && (
-                      <Button
-                        icon
-                        className="left postset delete"
-                        onClick={handelClickDelete}
-                      >
-                        <Icon name="delete" />
-                        Delete
-                      </Button>
-                    )}
-                    {reply && (
-                      <Button
-                        icon
-                        className="left postset delete"
-                        onClick={handelClickReplies}
-                      >
-                        <Icon name="reply" />
-                        Reply
-                      </Button>
-                    )}
+                <div className="ui label onlytext">
+                  <span className="header-span-first">조회수</span>
+                  <span>{readCount}</span>
+                </div>
+              </div>
+              <div className="right-area">
+                <div className="ui onlytext">
+                  {onClickModify && (
                     <Button
                       icon
-                      className="left postset commu-list16"
-                      onClick={onClickList}
+                      className="left postset edit"
+                      onClick={handelClickModify}
                     >
-                      <Icon className="commu-list16" />
-                      List
+                      <Icon name="edit" />
+                      Edit
                     </Button>
-                  </div>
+                  )}
+                  {deletable && (
+                    <Button
+                      icon
+                      className="left postset delete"
+                      onClick={handelClickDelete}
+                    >
+                      <Icon name="delete" />
+                      Delete
+                    </Button>
+                  )}
+                  {reply && (
+                    <Button
+                      icon
+                      className="left postset delete"
+                      onClick={handelClickReplies}
+                    >
+                      <Icon name="reply" />
+                      Reply
+                    </Button>
+                  )}
+                  <Button
+                    icon
+                    className="left postset commu-list16"
+                    onClick={onClickList}
+                  >
+                    <Icon className="commu-list16" />
+                    List
+                  </Button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="class-guide-txt fn-parents ql-snow">
+      </div>
+      {/* <div className="class-guide-txt fn-parents ql-snow">
           <div className="text ql-editor">
             <p>1234</p>
           </div>
         </div> */}
-      </>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default LectureTaskDetailContentHeaderView;
