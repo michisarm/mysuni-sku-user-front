87d447a4d19a3e47d1fca2cf6863166279be04be
곽은옥 <keunok@ucom.kr>
2020-11-26 12:59:25 +0900

    커뮤니티 프로필 namevalue로 저장

--- src/community/api/profileApi.ts
+++ src/community/api/profileApi.ts
@@ -1,9 +1,5 @@
 import { axiosApi } from '@nara.platform/accent';
-import Post from 'community/model/Post';
-import PostCdo from 'community/model/PostCdo';
-import PostUdo from 'community/model/PostUdo';
 import Profile from 'community/model/Profile';
-import ProfileUdo from 'community/model/ProfileUdo';
 import { NameValueList } from 'shared/model';
 
 const BASE_URL = '/api/community';
