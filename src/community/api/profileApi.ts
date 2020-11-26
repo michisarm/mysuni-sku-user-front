122cec63f74b6fb211d9fd1817ae54163005fd46
곽은옥 <keunok@ucom.kr>
2020-11-26 12:56:52 +0900

    커뮤니티 프로필 namevalue로 저장

--- src/community/api/profileApi.ts
+++ src/community/api/profileApi.ts
@@ -4,6 +4,7 @@ import PostCdo from 'community/model/PostCdo';
 import PostUdo from 'community/model/PostUdo';
 import Profile from 'community/model/Profile';
 import ProfileUdo from 'community/model/ProfileUdo';
+import { NameValueList } from 'shared/model';
 
 const BASE_URL = '/api/community';
 
@@ -16,10 +17,19 @@ export function findCommunityMyProfile(
 }
 
 export function modifyCommunityProfile(
+  profileNameValues: NameValueList
+): Promise<Profile> {
+  const url = `${BASE_URL}/profiles`;
+  return axiosApi
+    .put<Profile>(url,profileNameValues)
+    .then(response => response && response.data);
+}
+
+/*export function modifyCommunityProfile(
   profileUdo: ProfileUdo
 ): void {
   const url = `${BASE_URL}/profiles`;
   axiosApi
     .put<Profile>(url,profileUdo)
     .then(response => response && response.data);
-}
+}*/
