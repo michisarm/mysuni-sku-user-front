a2689492afc41a2a47427e6923b15686d9ff8a44
안희태 <ahnheetae@ucomp.co.kr>
2020-12-03 21:53:04 +0900

    follow-main list검색

--- src/community/service/useFollowCommunityIntro/utility/requestFollowCommunityIntro.ts
+++ src/community/service/useFollowCommunityIntro/utility/requestFollowCommunityIntro.ts
@@ -61,6 +61,39 @@ export function requestFollowCommunityList(offset: number = 0, limit: number = 2
         communitiesOffset:0,
       });
     } else {
+      const next: FollowCommunityItem[] = [];
+      communities.results.map(followPostList => {
+        next.push(followPostList);
+      });
+      setFollowCommunityIntro({
+        ...followCommunityIntro,
+        communities: next,
+        communitiesTotalCount: communities.totalCount,
+        communitiesOffset: next.length,   
+      });
+    }
+  });
+}
+
+export function requestAppendFollowCommunityList(offset: number = 0, limit: number = 2, nickName: string = "") {
+  followList(offset, limit, nickName).then(communities => {
+    const followCommunityIntro = getFollowCommunityIntro() || {
+      communities: [],
+      posts: [],
+      communitiesTotalCount: 0,
+      communitiesOffset: 0,
+      postsTotalCount: 0,
+      postsOffset:0
+    };
+
+    if (communities === undefined) {
+      setFollowCommunityIntro({
+        ...followCommunityIntro,
+        communities: [],
+        communitiesTotalCount: 0,
+        communitiesOffset:0,
+      });
+    } else {
       const next: FollowCommunityItem[] = [...followCommunityIntro.communities];
       communities.results.map(followPostList => {
         next.push(followPostList);
@@ -74,6 +107,7 @@ export function requestFollowCommunityList(offset: number = 0, limit: number = 2
     }
   });
 }
+
 
 // list 검색부분 별도
 export function requestFollowSearchList(offset: number = 0, limit: number = 5, nickName: string = "") {
