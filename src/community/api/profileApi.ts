78d9121c194c21e0d37d30c30644d0e2aab2d333
곽은옥 <keunok@ucom.kr>
2020-12-03 17:47:08 +0900

    커뮤니티 프로필 팔로우 개발

--- src/community/api/profileApi.ts
+++ src/community/api/profileApi.ts
@@ -35,6 +35,15 @@ export function modifyCommunityProfile(
     .then(response => response && response.data);
 }
 
+export function findCommunityProfile_new(
+  profileId: string
+): Promise<Profile | undefined> {
+  const url = `${BASE_URL}/profiles/flow/${profileId}`;
+  return axiosApi
+    .get<Profile>(url)
+    .then(AxiosReturn);
+}
+
 export function findCommunityProfile(
   profileId: string
 ): Promise<Profile | undefined> {
