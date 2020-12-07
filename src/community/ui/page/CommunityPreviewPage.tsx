14e911e587e95311f8555dd8df6d43f69b5be613
한상희 <hansang1993@ukom.kr>
2020-12-07 13:23:06 +0900

    미리보기 수정

--- src/community/ui/page/CommunityPreviewPage.tsx
+++ src/community/ui/page/CommunityPreviewPage.tsx
@@ -5,6 +5,11 @@ import { Link, matchPath, useLocation, useParams } from 'react-router-dom';
 import { Segment } from "semantic-ui-react";
 import { findPreViewHome } from '../../service/useCommunityHome/requestCommunityHome'
 import defaultHeader from '../../../style/media/bg-ttl-sample-02.png';
+import { requestCommunity, requestCommunityMenus } from "community/service/useCommunityHome/requestCommunity";
+import managerIcon from '../../../style/media/icon-community-manager.png';
+import boardIcon from '../../../style/media/icon-communtiy-menu-board.png';
+import homeArrowIcon from '../../../style/media/icon-community-menu-open.png';
+import homeIcon from '../../../style/media/icon-communtiy-menu-home-on.png';
 
 const CommunityPreviewPage : React.FC = function CommunityPreviewPage({
 
@@ -15,9 +20,12 @@ const CommunityPreviewPage : React.FC = function CommunityPreviewPage({
 
   const { communityId } = useParams<Params>();
   const preview = useCommunityHome();
+  const communityHome = useCommunityHome();
 
   useEffect(()=>{
+    requestCommunity(communityId);
     findPreViewHome(communityId , 1);
+    
   },[]);
 
     return (
@@ -38,12 +46,10 @@ const CommunityPreviewPage : React.FC = function CommunityPreviewPage({
                 )}
                 <div className="community-banner-inner">
                   <div className="community-banner-title">
-                    {preview?.preview?.introduce}
+                    {communityHome?.community?.name}
                   </div>
                   <div className="community-banner-copy">
-                    과정 플랫폼은 여러분들이 동기 구성원들과 서로 소통하고  배우며 성장할 수 있는 공간입니다
-                    적극적으로 참여하여 동기 구성원들과 함께 의미 있고, 소중한 경험을 쌓으시기를 바랍니다
-                    카페 소개 및 다양한 메세지 등을 넣을 수 있어요. 배경이미지 가로사이즈 고정, 세로 사이즈 가변입니다
+                    {preview?.preview?.introduce}
                   </div>
                 </div>
                 </>
@@ -56,6 +62,10 @@ const CommunityPreviewPage : React.FC = function CommunityPreviewPage({
               <div className="home-card-container">
                 <div className="home-card-title">
                   <p>공지사항</p>
+                  <div className="no-cont-wrap">
+                    <i aria-hidden="true" className="icon no-contents80" />
+                  <div className="text">등록된 게시물이 없습니다.</div>
+                  </div>
                   {/* more */}
                   <button className="ui icon button right btn-blue btn-more">
                   more{/* <i aria-hidden="true" className="icon more3"></i> */}
@@ -72,101 +82,54 @@ const CommunityPreviewPage : React.FC = function CommunityPreviewPage({
 
 export default CommunityPreviewPage;
 
-class LnbMenu extends Component {
-    render() {
+const LnbMenu: React.FC = function LnbMenu({ 
+
+}) {
+  interface Params{
+    communityId: string;
+  }
+
+  const { communityId } = useParams<Params>();
+  const communityHome = useCommunityHome();
+
+  useEffect(()=>{
+    requestCommunity(communityId);
+    requestCommunityMenus(communityId);
+  },[]);
+
+  console.log(communityHome);
       return (
         <div className="community-left community-home-left">
           <div className="sub-info-box">
             <div className="commnuity-left-top">
               <div className="community-left-header">
-                <span className="community-badge blue">커뮤니티주제</span>
+                <span className="community-badge blue">{communityHome?.community?.fieldName}</span>
                 <a className="community-share"><span>공유하기</span></a>
               </div>
   
-              <h3>에너지 거래의 현재와 미래</h3>
+              <h3>{communityHome?.community?.name}</h3>
               <div className="community-home-left-span">
                 <div className="community-user-info">
-                  <span><img src=""/><strong>nickname</strong></span>
-                  <span>멤버 <strong>260</strong></span>
+                  <span><img src={managerIcon}/><strong>{communityHome?.community?.managerName}</strong></span>
+                  <span>멤버 <strong>{communityHome?.community?.memberCount}</strong></span>
                 </div>
-                <button className="ui button fix line">가입하기</button>
+                {/* <button className="ui button fix line">가입하기</button> */}
               </div>
             </div>
             <div className="community-home-right-contents">
               <ul>
                 <li>
-                  <a className="act-on">
-                    {/* <img src={home} /> */}
+                  <img src={homeIcon} />
                     HOME
-                    {/* <img src={arrow} className="right-menu-arrow" /> */}
-                  </a>
+                  <img src={homeArrowIcon} className="right-menu-arrow" />
                 </li>
                 <li>
-                  <a>
-                    {/* <img src={leftBoard01} /> */}
+                    <img src={boardIcon} />
                     전체글
-                  </a>
                 </li>
                 <li>
-                  <a>
-                    {/* <img src={leftBoard01} /> */}
+                    <img src={boardIcon} />
                     공지사항
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    {/* <img src={leftBoard01} /> */}
-                    딥 러닝의 역사
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    {/* <img src={leftBoard01} /> */}
-                    딥 러닝의 중요성
-                  </a>
-                </li>
-                <ul>
-                  <li>
-                    <a className="act-on">
-                      {/* <img src={reply} />
-                      <img src={leftBoard01} /> */}
-                      알고리즘
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      {/* <img src={reply} />
-                      <img src={leftBoard04} /> */}
-                      심층 신경망
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      {/* <img src={reply} />
-                      <img src={leftBoard05} /> */}
-                      선호도 조사
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      {/* <img src={reply} />
-                      <img src={leftBoard06} /> */}
-                      강의듣기
-                    </a>
-                  </li>
-                </ul>
-  
-                <li>
-                  <a>
-                    {/* <img src={leftBoard02} /> */}
-                    왜 다시 딥 러닝인가?
-                  </a>
-                </li>
-                <li>
-                  <a>
-                    {/* <img src={leftBoard03} /> */}
-                    딥 러닝과 인간 두뇌
-                  </a>
                 </li>
               </ul>
             </div>
@@ -174,4 +137,3 @@ class LnbMenu extends Component {
         </div>
       );
     }
-  }
