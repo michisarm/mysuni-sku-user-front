e27af49279f9a656ff705e80d9ece13c9af8bc2c
전병길 <jeonbg@ucomp.co.kr>
2020-12-10 11:07:54 +0900

    OpenCommunity 컨텐츠 유무 조건처리 롤백

--- src/community/ui/view/OpenCommunityView.tsx
+++ src/community/ui/view/OpenCommunityView.tsx
@@ -5,17 +5,13 @@ import Segment from 'semantic-ui-react/dist/commonjs/elements/Segment';
 import { Link } from 'react-router-dom';
 import OpenCommunityIntroFieldListContainer from '../logic/OpenCommunityIntro/OpenCommunityIntroFieldListContainer';
 import OpenCommunityIntroCommunityListContainer from '../logic/OpenCommunityIntro/OpenCommunityIntroCommunityListContainer';
-import { useOpenCommunityIntro } from 'community/store/CommunityMainStore';
 
 interface OpenCommunityViewProps {}
 
 const OpenCommunityView: React.FC<OpenCommunityViewProps> = function OpenCommunityView() {
   const contextRef = useRef(null);
-  const openCommunityIntro = useOpenCommunityIntro();
 
   return (
-    <>
-
       <div ref={contextRef}>
         <Sticky context={contextRef} className="tab-menu offset0">
           <div className="cont-inner">
@@ -48,47 +44,13 @@ const OpenCommunityView: React.FC<OpenCommunityViewProps> = function OpenCommuni
             </Menu>
           </div>
         </Sticky>
-        {
-          // 컨텐츠 영역 
-          openCommunityIntro && openCommunityIntro !== undefined &&
-          openCommunityIntro && openCommunityIntro.communities.length !== 0 &&
-          openCommunityIntro && openCommunityIntro.communitiesTotalCount !== 0 ? (
-            
-            // 컨텐츠가 있을 경우
-            <Segment className="full">
-              <OpenCommunityIntroFieldListContainer />
-              <OpenCommunityIntroCommunityListContainer />
-            </Segment>
 
-          ) : (
-            // 컨텐츠가 없을 경우
-            <div className="course-detail-center community-containter">
-              <Segment className="full">
-                <div className="no-cont-wrap">
-                  <i aria-hidden="true" className="icon no-contents80" />
-                  <span className="blind">콘텐츠 없음</span>
-                  <div className="text lms-color-type1">
-                    현재 오픈 커뮤니티가 없습니다.
-                  </div>
-                  <div className="sub-text">
-                    다양한 분야로 만들어진 Open Community에서
-                    <br />
-                    관심 있는 커뮤니티를 찾아 Social Learning을 할 수 있습니다.
-                  </div>
-                  <Link
-                    to="/community/main"
-                    className="ui icon button right btn-blue2"
-                  >
-                    My Community 바로가기
-                    <i aria-hidden="true" className="icon morelink" />
-                  </Link>
-                </div>
-              </Segment>
-            </div> 
-          )
-        }
+        {/* 컨텐츠 영역 */}
+        <Segment className="full">
+          <OpenCommunityIntroFieldListContainer />
+          <OpenCommunityIntroCommunityListContainer />
+        </Segment>
       </div>
-    </>
   );
 };
 
