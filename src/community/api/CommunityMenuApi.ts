0e43dd2b79807800dad14336f685b22cc815d81b
박지훈 <parkjihoo.com@gmail.com>
2021-01-06 16:24:19 +0900

    커뮤니티 멤버 api 주소 수정
    커뮤니티 멤버 api 주소 수정
    
    (cherry picked from commit 0b48eec0b133458632a489fc4d5b1eef059a7a5f)

--- src/community/api/CommunityMenuApi.ts
+++ src/community/api/CommunityMenuApi.ts
@@ -18,7 +18,7 @@ export function findMembers(
   return (
     axios
     // .get(`${BASE_URL}/memberviews?communityId=${communityId}&offset=${pageNum}&limit=${limit}`)
-    .get(`${BASE_URL}/communities/${communityId}/members/memberviews/all`, {
+    .get(`${BASE_URL}/memberviews/${communityId}`, {
       params: searchBox,
     })
 
