5fc8553f09db0ad5b6b958eeeef1374ebaae7cbf
09985 <dhchoi@sk.com>
2021-01-29 21:21:42 +0900

    test

--- src/lecture/detail/ui/view/LectureTestView/LectureTestView.tsx
+++ src/lecture/detail/ui/view/LectureTestView/LectureTestView.tsx
@@ -132,6 +132,8 @@ const LectureTestView: React.FC<LectureTestViewProps> = function LectureTestView
                 break;
               case 'Passed':
               case 'TestPassed':
+                const course = getActiveCourseStructureItem();
+                const program = getActiveProgramStructureItem();
                 if (
                   course?.survey !== undefined ||
                   program?.survey !== undefined
