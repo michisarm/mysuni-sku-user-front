1ef9708a975bd0bb951f860116ed88b6e125ce35
winho_90 <>
2021-02-18 14:07:55 +0900

    퍼스널보드 작업중
    (cherry picked from commit 08acdad5e346edf9a7c37a60baf43679c133b30f)

--- src/main/sub/PersonalBoard/ui/logic/PersonalBoardContainer.tsx
+++ src/main/sub/PersonalBoard/ui/logic/PersonalBoardContainer.tsx
@@ -1,29 +1,42 @@
+import { mobxHelper } from '@nara.platform/accent';
+import { inject, observer } from 'mobx-react';
 import MyLearningSummaryModel from 'myTraining/model/MyLearningSummaryModel';
+import { MyLearningSummaryService } from 'myTraining/stores';
 import React, { useEffect, useRef, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { RouteComponentProps, useHistory, withRouter } from 'react-router-dom';
 
 
-interface PersonalBoardProps {
-  myLearningSummary : MyLearningSummaryModel
+interface Props extends RouteComponentProps {
+  myLearningSummaryService?: MyLearningSummaryService;
 }
 
-const PersonalBoardContainer: React.FC<PersonalBoardProps> = function PersonalBoard({
-  myLearningSummary
-}) {
+function PersonalBoardContainer(props: Props){
 
-  console.log('myLearningSummary', myLearningSummary)
-const history = useHistory();
+  const { myLearningSummaryService } = props;
+  const { totalMyLearningSummary2 } = myLearningSummaryService!;
+  const history = useHistory();
+
+  console.log('totalMyLearningSummary2', totalMyLearningSummary2)
 
 return (
-<>
-<span>1234</span>
-뱃지<br/>
-학습시간<br/>
-학습시간상세<br/>
-영역별 학습시간<br/>
-우리회사 인기코스<br/>
-</>
+  <>
+    <span>1234</span>
+    뱃지<br/>
+    학습시간<br/>
+    학습시간상세<br/>
+    영역별 학습시간<br/>
+    우리회사 인기코스<br/>
+  </>
 )
 }
 
-export default PersonalBoardContainer;
+// export default PersonalBoardContainer;
+
+export default inject(
+  mobxHelper.injectFrom(
+    'profile.skProfileService',
+    'myTraining.myLearningSummaryService',
+    'myTraining.myTrainingService',
+    'badge.badgeService'
+  )
+)(withRouter(observer(PersonalBoardContainer)));
