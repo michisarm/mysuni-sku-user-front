75f73e62dc5dddfff8946808af164b9867bc5a4a
김수경 <68100360+devsooki@users.noreply.github.com>
2021-02-26 13:08:18 +0900

    주관식 퍼블리싱 작업

--- src/lecture/detail/ui/view/LectureSurveyView/LectureSurveySummaryEssayView.tsx
+++ src/lecture/detail/ui/view/LectureSurveyView/LectureSurveySummaryEssayView.tsx
@@ -31,35 +31,45 @@ const LectureSurveyEssayView: React.FC<LectureSurveyEssayViewProps> = function L
 
   return (
     <LectureSurveyChoiceLayout {...lectureSurveyItem}>
-      {lectureSurveyAnswerItem && lectureSurveyAnswerItem.sentence}
-      <br />
-
-      {lectureSurveyItem.visible !== undefined &&
-        lectureSurveyItem.visible === true &&
-        answerList
-          ?.filter(f => f.answerItemType === 'Essay')
-          .map(answer =>
-            answer.summaryItems.sentences?.map((result, index) => (
-              <>{index >= 0 && index <= number ? <div>{result}</div> : ''}</>
-            ))
-          )}
-
-      {lectureSurveyItem.visible !== undefined &&
-      lectureSurveyItem.visible === true &&
-      lastIndex - 1 > number ? (
-        <div>
-          <Button icon className="left moreview" onClick={setCheckNumber}>
-            <Icon className="moreview" />
-            더보기 ({lastIndex - number - 1}개)
-          </Button>
+      <div className="course-radio-survey-new">
+        <div className="course-survey-list">
+          <p className="improve-text">
+            {lectureSurveyAnswerItem && lectureSurveyAnswerItem.sentence}
+          </p>
+          <ul className="improve-list">
+            {lectureSurveyItem.visible !== undefined &&
+              lectureSurveyItem.visible === true &&
+              answerList
+                ?.filter(f => f.answerItemType === 'Essay')
+                .map(answer =>
+                  answer.summaryItems.sentences?.map((result, index) => (
+                    <>
+                      {index >= 0 && index <= number ? <li>{result}</li> : ''}
+                    </>
+                  ))
+                )}
+            <li className="improve-list-more">
+              {lectureSurveyItem.visible !== undefined &&
+              lectureSurveyItem.visible === true &&
+              lastIndex - 1 > number ? (
+                <>
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/all/survey-list-more.png`}
+                  />
+                  <span onClick={setCheckNumber}>
+                    더보기 ({lastIndex - number - 1}개)
+                  </span>
+                </>
+              ) : (
+                ''
+              )}
+            </li>
+          </ul>
         </div>
-      ) : (
-        ''
-      )}
-
-      {lectureSurveyItem.visible !== undefined &&
-        lectureSurveyItem.visible !== true &&
-        '해당 문항은 비공개 처리되어 답변이 조회되지 않습니다.'}
+        {lectureSurveyItem.visible !== undefined &&
+          lectureSurveyItem.visible !== true &&
+          '해당 문항은 비공개 처리되어 답변이 조회되지 않습니다.'}
+      </div>
     </LectureSurveyChoiceLayout>
   );
 };
