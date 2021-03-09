919fbef69ddd6783bdd630e26a1901160e2a1a2a
박은호 <wagno@ucom.kr>
2021-03-09 14:39:04 +0900

    Chart 적용 - cherry pick

--- src/main/sub/PersonalBoard/ui/view/LearningTimeDetailView.tsx
+++ src/main/sub/PersonalBoard/ui/view/LearningTimeDetailView.tsx
@@ -1,45 +1,37 @@
 import moment from 'moment';
 import { MyLearningSummaryModal } from 'myTraining';
-import React, { useEffect, useState } from 'react';
-import { Button } from 'semantic-ui-react';
-import { timeToHourMinutePaddingFormat } from 'shared/helper/dateTimeHelper';
+import { PersonalBoardDoughnutChartView } from '@sku/chart';
+import React, { useMemo } from 'react';
 import { useLearningTimeDetailItem } from '../../store/PersonalBoardStore';
 
-interface propsType {
-  label: string,
-  value: number
-}
-const LearningTimeDetailView: React.FC = function LearningTimeDetailView({
-}) {
-  const badgeLearningTimeDetailItem = useLearningTimeDetailItem()
-  const [chartProps, setChartProps] = useState<propsType[]>()
+type ChartDataItem = { label: string; value: number };
 
-  const LearningTimeDetailView: React.FC = function LearningTimeDetailView({ }) {
-    const badgeLearningTimeDetailItem = useLearningTimeDetailItem();
-    const datas: ChartDataItem[] = useMemo<ChartDataItem[]>(
-      () => [
-        {
-          label: 'mySUNI',
-          value: badgeLearningTimeDetailItem?.suniLearningTime || 0,
-        },
-        {
-          label: '관계사',
-          value: badgeLearningTimeDetailItem?.displayMyCompanyLearningTime || 0,
-        },
-        {
-          label: '강의시간',
-          value: badgeLearningTimeDetailItem?.totalCollegeTime || 0,
-        },
-        {
-          label: '개인학습',
-          value: badgeLearningTimeDetailItem?.aplAllowTime || 0,
-        },
-      ],
-      [badgeLearningTimeDetailItem]
-    );
-    return (
-      <>
-        {/* {badgeLearningTimeDetailItem && (
+const LearningTimeDetailView: React.FC = function LearningTimeDetailView({ }) {
+  const badgeLearningTimeDetailItem = useLearningTimeDetailItem();
+  const datas: ChartDataItem[] = useMemo<ChartDataItem[]>(
+    () => [
+      {
+        label: 'mySUNI',
+        value: badgeLearningTimeDetailItem?.suniLearningTime || 0,
+      },
+      {
+        label: '관계사',
+        value: badgeLearningTimeDetailItem?.displayMyCompanyLearningTime || 0,
+      },
+      {
+        label: '강의시간',
+        value: badgeLearningTimeDetailItem?.totalCollegeTime || 0,
+      },
+      {
+        label: '개인학습',
+        value: badgeLearningTimeDetailItem?.aplAllowTime || 0,
+      },
+    ],
+    [badgeLearningTimeDetailItem]
+  );
+  return (
+    <>
+      {/* {badgeLearningTimeDetailItem && (
     <div style={{border: '2px solid', borderColor: 'blue'}}>
       <span>학습시간상세 pie chart</span><br/>
       <span>mySUNI{timeToHourMinutePaddingFormat(badgeLearningTimeDetailItem.suniLearningTime)}</span><br/>
@@ -48,77 +40,107 @@ const LearningTimeDetailView: React.FC = function LearningTimeDetailView({
       <span>개인학습{timeToHourMinutePaddingFormat(badgeLearningTimeDetailItem.totalCollegeTime)}</span><br/>
       <span>전체시간{badgeLearningTimeDetailItem.suniLearningTime+badgeLearningTimeDetailItem.displayMyCompanyLearningTime+badgeLearningTimeDetailItem.aplAllowTime+badgeLearningTimeDetailItem.totalCollegeTime}</span><br/>
     </div> */}
-        {badgeLearningTimeDetailItem && (
-          <MyLearningSummaryModal
-            trigger={(
-              <a>
-                <div className="personal-card-item">
-                  <div className="card-item-tit mb23">
-                    <h3>학습 시간 상세</h3>
-                    <span>{moment().year()}년 기준</span>
-                  </div>
-                  <div className="card-item-con sty2">
-                    <div className="item-con-box">
-                      <div className="item-con-left detail">
-                        파이 차트 들어가야한다.
-                  </div>
-                      <div className="item-con-right detail">
-                        <div className="card-gauge-bar">
-                          <div className="gauge-number sv">
-                            <em className="col-con">mySUNI</em>
-                            <div>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.suniLearningTime / 60)}<em>h</em>
-                              </strong>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.suniLearningTime % 60)}<em>m</em>
-                              </strong>
-                            </div>
+      {badgeLearningTimeDetailItem && (
+        <MyLearningSummaryModal
+          trigger={
+            <a>
+              <div className="personal-card-item">
+                <div className="card-item-tit mb23">
+                  <h3>학습 시간 상세</h3>
+                  <span>{moment().year()}년 기준</span>
+                </div>
+                <div className="card-item-con sty2">
+                  <div className="item-con-box">
+                    <div className="item-con-left detail">
+                      <PersonalBoardDoughnutChartView datas={datas} />
+                    </div>
+                    <div className="item-con-right detail">
+                      <div className="card-gauge-bar">
+                        <div className="gauge-number sv">
+                          <em className="col-con">mySUNI</em>
+                          <div>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.suniLearningTime /
+                                60
+                              )}
+                              <em>h</em>
+                            </strong>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.suniLearningTime %
+                                60
+                              )}
+                              <em>m</em>
+                            </strong>
                           </div>
-                          <div className="gauge-number mana">
-                            <em className="col-con">관계사</em>
-                            <div>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.displayMyCompanyLearningTime / 60)}<em>h</em>
-                              </strong>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.displayMyCompanyLearningTime % 60)}<em>m</em>
-                              </strong>
-                            </div>
+                        </div>
+                        <div className="gauge-number mana">
+                          <em className="col-con">관계사</em>
+                          <div>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.displayMyCompanyLearningTime /
+                                60
+                              )}
+                              <em>h</em>
+                            </strong>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.displayMyCompanyLearningTime %
+                                60
+                              )}
+                              <em>m</em>
+                            </strong>
                           </div>
-                          <div className="gauge-number semi">
-                            <em className="col-con">강의시간</em>
-                            <div>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.totalCollegeTime / 60)}<em>h</em>
-                              </strong>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.totalCollegeTime % 60)}<em>m</em>
-                              </strong>
-                            </div>
+                        </div>
+                        <div className="gauge-number semi">
+                          <em className="col-con">강의시간</em>
+                          <div>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.totalCollegeTime /
+                                60
+                              )}
+                              <em>h</em>
+                            </strong>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.totalCollegeTime %
+                                60
+                              )}
+                              <em>m</em>
+                            </strong>
                           </div>
-                          <div className="gauge-number stu">
-                            <em className="col-con">개인학습</em>
-                            <div>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.aplAllowTime / 60)}<em>h</em>
-                              </strong>
-                              <strong>
-                                {Math.floor(badgeLearningTimeDetailItem.aplAllowTime % 60)}<em>m</em>
-                              </strong>
-                            </div>
+                        </div>
+                        <div className="gauge-number stu">
+                          <em className="col-con">개인학습</em>
+                          <div>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.aplAllowTime / 60
+                              )}
+                              <em>h</em>
+                            </strong>
+                            <strong>
+                              {Math.floor(
+                                badgeLearningTimeDetailItem.aplAllowTime % 60
+                              )}
+                              <em>m</em>
+                            </strong>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </a>
-            )}
-          />
-        )}
-      </>
-    );
-  };
+              </div>
+            </a>
+          }
+        />
+      )}
+    </>
+  );
+};
 
-  export default LearningTimeDetailView;
+export default LearningTimeDetailView;
