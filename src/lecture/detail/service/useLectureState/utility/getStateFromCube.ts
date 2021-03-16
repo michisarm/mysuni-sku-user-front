5069889cc813a8a30f033c1bc109e822fedf5a95
곽은옥 <keunok@ucom.kr>
2021-03-16 16:03:38 +0900

    Classroom,Elearning 수강신청 알림 문구 수정

--- src/lecture/detail/service/useLectureState/utility/getStateFromCube.ts
+++ src/lecture/detail/service/useLectureState/utility/getStateFromCube.ts
@@ -168,16 +168,16 @@ async function mClassroomSubmit(
     classroom.freeOfCharge.approvalProcess
   ) {
     const messageStr =
-      '본 과정은 승인권자(본인리더 or HR담당자)가 승인 후 신청완료 됩니다. <br> 승인대기중/승인완료 된 과정은<br>&#39;Learning>학습예정&#39;에서 확인하실 수 있습니다.';
+      '본 과정은 승인권자가 승인 후 신청완료 됩니다. <br> 승인대기중/승인완료 된 과정은<br>&#39;Learning>학습예정&#39;에서 확인하실 수 있습니다.';
     reactAlert({ title: '알림', message: messageStr });
   } else if (!classroom.freeOfCharge.approvalProcess) {
     const messageStr =
       '본 과정 신청이 완료 되었습니다. <br> &#39;Learning>학습예정&#39;에서 확인하실 수 있습니다.';
     reactAlert({ title: '알림', message: messageStr });
   } else {
-    const messageStr =
-      '본 과정은 승인권자가 승인 후 신청완료 됩니다. <br> 승인대기중/승인완료 된 과정은 <br> &#39;Learning>학습예정&#39;에서 확인하실 수 있습니다.';
-    reactAlert({ title: '알림', message: messageStr });
+    //const messageStr =
+    //  '본 과정은 승인권자가 승인 후 신청완료 됩니다. <br> 승인대기중/승인완료 된 과정은 <br> &#39;Learning>학습예정&#39;에서 확인하실 수 있습니다.';
+    //reactAlert({ title: '알림', message: messageStr });
   }
 }
 
