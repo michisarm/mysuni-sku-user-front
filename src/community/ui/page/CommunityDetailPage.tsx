cf6a9dcf2292d9b1fbd7e2cadecf3f8bd4c46e92
박상태 <recyon@gmail.com>
2021-03-19 13:11:52 +0900

    탈퇴하기 상태 수정

--- src/community/ui/page/CommunityDetailPage.tsx
+++ src/community/ui/page/CommunityDetailPage.tsx
@@ -235,9 +235,9 @@ const CommunityDetailPage: React.FC<Post> = function CommunityDetailPage({
           type="button"
           className="ui button sece_btn"
           onClick={drawCommunity}
-          // disabled={
-          //   managerId == SkProfileService.instance.skProfile.id ? true : false
-          // }
+          disabled={
+            communityHome.community.approved != 'APPROVED' ? true : false
+          }
         >
           <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAGKADAAQAAAABAAAAGAAAAADiNXWtAAABdUlEQVRIDe2UMU7DQBBF568pECdAgYJbUBIKmiAOQMENEpN0WEIiBSJ0GOyKhoqWjtARuAQSBYcgQpFAiYe1hezs2uu1TMpESjQ7f/z+7qwzRMuPpQPQ9WPP354xHxKhAXCqM4NJ8F140XvSnylbr8yLHe9mZ8bRCIQfAn0wE2e65JPYyNbVIsUAFB0R8RetOptB3x1XQ5RXKQbMvC6b8h5WgHc8/1nW7wJOMxi4ryYbYRJseXknCZQpGsatNdXXNkiBzGtlJkqL0ofmgrbnPxJzay71F0ZZKjZBcpKW3i77CQrhGTuNDCexG6SECkGByWINCvawWANgAhLKPdgNgGHBxvKpAnhcZH2LwkF3P08jap9cnxFF/UQzwGPNfoKEUPJTAv+XgZy0yb9X77m+lXQcx4KcL7dygh6gIbYC1/3Wi+usFYPu6VVzOqURgLEc1G95oAiDS/c+nzdnlDvwz3svIGdPwh/kVP3Uv3B4YkYtlZod+AX115HSsSePVgAAAABJRU5ErkJggg==" />
           <span>탈퇴하기</span>
