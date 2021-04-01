b3dd756743b83cd61d76b2d0e8a137999f64c575
안희태 <ahnheetae@ucomp.co.kr>
2021-04-01 19:18:26 +0900

    releases 청크 수정

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,8 @@ import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
 // 2021-03-12
-import './style/css/2.0d88502f.chunk.css';
-import './style/css/main.8ffda00b.chunk.css';
+import './style/css/2.bf2d4321.chunk.css';
+import './style/css/main.76014ef3.chunk.css';
 
 initializeBody();
 
