82e71645da328e791f0b5e931ca38d9eb9d8de82
안희태 <ahnheetae@ucomp.co.kr>
2021-04-01 11:05:35 +0900

    청크업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import 'react-quill/dist/quill.snow.css';
 
 // 2021-03-12
 import './style/css/2.0d88502f.chunk.css';
-import './style/css/main.6052f01a.chunk.css';
+import './style/css/main.9507acc4.chunk.css';
 
 initializeBody();
 
