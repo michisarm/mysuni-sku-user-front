565e82d25192ad2f0a45a1135e219d2c3a180dd0
김승호 <winho_90@ucom.kr>
2021-04-05 14:29:39 +0900

    출석 이벤트

--- src/main/sub/PersonalBoard/ui/view/AttendanceModal.tsx
+++ src/main/sub/PersonalBoard/ui/view/AttendanceModal.tsx
@@ -1,6 +1,8 @@
 import React, {useCallback, useEffect, useState} from 'react'
 import { Modal } from 'semantic-ui-react'
 import { useHistory, Link } from 'react-router-dom';
+
+import moment from 'moment';
 import momentTZ from 'moment-timezone';
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
@@ -32,13 +34,10 @@ const AttendanceModal:React.FC<Props> = ({
     if(AttendCountItem === undefined) {
       return
     }
+    console.log('frontend time', momentTZ().tz('Asia/Seoul').format('YYYY-MM-DD hh:mm:ss'))
+    console.log('AttendCountItem', AttendCountItem)
     //출석 체크한 날이 오늘 날짜보다 이전의 날짜 여야 클릭 가능한 버튼 표시
-      const flag = AttendCountItem.find((element: { attendDate: any; }) => {
-      console.log('frontend time', momentTZ().tz('Asia/Seoul').format('YYYY-MM-DD hh:mm:ss'))
-      console.log('element.attendDate', element.attendDate)
-      return element.attendDate === momentTZ().tz('Asia/Seoul').format('YYYY-MM-DD hh:mm:ss')
-    })
-
+    const flag = AttendCountItem.find((element: { attendDate: any; }) => element.attendDate === moment().format('YYYY-MM-DD'))
     setAttendFlag(flag)
   }, [AttendCountItem])
 
