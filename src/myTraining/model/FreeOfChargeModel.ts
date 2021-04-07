dcf26e2fc1578b1b46f936cf00722e933da11a51
곽은옥 <keunok@ucom.kr>
2021-04-07 09:24:47 +0900

    Classroom,Elearning 수강신청 api 전달 항목 수정

--- src/myTraining/model/FreeOfChargeModel.ts
+++ src/myTraining/model/FreeOfChargeModel.ts
@@ -2,9 +2,10 @@ import { decorate, observable } from 'mobx';
 
 export class FreeOfChargeModel {
   //
-  freeOfCharge: boolean = false;    //유무료
-  chargeAmount: number = 0;       // 비용
+  freeOfCharge: boolean = false; //유무료
+  chargeAmount: number = 0; // 비용
   approvalProcess: boolean = true; //승인프로세스 여부
+  sendmailFlag: boolean = false; // 이메일 발송 여부
 
   constructor(freeOfCharge?: FreeOfChargeModel) {
     if (freeOfCharge) {
@@ -17,5 +18,5 @@ decorate(FreeOfChargeModel, {
   freeOfCharge: observable,
   chargeAmount: observable,
   approvalProcess: observable,
+  sendmailFlag: observable,
 });
-
