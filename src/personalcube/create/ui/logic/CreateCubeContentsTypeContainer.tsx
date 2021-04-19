75aa403a43d9abb5f0306786de3580e9044a419e
김동구 <bbonggu92@gmail.com>
2021-04-19 10:04:51 +0900

    [update] createPage 타입별 form view 수정

--- src/personalcube/create/ui/logic/CreateCubeContentsTypeContainer.tsx
+++ src/personalcube/create/ui/logic/CreateCubeContentsTypeContainer.tsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { observer } from 'mobx-react';
 import CreateCubeService from '../../../personalcube/present/logic/CreateCubeService';
-import CreateWebPageTypeView from '../view/CreateWebPageTypeView';
-import CreateDocumentTypeView from '../view/CreateDocumentTypeView';
+import WebPageFormView from '../view/WebPageFormView';
 import CreateVideoTypeView from '../view/CreateVideoTypeViewV2';
 import CreateAudioTypeView from '../view/CreateAudioTypeViewV2';
+import ReferenceFileFormView from '../view/ReferenceFileFormView';
+import DocumentFormView from '../view/DocumentFormView';
 
 function CreateCubeContentsTypeContainer() {
   const { cubeSdo } = CreateCubeService.instance;
 
+  const onChangeReferenceFile = useCallback((id: string) => {
+    CreateCubeService.instance.changeCubeSdoProps('fileBoxId', id);
+  }, []);
+  
+  const onChangeEducationFile = useCallback((id: string) => {
+    CreateCubeService.instance.changeCubeSdoProps(
+      'materialSdo.officeWebSdo.fileBoxId',
+      id
+    );
+  }, []);
+
+  const onChangeWebPageUrl = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    CreateCubeService.instance.changeCubeSdoProps('materialSdo.officeWebSdo.webPageUrl', e.target.value);
+  }, []);
+
   return (
     <>
-      {cubeSdo.type === 'WebPage' && <CreateWebPageTypeView />}
-      {cubeSdo.type === 'Documents' && <CreateDocumentTypeView />}
+      {cubeSdo.type === 'WebPage' && (
+          <WebPageFormView
+            webPageUrl={cubeSdo.materialSdo?.officeWebSdo.webPageUrl} 
+            onChangeUrl={onChangeWebPageUrl}
+          />
+        )
+      }
+      {cubeSdo.type === 'Documents' && 
+        (
+          <DocumentFormView 
+            fileBoxId={cubeSdo.materialSdo?.officeWebSdo.fileBoxId || ''}
+            onChangeFile={onChangeEducationFile}
+          />
+        )
+      }
       {cubeSdo.type === 'Video' && <CreateVideoTypeView />}
       {cubeSdo.type === 'Audio' && <CreateAudioTypeView />}
+      <ReferenceFileFormView 
+        fileBoxId={cubeSdo.fileBoxId || ''}
+        onChangeFile={onChangeReferenceFile}
+      />
     </>
   );
 }
