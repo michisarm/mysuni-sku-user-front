abc461a3c739081cd2220974a6504a6641141144
박은호 <eunho@ninestory.kr>
2021-04-23 11:24:41 +0900

    Panopto Demo 로 변경

--- src/lecture/detail/service/PanoptoEmbedPlayer.ts
+++ src/lecture/detail/service/PanoptoEmbedPlayer.ts
@@ -165,7 +165,7 @@ function createPanoptoEmbedPlayer() {
       //This is the URL of your Panopto site
       //https://sku.ap.panopto.com/Panopto/Pages/Auth/Login.aspx?support=true
       // serverName: 'sku.ap.panopto.com/Panopto/Pages/BrowserNotSupported.aspx?ReturnUrl=',
-      serverName: 'sku.ap.panopto.com',
+      serverName: 'sku-demo.ap.panopto.com',
       sessionId: panoptoSessionId,
       videoParams: {
         // Optional parameters
