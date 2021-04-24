06e0a36e37225c497eb3a52b266662be4d577dd8
황종옥 <ockkk@whatvita.kr>
2021-04-24 21:37:43 +0900

    recommend channel 채널 이름 나오지 않는 부분 수정

--- src/lecture/recommend/ui/view/RecommendCardRomView.tsx
+++ src/lecture/recommend/ui/view/RecommendCardRomView.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SkProfileService } from '../../../../profile/stores';
-import { getChannelName } from '../../../../shared/service/useCollege/useRequestCollege';
+// import { getChannelName } from '../../../../shared/service/useCollege/useRequestCollege';
 import { RecommendCardRom } from '../../../model/RecommendCardRom';
+import { getCheckableChannelsStore } from '../../../../shared/store/CheckableChannelsStore';
 import CardView from '../../../shared/Lecture/ui/view/CardVIew';
+import { find } from 'lodash';
+import { NoSuchContentPanel } from 'shared';
 
 export function RecommendCardRomView(props: RecommendCardRom) {
   const {
@@ -12,6 +15,13 @@ export function RecommendCardRomView(props: RecommendCardRom) {
     totalCardCount,
     cardWithRelatedCountRoms,
   } = props;
+  const checkableChannels = getCheckableChannelsStore();
+
+  const getChannelName = () => {
+    const filterChannelName = find(checkableChannels, { id: channelId });
+
+    return filterChannelName?.name;
+  };
 
   const getCardCount = () => {
     if (totalCardCount !== undefined && totalCardCount >= 0) {
@@ -24,35 +34,44 @@ export function RecommendCardRomView(props: RecommendCardRom) {
   return (
     <>
       <div className="section-head">
-        <span className="channel">{getChannelName(channelId)}</span>
+        <span className="channel">{getChannelName()}</span>
         {`채널에서 ${SkProfileService.instance.profileMemberName}님께 추천하는 과정입니다.`}
         <span className="channel">{`(${getCardCount()})`}</span>
-        <div className="right">
-          <Link to={`/lecture/recommend/channel/${channelId}`}>
-            <button className="ui icon button right btn-blue">
-              View all
-              <i aria-hidden="true" className="icon morelink" />
-            </button>
-          </Link>
-        </div>
+        {Array.isArray(cardWithRelatedCountRoms) &&
+          cardWithRelatedCountRoms.length > 0 && (
+            <div className="right">
+              <Link to={`/lecture/recommend/channel/${channelId}`}>
+                <button className="ui icon button right btn-blue">
+                  View all
+                  <i aria-hidden="true" className="icon morelink" />
+                </button>
+              </Link>
+            </div>
+          )}
       </div>
       <div className="scrolling">
         <ul className="belt">
           {Array.isArray(cardWithRelatedCountRoms) &&
-            cardWithRelatedCountRoms.map(({ card, cardRelatedCount }) => {
-              return (
-                <li>
-                  <div className="ui cards box-cards">
-                    <CardView
-                      key={card.id}
-                      cardId={card.id}
-                      {...card}
-                      {...cardRelatedCount}
-                    />
-                  </div>
-                </li>
-              );
-            })}
+            (cardWithRelatedCountRoms.length ? (
+              cardWithRelatedCountRoms.map(({ card, cardRelatedCount }) => {
+                return (
+                  <li>
+                    <div className="ui cards box-cards">
+                      <CardView
+                        key={card.id}
+                        cardId={card.id}
+                        {...card}
+                        {...cardRelatedCount}
+                      />
+                    </div>
+                  </li>
+                );
+              })
+            ) : (
+              <NoSuchContentPanel
+                message={`${getChannelName()}채널에 해당하는 추천 학습과정이 없습니다.`}
+              />
+            ))}
         </ul>
       </div>
     </>
