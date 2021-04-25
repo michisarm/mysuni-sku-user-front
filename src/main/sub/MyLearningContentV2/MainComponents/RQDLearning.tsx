29fa71174e260a3b052f08e4df693ace3a90f3a3
황종옥 <ockkk@whatvita.kr>
2021-04-25 18:55:42 +0900

    Deep Change 권장 과정 section 적용

---
+++ src/main/sub/MyLearningContentV2/MainComponents/RQDLearning.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { Button, Icon } from 'semantic-ui-react';
+import { NoSuchContentPanel } from 'shared';
+import { Lecture } from 'lecture';
+import { ContentWrapper } from '../MyLearningContentElementsView';
+import ReactGA from 'react-ga';
+import {
+  findRequiredLearning,
+  findCardFromCardBundle,
+} from '../../../../lecture/detail/api/cardApi';
+import { CardWithCardRealtedCount } from '../../../../lecture/model/CardWithCardRealtedCount';
+import CardView from '../../../../lecture/shared/Lecture/ui/view/CardVIew';
+import CardGroup, {
+  GroupType,
+} from '../../../../lecture/shared/Lecture/sub/CardGroup';
+import { Area } from 'tracker/model';
+
+interface Props extends RouteComponentProps {
+  profileMemberName?: string;
+}
+
+const RQDLearning: React.FC<Props> = function RQDLearning({ history }) {
+  const [cardList, setCardList] = useState<CardWithCardRealtedCount[]>([]);
+
+  const fetchCardList = async () => {
+    const cardIds = await findRequiredLearning();
+
+    if (cardIds) {
+      const cardList = await findCardFromCardBundle(cardIds, 8, false);
+
+      if (cardList !== undefined) {
+        setCardList(cardList);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchCardList();
+  }, []);
+
+  const onViewAll = () => {
+    window.sessionStorage.setItem('from_main', 'TRUE');
+
+    history.push(`/my-training/learning/Required/pages/1`);
+
+    // react-ga event
+    ReactGA.event({
+      category: '권장 과정',
+      action: 'Click',
+      label: '권장 과정 전체보기',
+    });
+  };
+
+  if (cardList.length === 0) {
+    return null;
+  }
+
+  return (
+    <ContentWrapper dataArea={Area.MAIN_REQUIRED}>
+      <div className="section-head">
+        <strong>Deep Change를 위한 권장과정</strong>
+        <div className="right">
+          <Button icon className="right btn-blue" onClick={onViewAll}>
+            View all <Icon className="morelink" />
+          </Button>
+        </div>
+      </div>
+      <Lecture.Group type={Lecture.GroupType.Line}>
+        {cardList.map((item, i) => {
+          const { card, cardRelatedCount } = item;
+          return (
+            <li key={i}>
+              <CardGroup type={GroupType.Box}>
+                <CardView
+                  cardId={item.card.id}
+                  permittedCinerooms={card.permittedCinerooms}
+                  learningTime={card.learningTime}
+                  additionalLearningTime={card.additionalLearningTime}
+                  thumbImagePath={card.thumbImagePath}
+                  mainCategory={card.mainCategory}
+                  name={card.name}
+                  stampCount={card.stampCount}
+                  simpleDescription={card.simpleDescription}
+                  type={card.type}
+                  passedStudentCount={cardRelatedCount.passedStudentCount}
+                  starCount={cardRelatedCount.starCount}
+                />
+              </CardGroup>
+            </li>
+          );
+        })}
+      </Lecture.Group>
+    </ContentWrapper>
+  );
+};
+
+export default withRouter(RQDLearning);
