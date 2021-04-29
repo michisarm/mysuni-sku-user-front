8f20fc8f412933f4d465a61e099cf2b01c7b8801
박은호 <eunho@ninestory.kr>
2021-04-29 15:03:33 +0900

    챕터 구분 선 추가

--- src/lecture/detail/ui/view/LectureStructureView/ProgramLectureStructureView.tsx
+++ src/lecture/detail/ui/view/LectureStructureView/ProgramLectureStructureView.tsx
@@ -134,12 +134,6 @@ const ProgramLectureStructureView: React.FC<ProgramLectureStructureViewProps> = 
         }
         if (item.type === 'DISCUSSION' && item.parentId !== undefined) {
           const discussion = item as LectureStructureDiscussionItem;
-          if (
-            discussion.parentId !== undefined &&
-            collapsedIds.includes(discussion.parentId)
-          ) {
-            return null;
-          }
           return (
             <Fragment>
               {(discussion.parentId === undefined ||
