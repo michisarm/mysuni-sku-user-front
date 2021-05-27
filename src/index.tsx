483dfdae05500e1987caa5e96af368318d925ed5
황종옥 <ockkk@whatvita.kr>
2021-05-27 17:25:43 +0900

    클래스룸 퍼블리셔 변경 사항 적용, 강사 중복 제거 로직 추가

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import 'react-quill/dist/quill.snow.css';
 
 // 2021-03-12
 import './style/css/2.06f1e362.chunk.css';
-import './style/css/main.57d160aa.chunk.css';
+import './style/css/main.7d723a16.chunk.css';
 
 initializeBody();
 
