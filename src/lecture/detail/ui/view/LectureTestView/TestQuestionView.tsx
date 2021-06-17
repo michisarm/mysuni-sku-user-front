4fe5016a10d03a388398320dd023bd3bff28cc47
곽은옥 <keunok@ucom.kr>
2021-06-17 16:41:25 +0900

    Test 이미지 노출

--- src/lecture/detail/ui/view/LectureTestView/TestQuestionView.tsx
+++ src/lecture/detail/ui/view/LectureTestView/TestQuestionView.tsx
@@ -8,11 +8,11 @@ import {
   setLectureTestAnswerItem,
 } from 'lecture/detail/store/LectureTestStore';
 import LearningState from 'lecture/detail/model/LearningState';
-import { EssayScore } from 'lecture/detail/model/GradeSheet';
 import LectureParams from '../../../viewModel/LectureParams';
 import { getActiveStructureItem } from '../../../utility/lectureStructureHelper';
 import ExamQuestion from '../../../model/ExamQuestion';
 import { Button, Icon } from 'semantic-ui-react';
+import Image from 'shared/components/Image';
 
 interface TestQuestionViewProps {
   question: ExamQuestion;
@@ -33,7 +33,7 @@ function setAnswer(questionNo: number, value: string) {
   if (answerItem === undefined) {
     return;
   }
-  const nextAnswers = answerItem.answers.map(answer => {
+  const nextAnswers = answerItem.answers.map((answer) => {
     if (questionNo === answer.sequence) {
       return { ...answer, answer: value };
     }
@@ -46,209 +46,212 @@ function setAnswer(questionNo: number, value: string) {
   setLectureTestAnswerItem(nextAnswerItem);
 }
 
-const TestQuestionView: React.FC<TestQuestionViewProps> = function TestQuestionView({
-  question,
-  indexNo,
-  answer,
-  answerResult,
-  readOnly,
-  learningState,
-  submitOk,
-  setSubmitOk,
-  dataLoadTime,
-  params,
-  obtainedScore,
-}) {
-  let questionClassName = ' course-radio-survey ';
-  if (
-    question.questionType === 'ShortAnswer' ||
-    question.questionType === 'Essay'
-  ) {
-    questionClassName += ' survey-radio-img ';
-  }
-  if (
-    question.questionType === 'SingleChoice' ||
-    question.questionType === 'MultiChoice' ||
-    question.questionType === 'ShortAnswer'
-  ) {
-    //if (submitted) {
-    const lectureStructureItem = getActiveStructureItem(params.pathname);
-    if (lectureStructureItem?.student?.extraWork.testStatus === 'PASS') {
-      // 답안을 전송했을 경우 채점
-      if (answerResult) {
-        questionClassName += ' correct ';
-      } else {
-        questionClassName += ' wrong ';
-      }
-    }
-    //}
-    if (question.imagePath !== undefined && question.imagePath !== '') {
+const TestQuestionView: React.FC<TestQuestionViewProps> =
+  function TestQuestionView({
+    question,
+    indexNo,
+    answer,
+    answerResult,
+    readOnly,
+    learningState,
+    submitOk,
+    setSubmitOk,
+    dataLoadTime,
+    params,
+    obtainedScore,
+  }) {
+    let questionClassName = ' course-radio-survey ';
+    if (
+      question.questionType === 'ShortAnswer' ||
+      question.questionType === 'Essay'
+    ) {
       questionClassName += ' survey-radio-img ';
     }
-  }
-  useEffect(() => {
     if (
       question.questionType === 'SingleChoice' ||
       question.questionType === 'MultiChoice' ||
       question.questionType === 'ShortAnswer'
     ) {
-      if (submitOk) {
-        if (!answerResult) {
-          const lectureStructureItem = getActiveStructureItem(params.pathname);
-          if (
-            lectureStructureItem?.student?.extraWork.testStatus === 'FAIL' ||
-            lectureStructureItem?.student?.extraWork.testStatus === 'SUBMIT'
-          ) {
-            setAnswer(question.sequence, ''); // 미이수 로딩시 틀린답안 표시 안함
+      //if (submitted) {
+      const lectureStructureItem = getActiveStructureItem(params.pathname);
+      if (lectureStructureItem?.student?.extraWork.testStatus === 'PASS') {
+        // 답안을 전송했을 경우 채점
+        if (answerResult) {
+          questionClassName += ' correct ';
+        } else {
+          questionClassName += ' wrong ';
+        }
+      }
+      //}
+      if (question.imagePath !== undefined && question.imagePath !== '') {
+        questionClassName += ' survey-radio-img ';
+      }
+    }
+    useEffect(() => {
+      if (
+        question.questionType === 'SingleChoice' ||
+        question.questionType === 'MultiChoice' ||
+        question.questionType === 'ShortAnswer'
+      ) {
+        if (submitOk) {
+          if (!answerResult) {
+            const lectureStructureItem = getActiveStructureItem(
+              params.pathname
+            );
+            if (
+              lectureStructureItem?.student?.extraWork.testStatus === 'FAIL' ||
+              lectureStructureItem?.student?.extraWork.testStatus === 'SUBMIT'
+            ) {
+              setAnswer(question.sequence, ''); // 미이수 로딩시 틀린답안 표시 안함
+            }
           }
         }
       }
+    }, [
+      params.pathname,
+      question.sequence,
+      learningState,
+      submitOk,
+      dataLoadTime,
+    ]); // 배열에는 변경을 감지할 항목(제출 후 미이수시)
+
+    const lectureStructureItem = getActiveStructureItem(params.pathname);
+
+    const showScore =
+      lectureStructureItem &&
+      lectureStructureItem.student?.extraWork.testStatus === 'PASS'
+        ? true
+        : false;
+
+    const [useAnswerView, setUseAnswerView] = useState<boolean>(false);
+
+    let strAnswer = '';
+    if (lectureStructureItem?.student?.extraWork.testStatus === 'PASS') {
+      if (question.questionType === 'SingleChoice') {
+        strAnswer = question.questionAnswer?.answer + '번';
+      } else if (question.questionType === 'MultiChoice') {
+        question.questionAnswer?.answer
+          .split(',')
+          .map((result: string, index: number) => {
+            if (index === 0) {
+              strAnswer = '' + result + '번';
+            } else {
+              strAnswer += ', ' + result + '번';
+            }
+          });
+      } else if (question.questionType === 'ShortAnswer') {
+        strAnswer = question.questionAnswer?.answer;
+      }
     }
-  }, [
-    params.pathname,
-    question.sequence,
-    learningState,
-    submitOk,
-    dataLoadTime,
-  ]); // 배열에는 변경을 감지할 항목(제출 후 미이수시)
 
-  const lectureStructureItem = getActiveStructureItem(params.pathname);
-
-  const showScore =
-    lectureStructureItem &&
-    lectureStructureItem.student?.extraWork.testStatus === 'PASS'
-      ? true
-      : false;
-
-  const [useAnswerView, setUseAnswerView] = useState<boolean>(false);
-
-  let strAnswer = '';
-  if (lectureStructureItem?.student?.extraWork.testStatus === 'PASS') {
-    if (question.questionType === 'SingleChoice') {
-      strAnswer = question.questionAnswer?.answer + '번';
-    } else if (question.questionType === 'MultiChoice') {
-      question.questionAnswer?.answer
-        .split(',')
-        .map((result: string, index: number) => {
-          if (index === 0) {
-            strAnswer = '' + result + '번';
-          } else {
-            strAnswer += ', ' + result + '번';
-          }
-        });
-    } else if (question.questionType === 'ShortAnswer') {
-      strAnswer = question.questionAnswer?.answer;
-    }
-  }
-
-  return (
-    <>
-      <div key={question.id} className={questionClassName}>
-        <p>
-          <span>{indexNo + 1}</span>
-          {(question.imagePath && (
-            <p>
-              <span
-                className="copy"
-                dangerouslySetInnerHTML={{
-                  __html: `${question.question} (${question.point}점)`,
-                }}
-              />
-            </p>
-          )) || (
-            <>
-              <span
-                className="copy"
-                dangerouslySetInnerHTML={{
-                  __html: `${question.question} (${question.point}점)`,
-                }}
-              />
-            </>
+    return (
+      <>
+        <div key={question.id} className={questionClassName}>
+          <p>
+            <span>{indexNo + 1}</span>
+            {(question.imagePath && (
+              <p>
+                <span
+                  className="copy"
+                  dangerouslySetInnerHTML={{
+                    __html: `${question.question} (${question.point}점)`,
+                  }}
+                />
+              </p>
+            )) || (
+              <>
+                <span
+                  className="copy"
+                  dangerouslySetInnerHTML={{
+                    __html: `${question.question} (${question.point}점)`,
+                  }}
+                />
+              </>
+            )}
+          </p>
+          {question.imagePath !== undefined && question.imagePath !== '' && (
+            <Image src={question.imagePath} />
           )}
-        </p>
-        {question.imagePath !== undefined && question.imagePath !== '' && (
-          <img src={question.imagePath} />
-        )}
-        {question.questionType === 'SingleChoice' && (
-          <TestSingleChoiceView
-            question={question}
-            answer={answer}
-            setAnswer={setAnswer}
-            readOnly={readOnly}
-            setSubmitOk={setSubmitOk}
-          />
-        )}
-        {question.questionType === 'MultiChoice' && (
-          <TestMultiChoiceView
-            question={question}
-            answer={answer}
-            setAnswer={setAnswer}
-            readOnly={readOnly}
-            setSubmitOk={setSubmitOk}
-          />
-        )}
-        {question.questionType === 'ShortAnswer' && (
-          <TestShortAnswerView
-            question={question}
-            answer={answer}
-            setAnswer={setAnswer}
-            readOnly={readOnly}
-          />
-        )}
-        {question.questionType === 'Essay' && (
-          <TestEssayView
-            question={question}
-            answer={answer}
-            setAnswer={setAnswer}
-            readOnly={readOnly}
-            showScore={showScore}
-            obtainedScore={obtainedScore}
-          />
-        )}
+          {question.questionType === 'SingleChoice' && (
+            <TestSingleChoiceView
+              question={question}
+              answer={answer}
+              setAnswer={setAnswer}
+              readOnly={readOnly}
+              setSubmitOk={setSubmitOk}
+            />
+          )}
+          {question.questionType === 'MultiChoice' && (
+            <TestMultiChoiceView
+              question={question}
+              answer={answer}
+              setAnswer={setAnswer}
+              readOnly={readOnly}
+              setSubmitOk={setSubmitOk}
+            />
+          )}
+          {question.questionType === 'ShortAnswer' && (
+            <TestShortAnswerView
+              question={question}
+              answer={answer}
+              setAnswer={setAnswer}
+              readOnly={readOnly}
+            />
+          )}
+          {question.questionType === 'Essay' && (
+            <TestEssayView
+              question={question}
+              answer={answer}
+              setAnswer={setAnswer}
+              readOnly={readOnly}
+              showScore={showScore}
+              obtainedScore={obtainedScore}
+            />
+          )}
 
-        {lectureStructureItem?.student?.extraWork.testStatus === 'PASS' &&
-          question.questionType !== 'Essay' && (
-            <>
-              {!useAnswerView && (
-                <div className="survey-explain">
-                  <Button
-                    className="ui icon button right btn-blue"
-                    onClick={() => setUseAnswerView(true)}
-                  >
-                    정답보기
-                    <Icon
-                      aria-hidden="true"
-                      className="morelink more2 view-down"
-                    />
-                  </Button>
-                </div>
-              )}
-              {useAnswerView && (
-                <div className="survey-explain">
-                  <Button
-                    className="ui icon button right btn-blue"
-                    onClick={() => setUseAnswerView(false)}
-                  >
-                    정답닫기
-                    <Icon aria-hidden="true" className="morelink more2" />
-                  </Button>
-                  <div className="survey-answer">
-                    <span>정답</span>
-                    <span>{strAnswer}</span>
+          {lectureStructureItem?.student?.extraWork.testStatus === 'PASS' &&
+            question.questionType !== 'Essay' && (
+              <>
+                {!useAnswerView && (
+                  <div className="survey-explain">
+                    <Button
+                      className="ui icon button right btn-blue"
+                      onClick={() => setUseAnswerView(true)}
+                    >
+                      정답보기
+                      <Icon
+                        aria-hidden="true"
+                        className="morelink more2 view-down"
+                      />
+                    </Button>
                   </div>
-                  {question.questionAnswer.explanation && (
+                )}
+                {useAnswerView && (
+                  <div className="survey-explain">
+                    <Button
+                      className="ui icon button right btn-blue"
+                      onClick={() => setUseAnswerView(false)}
+                    >
+                      정답닫기
+                      <Icon aria-hidden="true" className="morelink more2" />
+                    </Button>
                     <div className="survey-answer">
-                      <span>해설</span>
-                      <span>{question.questionAnswer.explanation}</span>
+                      <span>정답</span>
+                      <span>{strAnswer}</span>
                     </div>
-                  )}
-                </div>
-              )}
-            </>
-          )}
-      </div>
-    </>
-  );
-};
+                    {question.questionAnswer.explanation && (
+                      <div className="survey-answer">
+                        <span>해설</span>
+                        <span>{question.questionAnswer.explanation}</span>
+                      </div>
+                    )}
+                  </div>
+                )}
+              </>
+            )}
+        </div>
+      </>
+    );
+  };
 
 export default TestQuestionView;
--- src/lecture/detail/ui/view/LectureTestView/TestSingleChoiceView.tsx
+++ src/lecture/detail/ui/view/LectureTestView/TestSingleChoiceView.tsx
@@ -1,6 +1,7 @@
 import ExamQuestion from '../../../model/ExamQuestion';
 import React, { useCallback } from 'react';
 import { Radio } from 'semantic-ui-react';
+import Image from 'shared/components/Image';
 
 interface TestSingleChoiceViewProps {
   question: ExamQuestion;
@@ -10,41 +11,43 @@ interface TestSingleChoiceViewProps {
   setSubmitOk: (submitOk: boolean) => void;
 }
 
-const TestSingleChoiceView: React.FC<TestSingleChoiceViewProps> = function TestSingleChoiceView({
-  question,
-  answer,
-  setAnswer,
-  readOnly,
-  setSubmitOk,
-}) {
-  const setAnswerFromRadio = useCallback((e: any, data: any) => {
-    setAnswer(question.sequence, data.value);
-  }, []);
+const TestSingleChoiceView: React.FC<TestSingleChoiceViewProps> =
+  function TestSingleChoiceView({
+    question,
+    answer,
+    setAnswer,
+    readOnly,
+    setSubmitOk,
+  }) {
+    const setAnswerFromRadio = useCallback((e: any, data: any) => {
+      setAnswer(question.sequence, data.value);
+    }, []);
 
-  return (
-    <div className="course-survey-list">
-      {question.items?.map(item => (
-        <>
-          <Radio
-            key={question.sequence + '_' + item.itemNo}
-            className="base"
-            label={
-              (readOnly && item.itemNo + '. ' + item.itemText) || item.itemText
-            }
-            name={`test_${question.sequence}`}
-            value={item.itemNo}
-            checked={item.itemNo === answer}
-            onChange={setAnswerFromRadio}
-            readOnly={readOnly}
-            onClick={() => setSubmitOk(false)}
-          />
-          {item.imgSrc !== undefined && item.imgSrc !== '' && (
-            <img src={item.imgSrc} />
-          )}
-        </>
-      ))}
-    </div>
-  );
-};
+    return (
+      <div className="course-survey-list">
+        {question.items?.map((item) => (
+          <>
+            <Radio
+              key={question.sequence + '_' + item.itemNo}
+              className="base"
+              label={
+                (readOnly && item.itemNo + '. ' + item.itemText) ||
+                item.itemText
+              }
+              name={`test_${question.sequence}`}
+              value={item.itemNo}
+              checked={item.itemNo === answer}
+              onChange={setAnswerFromRadio}
+              readOnly={readOnly}
+              onClick={() => setSubmitOk(false)}
+            />
+            {item.imgSrc !== undefined && item.imgSrc !== '' && (
+              <Image src={item.imgSrc} />
+            )}
+          </>
+        ))}
+      </div>
+    );
+  };
 
 export default TestSingleChoiceView;
