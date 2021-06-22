8393682207e955aad36f75a0f89196429c16b129
유성근 <2000511@partner.skcc.com>
2021-06-22 10:23:28 +0900

    메인팝업 수정

--- src/main/sub/MainPagePopup/api/MainPagePopupApi.ts
+++ src/main/sub/MainPagePopup/api/MainPagePopupApi.ts
@@ -3,7 +3,6 @@ import { MainPagePopupModel } from '../model/MainPagePopupModel';
 
 const mainURL = '/api/board/popup/main';
 
-//이메일 암호화
 export function getMainPagePopupFirst() {
   return axiosApi.get<MainPagePopupModel>(mainURL)
     .then(response => response && response.data);
