fc008fef7579aa127c596fd5d27cee9c104900c5
박은호 <jz@yaman.io>
2021-07-14 17:38:06 +0900

    CSS 변경 및 오류 이미지 제거

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,8 @@ import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
 // 2021-07-05
-import './style/css/2.06f1e362.chunk.css';
-import './style/css/main.8bf3fcf9.chunk.css';
+import './style/css/2.74b577d9.chunk.css';
+import './style/css/main.d3f6c62b.chunk.css';
 
 initializeBody();
 
