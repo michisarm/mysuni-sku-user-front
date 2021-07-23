dff7f5443ffdd33776d98073b444c3be1e3dfdec
황종옥 <ockkk@whatvita.kr>
2021-07-23 11:03:49 +0900

    receivesMessage 함수 callback 부분 수정

--- src/lecture/detail/ui/logic/LectureStateContainer.tsx
+++ src/lecture/detail/ui/logic/LectureStateContainer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { clearFindMyCardRelatedStudentsCache } from '../../api/cardApi';
@@ -44,7 +43,7 @@ function LectureStateContainer() {
         }
       }
     },
-    [lectureState?.cubeType, params.cardId, params.cubeId]
+    [lectureState?.cubeType, params.cardId, params.cubeId, lectureWebpage]
   );
 
   useEffect(() => {
@@ -58,7 +57,7 @@ function LectureStateContainer() {
     return () => {
       window.removeEventListener('message', receiveMessage, false);
     };
-  }, [lectureWebpage?.urlType, lectureState?.cubeType]);
+  }, [lectureWebpage?.urlType, lectureState?.cubeType, receiveMessage]);
 
   if (lectureState === undefined) {
     return null;
