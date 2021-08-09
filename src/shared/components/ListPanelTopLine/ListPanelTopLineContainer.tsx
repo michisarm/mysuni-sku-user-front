e9a7f924498f8419e0043849aab6baeac59b09ef
sanggeun <tkdrms753@gmail.com>
2021-08-09 14:49:33 +0900

    다국어_TopLine_분기함수 추가

--- src/shared/components/ListPanelTopLine/ListPanelTopLineContainer.tsx
+++ src/shared/components/ListPanelTopLine/ListPanelTopLineContainer.tsx
@@ -1,15 +1,13 @@
-
 import React, { Component } from 'react';
 import { reactAutobind } from '@nara.platform/accent';
 import { observer } from 'mobx-react';
 import classNames from 'classnames';
 import { getPolyglotText, PolyglotText } from '../../ui/logic/PolyglotText';
 
-
 interface Props {
-  count: number,
-  className?: string,
-  countMessage?: string
+  count: number;
+  className?: string;
+  countMessage?: string;
 }
 
 @reactAutobind
@@ -20,17 +18,64 @@ class ListPanelTopLineContainer extends Component<Props> {
     //
     const { className, count, children, countMessage } = this.props;
 
+    function headLineRender() {
+      if (countMessage === 'AllBadgeList') {
+        return (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: getPolyglotText(
+                `총 <strong>${
+                  count || 0
+                }개</strong>의 획득할 수 있는 Badge가 있습니다.`,
+                'Certification-bdls-lcnt'
+              ),
+            }}
+          />
+        );
+      } else if (countMessage === 'ChallengingBadgeList') {
+        return (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: getPolyglotText(
+                `총 <strong>${
+                  count || 0
+                }개</strong>의 도전중인 Badge가 있습니다.`,
+                'Certification-clls-lcnt'
+              ),
+            }}
+          />
+        );
+      } else if (countMessage === 'EarnedBadgeList') {
+        return (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: getPolyglotText(
+                `총 <strong>${count || 0}개</strong>의 Badge를 획득하였습니다.`,
+                'Certification-clls-lcnt'
+              ),
+            }}
+          />
+        );
+      } else {
+        <div
+          dangerouslySetInnerHTML={{
+            __html: getPolyglotText(
+              `총 <strong>${count || 0}개</strong>의 리스트가 있습니다.`,
+              'Create-MainList-리스트'
+            ),
+          }}
+        />;
+      }
+    }
+
     return (
       <div className={classNames('top-guide-title', className)}>
         <div className="list-number">
-          <PolyglotText defaultString="총" id="Create-MainList-총" />{' '}
-          <strong>
-            {count || 0}<PolyglotText defaultString="개" id="Create-MainList-개" />
-          </strong>
-          <PolyglotText defaultString="의" id="Create-MainList-의" />{' '}
-          {countMessage ? countMessage : getPolyglotText('리스트가 있습니다.', 'Create-MainList-리스트')}
+          {/* <PolyglotText defaultString="총" id="Create-MainList-총" /> */}
+          {/* <PolyglotText defaultString="의" id="Create-MainList-의" /> */}
+          {headLineRender()}
         </div>
-        { children }
+        {children}
       </div>
     );
   }
