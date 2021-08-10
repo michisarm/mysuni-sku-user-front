b34f0c7b731a1a49b177db4bbaee3d14dbea71ff
최다희 <dhchoi@sk.com>
2021-08-10 12:40:21 +0900

    css modify

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,8 @@ import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
 // 2021-07-05
-import './style/css/2.1ba1de7b.chunk.css';
-import './style/css/main.a0e5ee8f.chunk.css';
+import './style/css/2.4877abc3.chunk.css';
+import './style/css/main.71550b56.chunk.css';
 
 initializeBody();
 
