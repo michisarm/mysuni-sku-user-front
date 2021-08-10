ef9498b20d3c109a1dd9bfa53f0471c4eff35258
박은호 <jz@yaman.io>
2021-08-10 17:23:28 +0900

    css 변경

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,8 @@ import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
 // 2021-07-27
-import './style/css/2.74b577d9.chunk.css';
-import './style/css/main.b342ba33.chunk.css';
+import './style/css/2.4877abc3.chunk.css';
+import './style/css/main.255e093a.chunk.css';
 import { initializeI18nResource } from 'shared/viewmodel/PolyglotText';
 
 initializeBody();
