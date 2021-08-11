1122ce75a4453c1e9a731aaa8b2ef5f05ad481f0
sanggeun <tkdrms753@gmail.com>
2021-08-11 09:27:44 +0900

    다국어_개인학습상태추가2

--- src/myTraining/model/AplStateName.ts
+++ src/myTraining/model/AplStateName.ts
@@ -2,13 +2,13 @@ import { getPolyglotText } from 'shared/ui/logic/PolyglotText';
 
 export function aplStateNamePolyglotText(s: string) {
   if (s === 'Created') {
-    return getPolyglotText('생성test', 'apl-state-생성');
+    return getPolyglotText('생성', 'apl-state-생성');
   } else if (s === 'OpenApproval') {
-    return getPolyglotText('승인대기test', 'apl-state-승인대기');
+    return getPolyglotText('승인대기', 'apl-state-승인대기');
   } else if (s === 'Opened') {
-    return getPolyglotText('승인test', 'apl-state-승인');
+    return getPolyglotText('승인', 'apl-state-승인');
   } else if (s === 'Rejected') {
-    return getPolyglotText('반려test', 'apl-state-반려');
+    return getPolyglotText('반려', 'apl-state-반려');
   } else {
     return '';
   }
