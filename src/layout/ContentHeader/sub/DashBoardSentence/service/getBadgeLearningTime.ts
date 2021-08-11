eec2b9552780c10980ecc620313af11e7c928c30
박은호 <jz@yaman.io>
2021-08-11 11:26:24 +0900

    대시보드 문구 관련 오류 수정

--- src/layout/ContentHeader/sub/DashBoardSentence/service/getBadgeLearningTime.ts
+++ src/layout/ContentHeader/sub/DashBoardSentence/service/getBadgeLearningTime.ts
@@ -7,12 +7,18 @@ export async function requestDashBoardSentence(count?: number) {
     if (item) {
       const userLanguage = SkProfileService.instance.skProfile.language;
       let dashboardSentence: string[] = [];
-      if(userLanguage === 'Korean'){
-        dashboardSentence = [...item.koreanTexts];
-      } else if(userLanguage === 'English') {
-        dashboardSentence = [...item.englishTexts];
-      } else if(userLanguage === 'Chinese') {
-        dashboardSentence = [...item.chineseTexts];
+      if (userLanguage === 'Korean') {
+        dashboardSentence = Array.isArray(item.koreanTexts)
+          ? [...item.koreanTexts]
+          : [];
+      } else if (userLanguage === 'English') {
+        dashboardSentence = Array.isArray(item.englishTexts)
+          ? [...item.englishTexts]
+          : [];
+      } else if (userLanguage === 'Chinese') {
+        dashboardSentence = Array.isArray(item.chineseTexts)
+          ? [...item.chineseTexts]
+          : [];
       }
       console.log(dashboardSentence);
 
