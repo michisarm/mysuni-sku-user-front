5d76f93cb83c297d10b695e9ddfb850c74e6c549
sanggeun <tkdrms753@gmail.com>
2021-08-11 16:10:23 +0900

    Create  enum다국어기능 추가

--- src/shared/ui/logic/EnumUtil.ts
+++ src/shared/ui/logic/EnumUtil.ts
@@ -1,3 +1,5 @@
+import { getPolyglotText } from './PolyglotText';
+
 export enum CubeStateView {
   Created = '저장',
   OpenApproval = '승인대기',
@@ -17,12 +19,26 @@ type StringDict = { [key: string]: string };
 const EnumUtil = {
   getEnumValue(dict: StringDict, targetKey: string) {
     const result: Map<string, string> = new Map<string, string>();
-    Object.keys(dict).map(key => {
+    Object.keys(dict).map((key) => {
       if (key === targetKey) result.set(key, dict[key]);
     });
 
     return result;
   },
 };
+
+export function cubeStatePolyglot(s: string) {
+  if (s === '저장') {
+    return getPolyglotText('저장', 'Create-MainList-저장');
+  } else if (s === '승인') {
+    return getPolyglotText('승인ts', 'Create-MainList-승인dd');
+  } else if (s === '승인대기') {
+    return getPolyglotText('승인대기', 'Create-MainList-승대');
+  } else if (s === '반려') {
+    return getPolyglotText('반려', 'Create-MainList-반려');
+  } else {
+    return '';
+  }
+}
 
 export default EnumUtil;
