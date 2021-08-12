b575feab961f186dbcb22fefcea47146ecbc378e
sanggeun <tkdrms753@gmail.com>
2021-08-12 20:54:23 +0900

    selectoption 수정 /오류34

--- src/certification/ui/model/SelectOptions.ts
+++ src/certification/ui/model/SelectOptions.ts
@@ -3,13 +3,13 @@ import { getPolyglotText } from 'shared/ui/logic/PolyglotText';
 const SelectOptions = {
   difficultyLevel: [
     {
-      key: '1',
+      key: '0',
       text: getPolyglotText('전체', 'Certification-bdls-sl전체'),
       value: '',
     },
-    { key: '2', text: 'Level1', value: 'Level1' },
-    { key: '3', text: 'Level2', value: 'Level2' },
-    { key: '4', text: 'Level3', value: 'Level3' },
+    { key: '1', text: 'Level1', value: 'Level1' },
+    { key: '2', text: 'Level2', value: 'Level2' },
+    { key: '3', text: 'Level3', value: 'Level3' },
   ],
 };
 
