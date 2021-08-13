ddb8fec0e6eff9fa3e6255a079f02eeee0e7ea7d
sanggeun <tkdrms753@gmail.com>
2021-08-13 16:39:22 +0900

    학습_테이블_학습상태표시 추가

--- src/shared/model/LearningStateName.ts
+++ src/shared/model/LearningStateName.ts
@@ -1,3 +1,14 @@
+import { getPolyglotText } from 'shared/ui/logic/PolyglotText';
+
+export function stateNamePolytglot(s: string) {
+  if (s === '학습중') {
+    return getPolyglotText('학습중', 'learning-학습상태-학습중');
+  } else if (s === '학습완료') {
+    return getPolyglotText('학습완료', 'learning-학습상태-학습완료');
+  } else if (s === '취소/미이수') {
+    return getPolyglotText('취소/미이수', 'learning-학습상태-취소미이수');
+  } else return '';
+}
 
 enum LearningStateName {
   Progress = '학습중',
