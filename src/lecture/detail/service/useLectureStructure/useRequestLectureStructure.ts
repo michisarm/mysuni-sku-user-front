839c27121aa3cba18140faa61175bcf903cc1b03
김동구 <bbonggu92@gmail.com>
2021-08-15 22:25:51 +0900

    update 카드  접근 권한 없음 문구 수정

--- src/lecture/detail/service/useLectureStructure/useRequestLectureStructure.ts
+++ src/lecture/detail/service/useLectureStructure/useRequestLectureStructure.ts
@@ -15,7 +15,7 @@ export function useRequestLectureStructure() {
         reactAlert({
           title: getPolyglotText('권한이 없습니다.', 'learning-권한-권한없음'),
           message: getPolyglotText(
-            '본 콘텐츠는 아직 서비스 준비가 되지 않았습니다. 각 사 담당자와 협의 후, 추후 오픈하겠습니다.<br/>관련 문의는 Q&A를 이용해주시기 바랍니다.',
+            '본 콘텐츠는 아직 서비스 준비가 되지 않았습니다.<br/>각 사 담당자와 협의 후, 추후 오픈하겠습니다.<br/>관련 문의는 Q&A를 이용해주시기 바랍니다.',
             'learning-권한-접근불가'
           ),
           onClose: () => {
