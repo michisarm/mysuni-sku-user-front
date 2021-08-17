f26832a07e042f877c98f930754a6b3b42d1e09f
sanggeun <tkdrms753@gmail.com>
2021-08-17 14:42:45 +0900

    다국어_강의대본스크립트 <br/>제거

--- src/lecture/detail/ui/logic/LectureCubeTranscriptContainer.tsx
+++ src/lecture/detail/ui/logic/LectureCubeTranscriptContainer.tsx
@@ -27,226 +27,247 @@ interface LectureTranscriptContainerProps {
   // trascriptScrollMove: () => void;
 }
 
-const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> = function LectureTranscriptContainer({
-  transLangVal,
-  setTransLangVal,
-  lectureSummary,
-  // trascriptScrollMove
-}) {
-  const selectTransLangObj = [
-    { key: 'ko', value: 'ko', text: 'KR' },
-    { key: 'eng', value: 'en', text: 'ENG' },
-    { key: 'chn', value: 'cn', text: 'CHN' },
-  ];
+const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> =
+  function LectureTranscriptContainer({
+    transLangVal,
+    setTransLangVal,
+    lectureSummary,
+    // trascriptScrollMove
+  }) {
+    const selectTransLangObj = [
+      { key: 'ko', value: 'ko', text: 'KR' },
+      { key: 'eng', value: 'en', text: 'ENG' },
+      { key: 'chn', value: 'cn', text: 'CHN' },
+    ];
 
-  const ERROR_RANGE = 1.5; // getCurrentTime으로 동영상 시간을 받을 때 0.n초의 오차가 발생 할 때가 있어서 오차 범위 지정
+    const ERROR_RANGE = 1.5; // getCurrentTime으로 동영상 시간을 받을 때 0.n초의 오차가 발생 할 때가 있어서 오차 범위 지정
 
-  const [transcriptList, setTranScriptList] = useState<any>();
-  const [panoptoSessionId, setPanoptoSessionId] = useState<string | undefined>(
-    getLectureMedia()?.mediaContents.internalMedias[0].panoptoSessionId
-  );
+    const [transcriptList, setTranScriptList] = useState<any>();
+    const [panoptoSessionId, setPanoptoSessionId] = useState<
+      string | undefined
+    >(getLectureMedia()?.mediaContents.internalMedias[0].panoptoSessionId);
 
-  const [autoHighlight, setAutoHighlight] = useState<boolean>();
-  const intervalTranscript: any = useRef<any>(null);
-  const [transciptHighlight, setTransciptHighlight] = useState<string>();
+    const [autoHighlight, setAutoHighlight] = useState<boolean>();
+    const intervalTranscript: any = useRef<any>(null);
+    const [transciptHighlight, setTransciptHighlight] = useState<string>();
 
-  const panoptoEmbedPlayerState = usePanoptoEmbedPlayerState();
+    const panoptoEmbedPlayerState = usePanoptoEmbedPlayerState();
 
-  // 특정 위치로 재생 위치 이동
-  const seekByIndex = (startIndex: number, endIndex: number) => {
-    if (startIndex >= 0) {
-      // jz - 무슨 의미?
-      // if (getEmbed().isPaused === false) {
-      //   seekTo(startIndex);
-      // }
-      if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
-        seekTo(startIndex);
-      }
-    }
-  };
-
-  // 동영상 플레이어 시간 형식으로 변환
-  const convertStringTimeToNumber = (time: string = '0') => {
-    return (
-      parseInt(time.substr(0, 2), 10) * 60 * 60 +
-      parseInt(time.substr(2, 2), 10) * 60 +
-      parseInt(time.substr(4, 2), 10)
-    );
-  };
-
-  useEffect(() => {
-    // 동영상 상태 변경 시 callback
-    if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
-      setAutoHighlight(true);
-    }
-  }, [panoptoEmbedPlayerState?.playerState]);
-
-  // 대본 선택 관련 초기화
-  const initialize = () => {
-    //자동 하이라이트 기능
-    if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
-      setAutoHighlight(true);
-    }
-    clearInterval(intervalTranscript.current);
-  };
-
-  useEffect(() => {
-    return () => {
-      clearInterval(intervalTranscript.current);
-      setAutoHighlight(false);
-      setPanoptoSessionId('');
-    };
-  }, []);
-
-  useEffect(() => {
-    setPanoptoSessionId(
-      getLectureMedia()?.mediaContents.internalMedias[0].panoptoSessionId
-    );
-  });
-
-  useEffect(() => {
-    // 언어를 바꿨을 때 초기화
-    initialize();
-
-    const getTranScriptsFunc = async () => {
-      if (panoptoSessionId !== undefined) {
-        // '0002939c-8307-4dd2-8f5c-abec0022d4fc'
-        const transcriptsItem = await findTranscript(
-          panoptoSessionId,
-          transLangVal
-        );
-
-        setTranScriptList(transcriptsItem);
-      }
-      // trascriptScrollMove();
-    };
-
-    getTranScriptsFunc();
-  }, [transLangVal, panoptoSessionId]);
-
-  useEffect(() => {
-    clearInterval(intervalTranscript.current);
-
-    if (autoHighlight) {
-      if (panoptoEmbedPlayerState?.playerState === PlayerState.Paused) {
-        setAutoHighlight(false);
-        clearInterval(intervalTranscript);
-      } else {
-        if (transcriptList !== undefined) {
-          //시간 2 초마다 체크해서 자막 스크롤 이동 및 하이라이트 넣기
-          transcriptList.map((item: any, key: number) => {
-            if (
-              convertStringTimeToNumber(item.startTime) <
-              (panoptoEmbedPlayerState?.currentTime || 0)
-            ) {
-              if (
-                (panoptoEmbedPlayerState?.currentTime || 0) <
-                convertStringTimeToNumber(item.endTime)
-              ) {
-                setTransciptHighlight(item.idx);
-              }
-            }
-          });
+    // 특정 위치로 재생 위치 이동
+    const seekByIndex = (startIndex: number, endIndex: number) => {
+      if (startIndex >= 0) {
+        // jz - 무슨 의미?
+        // if (getEmbed().isPaused === false) {
+        //   seekTo(startIndex);
+        // }
+        if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
+          seekTo(startIndex);
         }
       }
-    }
-    // return () => {
-    //   clearInterval(intervalTranscript);
-    // };
-  }, [
-    panoptoEmbedPlayerState?.playerState,
-    panoptoEmbedPlayerState?.currentTime,
-    autoHighlight,
-    transcriptList,
-  ]);
+    };
 
-  useEffect(() => {
-    // 하드코딩하여 적용... 추후 필요시 체크해서 하는 부분이 필요할 듯
-    const cont = document.getElementById('panopto-embed-player');
-    if (cont) {
-      window.scrollTo(0, 800);
-    }
-  }, [transcriptList]);
+    // 동영상 플레이어 시간 형식으로 변환
+    const convertStringTimeToNumber = (time: string = '0') => {
+      return (
+        parseInt(time.substr(0, 2), 10) * 60 * 60 +
+        parseInt(time.substr(2, 2), 10) * 60 +
+        parseInt(time.substr(4, 2), 10)
+      );
+    };
 
-  const highlight = (id: string) => {
-    if (transciptHighlight === id) {
-      return 'transcript-active';
-    } else {
-      return 'transcript-hover';
-    }
-  };
+    useEffect(() => {
+      // 동영상 상태 변경 시 callback
+      if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
+        setAutoHighlight(true);
+      }
+    }, [panoptoEmbedPlayerState?.playerState]);
 
-  return (
-    <>
-      <div className="transcript-box" id="tanscript-scroll">
-        <div className="transcript-top">
-          <Select
-            placeholder={getPolyglotText('분류를 선택해주세요', 'cube-Contents-분류를 선택해주세요')}
-            className="ui small-border dropdown m0"
-            defaultValue={transLangVal}
-            options={selectTransLangObj}
-            onChange={(e: any, data: any) => {
-              setTransLangVal(data.value);
-            }}
-          />
-          <div>
-            <Popup
-              trigger={<Button icon="tooltip" />}
-              style={style}
-              position="bottom center"
-              wide
-            >
-              <p>
-                <PolyglotText defaultString="현재 선택된 언어의 Transcript를 <br /> txt 파일로 다운로드 받으실 수 있습니다." id="cube-Contents-다운로드 " />
-              </p>
-            </Popup>
-            <button
-              className="ui icon button left post delete-kr"
-              onClick={() => {
-                if (transcriptList?.length > 0) {
-                  const langText =
-                    selectTransLangObj.find(lang => lang.value === transLangVal)
-                      ?.text || '';
-                  console.log('lectureSummary', lectureSummary);
-                  downloadTranscript(
-                    transcriptList,
-                    (lectureSummary?.name || 'transcript').concat('_', langText)
-                  );
+    // 대본 선택 관련 초기화
+    const initialize = () => {
+      //자동 하이라이트 기능
+      if (panoptoEmbedPlayerState?.playerState === PlayerState.Playing) {
+        setAutoHighlight(true);
+      }
+      clearInterval(intervalTranscript.current);
+    };
+
+    useEffect(() => {
+      return () => {
+        clearInterval(intervalTranscript.current);
+        setAutoHighlight(false);
+        setPanoptoSessionId('');
+      };
+    }, []);
+
+    useEffect(() => {
+      setPanoptoSessionId(
+        getLectureMedia()?.mediaContents.internalMedias[0].panoptoSessionId
+      );
+    });
+
+    useEffect(() => {
+      // 언어를 바꿨을 때 초기화
+      initialize();
+
+      const getTranScriptsFunc = async () => {
+        if (panoptoSessionId !== undefined) {
+          // '0002939c-8307-4dd2-8f5c-abec0022d4fc'
+          const transcriptsItem = await findTranscript(
+            panoptoSessionId,
+            transLangVal
+          );
+
+          setTranScriptList(transcriptsItem);
+        }
+        // trascriptScrollMove();
+      };
+
+      getTranScriptsFunc();
+    }, [transLangVal, panoptoSessionId]);
+
+    useEffect(() => {
+      clearInterval(intervalTranscript.current);
+
+      if (autoHighlight) {
+        if (panoptoEmbedPlayerState?.playerState === PlayerState.Paused) {
+          setAutoHighlight(false);
+          clearInterval(intervalTranscript);
+        } else {
+          if (transcriptList !== undefined) {
+            //시간 2 초마다 체크해서 자막 스크롤 이동 및 하이라이트 넣기
+            transcriptList.map((item: any, key: number) => {
+              if (
+                convertStringTimeToNumber(item.startTime) <
+                (panoptoEmbedPlayerState?.currentTime || 0)
+              ) {
+                if (
+                  (panoptoEmbedPlayerState?.currentTime || 0) <
+                  convertStringTimeToNumber(item.endTime)
+                ) {
+                  setTransciptHighlight(item.idx);
                 }
+              }
+            });
+          }
+        }
+      }
+      // return () => {
+      //   clearInterval(intervalTranscript);
+      // };
+    }, [
+      panoptoEmbedPlayerState?.playerState,
+      panoptoEmbedPlayerState?.currentTime,
+      autoHighlight,
+      transcriptList,
+    ]);
+
+    useEffect(() => {
+      // 하드코딩하여 적용... 추후 필요시 체크해서 하는 부분이 필요할 듯
+      const cont = document.getElementById('panopto-embed-player');
+      if (cont) {
+        window.scrollTo(0, 800);
+      }
+    }, [transcriptList]);
+
+    const highlight = (id: string) => {
+      if (transciptHighlight === id) {
+        return 'transcript-active';
+      } else {
+        return 'transcript-hover';
+      }
+    };
+
+    return (
+      <>
+        <div className="transcript-box" id="tanscript-scroll">
+          <div className="transcript-top">
+            <Select
+              placeholder={getPolyglotText(
+                '분류를 선택해주세요',
+                'cube-Contents-분류를 선택해주세요'
+              )}
+              className="ui small-border dropdown m0"
+              defaultValue={transLangVal}
+              options={selectTransLangObj}
+              onChange={(e: any, data: any) => {
+                setTransLangVal(data.value);
               }}
-            >
-              <Icon className="icon-down-type5" />
-              <PolyglotText defaultString="Download" id="cube-Contents-Download" />
-            </button>
-          </div>
-        </div>
-        {transcriptList?.length > 0 ? (
-          transcriptList?.map((lectureTranscript: any) => {
-            return (
-              <>
-                <span
-                  id={lectureTranscript.idx + ''}
-                  key={lectureTranscript.idx}
-                  className={highlight(lectureTranscript.idx + '')}
-                  onClick={() => {
-                    seekByIndex(
-                      convertStringTimeToNumber(lectureTranscript.startTime),
-                      convertStringTimeToNumber(lectureTranscript.endTime)
-                    );
+            />
+            <div>
+              <Popup
+                trigger={<Button icon="tooltip" />}
+                style={style}
+                position="bottom center"
+                wide
+              >
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: getPolyglotText(
+                      `현재 선택된 언어의 Transcript를 <br /> txt 파일로 다운로드 받으실 수 있습니다.`,
+                      'cube-Contents-다운로드'
+                    ),
                   }}
-                  style={{ cursor: 'pointer' }}
-                >
-                  {lectureTranscript.text}&nbsp;
-                </span>
-              </>
-            );
-          })
-        ) : (
-          <span><PolyglotText defaultString="저장 된 대본이 없습니다." id="cube-Contents-대본없음" /></span>
-        )}
-      </div>
-    </>
-  );
-};
+                />
+              </Popup>
+              <button
+                className="ui icon button left post delete-kr"
+                onClick={() => {
+                  if (transcriptList?.length > 0) {
+                    const langText =
+                      selectTransLangObj.find(
+                        (lang) => lang.value === transLangVal
+                      )?.text || '';
+                    console.log('lectureSummary', lectureSummary);
+                    downloadTranscript(
+                      transcriptList,
+                      (lectureSummary?.name || 'transcript').concat(
+                        '_',
+                        langText
+                      )
+                    );
+                  }
+                }}
+              >
+                <Icon className="icon-down-type5" />
+                <PolyglotText
+                  defaultString="Download"
+                  id="cube-Contents-Download"
+                />
+              </button>
+            </div>
+          </div>
+          {transcriptList?.length > 0 ? (
+            transcriptList?.map((lectureTranscript: any) => {
+              return (
+                <>
+                  <span
+                    id={lectureTranscript.idx + ''}
+                    key={lectureTranscript.idx}
+                    className={highlight(lectureTranscript.idx + '')}
+                    onClick={() => {
+                      seekByIndex(
+                        convertStringTimeToNumber(lectureTranscript.startTime),
+                        convertStringTimeToNumber(lectureTranscript.endTime)
+                      );
+                    }}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    {lectureTranscript.text}&nbsp;
+                  </span>
+                </>
+              );
+            })
+          ) : (
+            <span>
+              <PolyglotText
+                defaultString="저장 된 대본이 없습니다."
+                id="cube-Contents-대본없음"
+              />
+            </span>
+          )}
+        </div>
+      </>
+    );
+  };
 
 export default LectureTranscriptContainer;
