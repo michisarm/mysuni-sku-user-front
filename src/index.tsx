9ceaa9e7bccf1570d5191afaf69ad428383635c2
황종옥 <ockkk@whatvita.kr>
2021-08-24 15:51:11 +0900

    0823 css 변경

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,9 @@ import { initializeBody } from './shared/helper/bodyHelper';
 import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
-// 2021-08-19
-import './style/css/2.12a2c93e.chunk.css';
-import './style/css/main.8c6ced2a.chunk.css';
+// 2021-08-23
+import './style/css/2.62abae99.chunk.css';
+import './style/css/main.fff2e117.chunk.css';
 import { initializeI18nResource } from 'shared/viewmodel/PolyglotText';
 
 initializeBody();
