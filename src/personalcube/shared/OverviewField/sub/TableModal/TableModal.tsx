cc0445dd1195ca7c24c4160e0fb1b5da7932bf1b
sanggeun <tkdrms753@gmail.com>
2021-08-31 15:57:51 +0900

    lecture_차수세부정보 다국어 추가

--- src/personalcube/shared/OverviewField/sub/TableModal/TableModal.tsx
+++ src/personalcube/shared/OverviewField/sub/TableModal/TableModal.tsx
@@ -5,6 +5,7 @@ import classNames from 'classnames';
 import { Modal, Table, Popup, Icon, Button } from 'semantic-ui-react';
 import { Classroom } from '../../../../../lecture/detail/viewModel/LectureClassroom';
 import { parsePolyglotString } from 'shared/viewmodel/PolyglotString';
+import { PolyglotText } from 'shared/ui/logic/PolyglotText';
 
 interface Props {
   classrooms: Classroom[];
@@ -71,22 +72,70 @@ class TableModal extends Component<Props, States> {
         onOpen={this.show}
       >
         <Modal.Header className="res">
-          차수세부내용
-          <span className="sub f12">차수를 확인해주세요.</span>
+          <PolyglotText
+            defaultString="차수세부내용"
+            id="lecture-차수세부내용모달-타이틀"
+          />
+          <span className="sub f12">
+            <PolyglotText
+              defaultString="차수를 선택해주세요."
+              id="lecture-차수세부내용모달-차수선택"
+            />
+          </span>
         </Modal.Header>
         <Modal.Content onScroll={this.onScroll}>
           <div className="scrolling-60vh">
             <Table className="head-fix ml-05-p01">
               <Table.Header>
                 <Table.Row>
-                  <Table.HeaderCell>차수</Table.HeaderCell>
-                  <Table.HeaderCell>강사</Table.HeaderCell>
-                  <Table.HeaderCell>담당자 정보 및 이메일</Table.HeaderCell>
-                  <Table.HeaderCell>교육장소/웹사이트</Table.HeaderCell>
-                  <Table.HeaderCell>정원정보</Table.HeaderCell>
-                  <Table.HeaderCell>수강신청 기간</Table.HeaderCell>
-                  <Table.HeaderCell>교육기간</Table.HeaderCell>
-                  <Table.HeaderCell>취소 패널티</Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="차수"
+                      id="lecture-차수세부내용모달-차수"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="강사"
+                      id="lecture-차수세부내용모달-강사"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="담당자 정보 및 이메일"
+                      id="lecture-차수세부내용모달-담당자정보"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="교육장소/웹사이트"
+                      id="lecture-차수세부내용모달-교육장소"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="정원정보"
+                      id="lecture-차수세부내용모달-정원정보"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="수강신청 기간"
+                      id="lecture-차수세부내용모달-신청기간"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="교육기간"
+                      id="lecture-차수세부내용모달-교육기간"
+                    />
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <PolyglotText
+                      defaultString="취소 패널티"
+                      id="lecture-차수세부내용모달-패널티"
+                    />
+                  </Table.HeaderCell>
                 </Table.Row>
               </Table.Header>
 
@@ -146,7 +195,12 @@ class TableModal extends Component<Props, States> {
                           trigger={
                             <Button icon className="img-icon custom">
                               <Icon className="noti32" />
-                              <span className="blind">취소 패널티</span>
+                              <span className="blind">
+                                <PolyglotText
+                                  defaultString="취소 패널티"
+                                  id="lecture-차수세부내용모달-패널티"
+                                />
+                              </span>
                             </Button>
                           }
                         />
