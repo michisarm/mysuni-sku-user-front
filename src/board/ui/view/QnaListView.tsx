ade8ad34f13e296d87b7eef45076e70057211876
김종현 <jhkim@nextree.io>
2021-09-29 15:52:07 +0900

    update: qna href history back # option 제거

--- src/board/ui/view/QnaListView.tsx
+++ src/board/ui/view/QnaListView.tsx
@@ -57,7 +57,7 @@ class QnaListView extends ReactComponent<Props, {}> {
                   <Table.Cell className="ctg">{`${getCategoryName(question.mainCategoryId)} > ${getCategoryName(question.subCategoryId)}`}</Table.Cell>
                   <Table.Cell className="title">
                     <div className="tit inner">
-                      <a href="#" onClick={() => onClickPost(question.id)}>
+                      <a href="" onClick={() => onClickPost(question.id)}>
                         <span className="ellipsis">{question.title}</span>
                       </a>
                     </div>
