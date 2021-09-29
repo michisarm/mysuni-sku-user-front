de3dc8b932596d207f872863e3d40baa472f013b
박종유 <jypark@nextree.io>
2021-09-29 19:12:13 +0900

    Update: Support UI/UX, Style

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,9 @@ import { initializeBody } from './shared/helper/bodyHelper';
 import './style/app.css';
 import 'react-quill/dist/quill.snow.css';
 
-// 2021-08-31-20:15
-import './style/css/2.f15022a9.chunk.css';
-import './style/css/main.04eced77.chunk.css';
+// 2021-09-29 19:11
+import './style/css/2.1ba1de7b.chunk.css';
+import './style/css/main.341c44d3.chunk.css';
 import { initializeI18nResource } from 'shared/viewmodel/PolyglotText';
 
 initializeBody();
