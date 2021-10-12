eabb83ee698b55b908f61f1061587f0c8051e10f
sanggeun <tkdrms753@gmail.com>
2021-10-12 16:33:25 +0900

    footer familysite 탭 수정

--- src/layout/UserApp/Footer/ui/logic/FooterContainer.tsx
+++ src/layout/UserApp/Footer/ui/logic/FooterContainer.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useRef, useEffect } from 'react';
 import { reactAutobind } from '@nara.platform/accent';
 import { Icon, Button, Select } from 'semantic-ui-react';
 import {
@@ -8,213 +8,265 @@ import {
 import PrivacyPolicyModalContainer from '../logic/PrivacyPolicyModalContainer';
 import SiteMapModalContainer from 'layout/UserApp/QuickNav/ui/logic/SiteMapModalContainerV2';
 
-const gotoUrl = (url: string) => {
-  window.open(url);
-};
+import './FooterContainer.css';
 
-@reactAutobind
-class FooterContainer extends Component {
-  //
+function FooterContainer() {
+  const [opened, setOpend] = useState(false);
+  const familyRef = useRef<HTMLInputElement>(null);
 
-  render() {
-    //
-    const selectOptionsSite = [
-      {
-        key: 0,
-        value: 'https://www.icheonforum.com/',
-        text: getPolyglotText('이천포럼', 'home-family-이천포럼'),
-      },
-      {
-        key: 1,
-        value: 'https://on.mysuni.com',
-        text: getPolyglotText('mySUNI ON', 'home-family-mySUNION'),
-      },
-      {
-        key: 2,
-        value: 'http://connect.mysuni.com',
-        text: getPolyglotText('mySUNI Connect', 'home-family-Connect'),
-      },
-      {
-        key: 3,
-        value: 'https://www.linkedin.com/learning/',
-        text: getPolyglotText('LinkedIn Learning', 'home-family-Linkedin'),
-      },
-      {
-        key: 4,
-        value: 'https://www.coursera.org/',
-        text: getPolyglotText('Coursera', 'home-family-Coursera'),
-      },
-    ];
-    return (
-      <section className="footer footer2">
-        <div className="cont-inner">
-          <div className="footer-inner foot-nav-wrap">
-            <div className="foot-nav">
-              <dl className="foot-nav-item">
-                <dt className="foot-nav-li tit">
-                  <a className="item" href="/suni-main/introduction/MySuni">
-                    <span>
-                      <PolyglotText
-                        defaultString="About US"
-                        id="home-ftr-AboutUS"
-                      />
-                    </span>
-                  </a>
-                </dt>
-                <dd className="foot-nav-li">
-                  <a className="item" href="/suni-main/introduction/MySuni">
-                    <span>
-                      <PolyglotText
-                        defaultString="mySUNI"
-                        id="home-ftr-mysuni"
-                      />
-                    </span>
-                  </a>
-                </dd>
-                <dd className="foot-nav-li">
-                  <a className="item" href="/suni-main/introduction/College">
-                    <span>
-                      <PolyglotText
-                        defaultString="College"
-                        id="home-ftr-college"
-                      />
-                    </span>
-                  </a>
-                </dd>
-                <dd className="foot-nav-li">
-                  <a
-                    className="item"
-                    href="/suni-main/introduction/Certification"
-                  >
-                    <span>
-                      <PolyglotText
-                        defaultString="인증제도"
-                        id="home-ftr-인증제도"
-                      />
-                    </span>
-                  </a>
-                </dd>
-                <dd className="foot-nav-li">
-                  <a
-                    className="item"
-                    href="/suni-main/introduction/PromotionTab"
-                  >
-                    <span>
-                      <PolyglotText
-                        defaultString="홍보자료"
-                        id="home-ftr-홍보자료"
-                      />
-                    </span>
-                  </a>
-                </dd>
-              </dl>
-              <dl className="foot-nav-item">
-                <dt className="foot-nav-li tit">
-                  <a className="item" href="/suni-main/board/support/Notice">
-                    <span>
-                      <PolyglotText
-                        defaultString="Help Center"
-                        id="home-ftr-Help"
-                      />
-                    </span>
-                  </a>
-                </dt>
-                <dd className="foot-nav-li">
-                  <a className="item" href="/suni-main/board/support/Notice">
-                    <span>
-                      <PolyglotText
-                        defaultString="Notice"
-                        id="home-ftr-Notice"
-                      />
-                    </span>
-                  </a>
-                </dd>
-                <dd className="foot-nav-li">
-                  <a className="item" href="/suni-main/board/support/FAQ">
-                    <span>
-                      <PolyglotText defaultString="FAQ" id="home-ftr-FAQ" />
-                    </span>
-                  </a>
-                </dd>
-                <dd className="foot-nav-li">
-                  <a className="item" href="/suni-main/board/support/Q&A">
-                    <span>
-                      <PolyglotText
-                        defaultString="1:1 문의"
-                        id="home-ftr-일대일문의"
-                      />
-                    </span>
-                  </a>
-                </dd>
-              </dl>
-              <SiteMapModalContainer
-                trigger={
-                  <dl className="foot-nav-item arrow">
-                    <dt className="foot-nav-li tit">
-                      <a>
-                        <span>
-                          <PolyglotText
-                            defaultString="Sitemap"
-                            id="home-ftr-sitemap"
-                          />
-                        </span>
-                        <span className="item-icon">
-                          <Icon className="angle right" />
-                        </span>
+  function familyToggle() {
+    setOpend(!opened);
+  }
+
+  const handleClickOutside = (event: any) => {
+    if (opened && !familyRef.current?.contains(event.target)) {
+      setOpend(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [familyRef, opened]);
+
+  return (
+    <section className="footer footer2">
+      <div className="cont-inner">
+        <div className="footer-inner foot-nav-wrap">
+          <div className="foot-nav">
+            <dl className="foot-nav-item">
+              <dt className="foot-nav-li tit">
+                <a className="item" href="/suni-main/introduction/MySuni">
+                  <span>
+                    <PolyglotText
+                      defaultString="About US"
+                      id="home-ftr-AboutUS"
+                    />
+                  </span>
+                </a>
+              </dt>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/introduction/MySuni">
+                  <span>
+                    <PolyglotText defaultString="mySUNI" id="home-ftr-mysuni" />
+                  </span>
+                </a>
+              </dd>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/introduction/College">
+                  <span>
+                    <PolyglotText
+                      defaultString="College"
+                      id="home-ftr-college"
+                    />
+                  </span>
+                </a>
+              </dd>
+              <dd className="foot-nav-li">
+                <a
+                  className="item"
+                  href="/suni-main/introduction/Certification"
+                >
+                  <span>
+                    <PolyglotText
+                      defaultString="인증제도"
+                      id="home-ftr-인증제도"
+                    />
+                  </span>
+                </a>
+              </dd>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/introduction/PromotionTab">
+                  <span>
+                    <PolyglotText
+                      defaultString="홍보자료"
+                      id="home-ftr-홍보자료"
+                    />
+                  </span>
+                </a>
+              </dd>
+            </dl>
+            <dl className="foot-nav-item">
+              <dt className="foot-nav-li tit">
+                <a className="item" href="/suni-main/board/support/Notice">
+                  <span>
+                    <PolyglotText
+                      defaultString="Help Center"
+                      id="home-ftr-Help"
+                    />
+                  </span>
+                </a>
+              </dt>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/board/support/Notice">
+                  <span>
+                    <PolyglotText defaultString="Notice" id="home-ftr-Notice" />
+                  </span>
+                </a>
+              </dd>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/board/support/FAQ">
+                  <span>
+                    <PolyglotText defaultString="FAQ" id="home-ftr-FAQ" />
+                  </span>
+                </a>
+              </dd>
+              <dd className="foot-nav-li">
+                <a className="item" href="/suni-main/board/support/Q&A">
+                  <span>
+                    <PolyglotText
+                      defaultString="1:1 문의"
+                      id="home-ftr-일대일문의"
+                    />
+                  </span>
+                </a>
+              </dd>
+            </dl>
+            <SiteMapModalContainer
+              trigger={
+                <dl className="foot-nav-item arrow">
+                  <dt className="foot-nav-li tit">
+                    <a>
+                      <span>
+                        <PolyglotText
+                          defaultString="Sitemap"
+                          id="home-ftr-sitemap"
+                        />
+                      </span>
+                      <span className="item-icon">
+                        <Icon className="angle right" />
+                      </span>
+                    </a>
+                  </dt>
+                </dl>
+              }
+            />
+            <PrivacyPolicyModalContainer
+              trigger={
+                <dl className="foot-nav-item arrow">
+                  <dt className="foot-nav-li tit">
+                    <a className="on">
+                      <span>
+                        <PolyglotText
+                          defaultString="개인정보처리방침"
+                          id="home-ftr-개인정보"
+                        />
+                      </span>
+                      <span className="item-icon">
+                        <Icon className="angle right" />
+                      </span>
+                    </a>
+                  </dt>
+                </dl>
+              }
+            />
+          </div>
+          <div className="foot-site" ref={familyRef}>
+            <div className="f-site-box">
+              <div className="f-site-btnbox" onClick={familyToggle}>
+                <span>Family Site</span>
+
+                {opened === true ? (
+                  <Icon
+                    name="chevron down"
+                    style={{ transform: 'rotate(180deg)' }}
+                  />
+                ) : (
+                  <Icon name="chevron down" />
+                )}
+              </div>
+              {opened && (
+                <div
+                  className="f-site-menu"
+                  style={{ display: 'block' }}
+                  onClick={familyToggle}
+                >
+                  <ul>
+                    <li className="link-options">
+                      <a
+                        href="https://www.icheonforum.com/view/fo/main/after"
+                        target="_blank"
+                      >
+                        <PolyglotText
+                          defaultString="이천포럼"
+                          id="home-family-이천포럼"
+                        />
                       </a>
-                    </dt>
-                  </dl>
-                }
-              />
-              <PrivacyPolicyModalContainer
-                trigger={
-                  <dl className="foot-nav-item arrow">
-                    <dt className="foot-nav-li tit">
-                      <a className="on">
-                        <span>
-                          <PolyglotText
-                            defaultString="개인정보처리방침"
-                            id="home-ftr-개인정보"
-                          />
-                        </span>
-                        <span className="item-icon">
-                          <Icon className="angle right" />
-                        </span>
+                    </li>
+                    <li className="link-options">
+                      <a
+                        href="https://on.mysuni.com/auth/login"
+                        target="_blank"
+                      >
+                        <PolyglotText
+                          defaultString="mySUNI ON"
+                          id="home-family-mySUNION"
+                        />
                       </a>
-                    </dt>
-                  </dl>
-                }
-              />
+                    </li>
+                    <li className="link-options">
+                      <a
+                        href="http://connect.mysuni.com/Mobile/index.asp"
+                        target="_blank"
+                      >
+                        <PolyglotText
+                          defaultString="mySUNI Connect"
+                          id="home-family-Connect"
+                        />
+                      </a>
+                    </li>
+                    <li className="link-options">
+                      <a
+                        href="https://www.linkedin.com/checkpoint/enterprise/login/81530810?application=learning&redirect=https://www.linkedin.com/learning"
+                        target="_blank"
+                      >
+                        <PolyglotText
+                          defaultString="LinkedIn Learning"
+                          id="home-family-Linkedin"
+                        />
+                      </a>
+                    </li>
+                    <li className="link-options">
+                      <a href="https://www.coursera.org" target="_blank">
+                        <PolyglotText
+                          defaultString="Coursera"
+                          id="home-family-Coursera"
+                        />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              )}
             </div>
-            <div className="foot-site">
-              <Select
+            {/* <Select
                 placeholder="Family Site"
                 className="f-site"
                 options={selectOptionsSite}
                 onChange={(event: any, data: any) => gotoUrl(data.value)}
-              />
-            </div>
+              /> */}
           </div>
         </div>
-        <div className="copy-wrap">
-          <div className="cont-inner">
-            <div className="foot-logo">
-              <h2 id="foot-logo">
-                <Icon className="s-kuniversity" />
-                <span className="blind">SK university</span>
-              </h2>
-            </div>
-            <div className="foot-copyright">
-              COPYRIGHT
-              <Icon className="copyright" />
-              <span className="blind">copyright sign</span>
-              mySUNI. ALL RIGHTS RESERVED.
-            </div>
+      </div>
+      <div className="copy-wrap">
+        <div className="cont-inner">
+          <div className="foot-logo">
+            <h2 id="foot-logo">
+              <Icon className="s-kuniversity" />
+              <span className="blind">SK university</span>
+            </h2>
+          </div>
+          <div className="foot-copyright">
+            COPYRIGHT
+            <Icon className="copyright" />
+            <span className="blind">copyright sign</span>
+            mySUNI. ALL RIGHTS RESERVED.
           </div>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default FooterContainer;
