a2daa5debe0ab62186b42a8e289c7e4d765b8dfc
김민준 <mjkim@nextree.io>
2021-10-12 12:30:57 +0900

    edit popup 문구 수정

--- src/lecture/detail/service/useLectureStructure/useRequestLectureStructure.ts
+++ src/lecture/detail/service/useLectureStructure/useRequestLectureStructure.ts
@@ -13,9 +13,12 @@ export function useRequestLectureStructure() {
     if (cardId !== undefined) {
       requestCardLectureStructure(cardId).catch(() => {
         reactAlert({
-          title: getPolyglotText('권한 없음', 'learning-권한-권한없음'),
+          title: getPolyglotText(
+            '본 컨텐츠에 접근할 수 없습니다.',
+            'learning-권한-권한없음'
+          ),
           message: getPolyglotText(
-            '본 컨텐츠에 접근할 수 없습니다.<br/>보다 자세한 사항은 1:1 문의나 전화 상담(02-6323-9002)을 이용해 주세요.',
+            '보다 자세한 사항은 1:1 문의나 전화 상담(02-6323-9002)을 이용해 주세요.',
             'learning-권한-접근불가'
           ),
           onClose: () => {
