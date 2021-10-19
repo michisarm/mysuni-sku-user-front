d665d0605debc8e6f8c53a9884b91309d97d452f
박은호 <jz@yaman.io>
2021-10-19 17:12:02 +0900

    pisAgreemet API 호출 오류 수정

--- src/profile/present/apiclient/instructorApi.ts
+++ src/profile/present/apiclient/instructorApi.ts
@@ -7,12 +7,12 @@ const BASE_URL = '/api/user';
 
 export function registerPisAgreement(pisAgreementSdo: PisAgreementSdo) {
   const axios = getAxios();
-  const url = `${BASE_URL}/pisAgreement`;
+  const url = `${BASE_URL}/users/pisAgreement`;
   return axios.post(url, pisAgreementSdo).then(AxiosReturn);
 }
 
 export function findMyPisAgreement(agreementFormId: string, serviceId: string) {
   const axios = getAxios();
-  const url = `${BASE_URL}/pisAgreement/my?agreementFormId=${agreementFormId}&serviceId=${serviceId}`;
+  const url = `${BASE_URL}/users/pisAgreement/my?agreementFormId=${agreementFormId}&serviceId=${serviceId}`;
   return axios.get<CpPisAgreementModel>(url).then(AxiosReturn);
 }
