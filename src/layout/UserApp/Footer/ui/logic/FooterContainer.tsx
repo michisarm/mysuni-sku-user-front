7a5a29be4e901c05b53731dc318932021d822b9f
박은호 <jz@yaman.io>
2021-10-25 09:26:17 +0900

    FAQ 전체 다국어, 영어/중국어 사용자 Notice 메뉴 숨김

--- src/layout/UserApp/Footer/ui/logic/FooterContainer.tsx
+++ src/layout/UserApp/Footer/ui/logic/FooterContainer.tsx
@@ -9,6 +9,7 @@ import PrivacyPolicyModalContainer from '../logic/PrivacyPolicyModalContainer';
 import SiteMapModalContainer from 'layout/UserApp/QuickNav/ui/logic/SiteMapModalContainerV2';
 
 import './FooterContainer.css';
+import { SkProfileService } from '../../../../../profile/stores';
 
 function FooterContainer() {
   const [opened, setOpend] = useState(false);
@@ -99,13 +100,18 @@ function FooterContainer() {
                   </span>
                 </a>
               </dt>
-              <dd className="foot-nav-li">
-                <a className="item" href="/suni-main/board/support/Notice">
-                  <span>
-                    <PolyglotText defaultString="Notice" id="home-ftr-Notice" />
-                  </span>
-                </a>
-              </dd>
+              {SkProfileService.instance.skProfile.language === 'Korean' && (
+                <dd className="foot-nav-li">
+                  <a className="item" href="/suni-main/board/support/Notice">
+                    <span>
+                      <PolyglotText
+                        defaultString="Notice"
+                        id="home-ftr-Notice"
+                      />
+                    </span>
+                  </a>
+                </dd>
+              )}
               <dd className="foot-nav-li">
                 <a className="item" href="/suni-main/board/support/FAQ">
                   <span>
