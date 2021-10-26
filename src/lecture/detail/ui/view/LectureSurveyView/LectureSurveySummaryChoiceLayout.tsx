55aa0f0642e0961bf53bf79baf3bc08a9fcc817e
곽은옥 <keunok@ucom.kr>
2021-10-26 16:50:22 +0900

    Survey 다국어 적용(결과 팝업)

--- src/lecture/detail/ui/view/LectureSurveyView/LectureSurveySummaryChoiceLayout.tsx
+++ src/lecture/detail/ui/view/LectureSurveyView/LectureSurveySummaryChoiceLayout.tsx
@@ -1,40 +1,49 @@
 import React from 'react';
 import { LectureSurveyItem } from '../../../viewModel/LectureSurvey';
 import { Image } from 'semantic-ui-react';
+import { SkProfileService } from 'profile/stores';
 
-const LectureSurveySummaryChoiceLayout: React.FC<LectureSurveyItem> = function LectureSurveySummaryChoiceLayout({
-  no,
-  title,
-  image,
-  children,
-  isRequired,
-}) {
-  return (
-    <div className="course-radio-survey-new">
-      <p>
-        <span>{no}.</span>
-        {isRequired === true && (
-          <>
-            <span>
-              {title}
-              <span className="importantBtn">
-                <Image
-                  style={{ display: 'inline-block' }}
-                  src={`${process.env.PUBLIC_URL}/images/all/survey-important.png`}
-                />
+const LectureSurveySummaryChoiceLayout: React.FC<LectureSurveyItem> =
+  function LectureSurveySummaryChoiceLayout({
+    no,
+    title,
+    image,
+    children,
+    isRequired,
+  }) {
+    let iconRequired = `${process.env.PUBLIC_URL}/images/all/survey-important.png`;
+    if (SkProfileService.instance.skProfile.language === 'English') {
+      iconRequired = `${process.env.PUBLIC_URL}/images/all/survey-important-2.png`;
+    }
+    if (SkProfileService.instance.skProfile.language === 'Chinese') {
+      iconRequired = `${process.env.PUBLIC_URL}/images/all/survey-important-3.png`;
+    }
+    return (
+      <div className="course-radio-survey-new">
+        <p>
+          <span>{no}.</span>
+          {isRequired === true && (
+            <>
+              <span>
+                {title}
+                <span className="importantBtn">
+                  <Image
+                    style={{ display: 'inline-block' }}
+                    src={iconRequired}
+                  />
+                </span>
               </span>
-            </span>
-          </>
-        )}
-        {isRequired === false && (
-          <>
-            <span>{title}</span>
-          </>
-        )}
-      </p>
-      {children}
-    </div>
-  );
-};
+            </>
+          )}
+          {isRequired === false && (
+            <>
+              <span>{title}</span>
+            </>
+          )}
+        </p>
+        {children}
+      </div>
+    );
+  };
 
 export default LectureSurveySummaryChoiceLayout;
