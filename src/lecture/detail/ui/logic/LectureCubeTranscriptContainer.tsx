c39173084c5d51b7e73eef5dcd099c3be3b64deb
김민준 <mjkim@nextree.io>
2021-11-05 14:17:51 +0900

    transcript 알림 추가

--- src/lecture/detail/ui/logic/LectureCubeTranscriptContainer.tsx
+++ src/lecture/detail/ui/logic/LectureCubeTranscriptContainer.tsx
@@ -11,6 +11,8 @@ import LectureCubeSummary from 'lecture/detail/viewModel/LectureOverview/Lecture
 import { PlayerState, seekTo } from '@sku/skuniv-ui-video-player';
 import { usePanoptoEmbedPlayerState } from '@sku/skuniv-ui-video-player';
 import { getPolyglotText, PolyglotText } from 'shared/ui/logic/PolyglotText';
+import { TranScriptAlertModalView } from '../view/TranScriptAlertModalView';
+import { reactAlert } from '@nara.platform/accent';
 
 const style = {
   borderRadius: '0.375rem',
@@ -50,6 +52,8 @@ const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> =
     const [autoHighlight, setAutoHighlight] = useState<boolean>();
     const intervalTranscript: any = useRef<any>(null);
     const [transciptHighlight, setTransciptHighlight] = useState<string>();
+
+    const [openAlertModal, setOpenAlertModal] = useState<boolean>(false);
 
     const panoptoEmbedPlayerState = usePanoptoEmbedPlayerState();
 
@@ -177,6 +181,35 @@ const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> =
       }
     };
 
+    const onClickDownloadTranScript = () => {
+      reactAlert({
+        title: getPolyglotText(
+          `본 Transcript 파일은 학습의 목적으로 사내 한하여 활용 부탁드리며, <strong>외부 공유를 제한</strong>합니다.`,
+          'cube-transcript-안내'
+        ),
+        message: getPolyglotText(
+          `본 Transcript 파일은 학습의 목적으로 사내 한하여 활용 부탁드리며,<br/><strong>외부 공유를 제한</strong>합니다.`,
+          'cube-transcript-내용'
+        ),
+        onClose: onClickOkInModal,
+      });
+    };
+
+    const onClickOkInModal = () => {
+      //
+      if (transcriptList?.length > 0) {
+        const langText =
+          selectTransLangObj.find((lang) => lang.value === transLangVal)
+            ?.text || '';
+        downloadTranscript(
+          transcriptList,
+          (lectureSummary?.name || 'transcript').concat('_', langText)
+        );
+      }
+
+      setOpenAlertModal(false);
+    };
+
     return (
       <>
         <div className="transcript-box" id="tanscript-scroll">
@@ -196,22 +229,7 @@ const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> =
             <div>
               <button
                 className="ui icon button left post delete-kr"
-                onClick={() => {
-                  if (transcriptList?.length > 0) {
-                    const langText =
-                      selectTransLangObj.find(
-                        (lang) => lang.value === transLangVal
-                      )?.text || '';
-                    console.log('lectureSummary', lectureSummary);
-                    downloadTranscript(
-                      transcriptList,
-                      (lectureSummary?.name || 'transcript').concat(
-                        '_',
-                        langText
-                      )
-                    );
-                  }
-                }}
+                onClick={() => onClickDownloadTranScript()}
               >
                 <Icon className="icon-down-type5" />
                 <PolyglotText
@@ -266,6 +284,10 @@ const LectureTranscriptContainer: React.FC<LectureTranscriptContainerProps> =
             </span>
           )}
         </div>
+        <TranScriptAlertModalView
+          open={openAlertModal}
+          onClickOk={onClickOkInModal}
+        />
       </>
     );
   };
