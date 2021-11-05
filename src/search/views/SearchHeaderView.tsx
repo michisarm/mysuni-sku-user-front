fa70e8650822749aa6e3f596a324692af60eb143
김종현 <jhkim@nextree.io>
2021-11-05 13:59:41 +0900

    update: SearchHeader UI/UX 변경사항 적용 (style)

--- src/search/views/SearchHeaderView.tsx
+++ src/search/views/SearchHeaderView.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Checkbox, Icon, Input, Menu, Popup } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 import { useParams } from 'react-router-dom';
-import { SearchHeaderFieldViewOld } from './SearchHeaderFieldViewOld';
-import { SearchParam } from '../../search/search.models';
-import { getQueryId, search } from '../../search/search.events';
-import classNames from 'classnames';
+import { SearchParam } from '../search.models';
+import { getQueryId } from '../search.events';
 import { getCurrentHistory } from '../../shared/store/HistoryStore';
 import {
   getMenuAuth,
@@ -12,24 +10,18 @@ import {
   setSearchInSearchInfo,
   useSearchInSearchInfo,
   useSearchRelatedList,
-} from '../../search/search.services';
-import {
-  getPolyglotText,
-  PolyglotText,
-} from '../../shared/ui/logic/PolyglotText';
+} from '../search.services';
+import { PolyglotText } from '../../shared/ui/logic/PolyglotText';
 import findAvailablePageElements from 'lecture/shared/api/arrangeApi';
 import { Area } from 'tracker/model';
-import { SearchHeaderFieldView } from './SearchHeaderFieldView';
 
 export function SearchHeaderView() {
   const [activeItem, setActiveItem] = useState<string>('');
-  const [focus, setFocus] = useState<boolean>(false);
   const [write, setWrite] = useState<string>('');
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const queryId = getQueryId();
   const searchInSearchInfo = useSearchInSearchInfo();
-  const relatedList = useSearchRelatedList();
 
   const handleMenuClick = (e: any, { name }: any) => {
     //search(queryId, name === 'all' ? undefined : name);
@@ -89,101 +81,7 @@ export function SearchHeaderView() {
 
   return (
     <>
-      <div className="top_search_area" data-area={Area.SEARCH}>
-        {/*<div className="search_area">*/}
-        {/*  <div className="search_inner">*/}
-        {/*    /!* 검색어 입력필드 *!/*/}
-        {/*    <div className="field">*/}
-        {/*      <div className="search_input">*/}
-        {/*        <Popup*/}
-        {/*          postion="left bottom"*/}
-        {/*          className="history_popup navi_popup"*/}
-        {/*          positionfixed="true"*/}
-        {/*          open={isOpen}*/}
-        {/*          trigger={*/}
-        {/*            <div*/}
-        {/*              className={classNames('search show_text', {*/}
-        {/*                focus: 'focus',*/}
-        {/*                write: 'write',*/}
-        {/*                on: isOpen === true, //input이 popup에 맞춰서 모양이 변경됨*/}
-        {/*                research:*/}
-        {/*                  searchInSearchInfo?.checkSearchInSearch === true, //결과내 재검색 체크박스 클릭시, reseach 클래스 추가 .re_text display제어*/}
-        {/*              })}*/}
-        {/*            >*/}
-        {/*              <div className="re_text">*/}
-        {/*                <span className="ellipsis">*/}
-        {/*                  {searchInSearchInfo?.parentSearchValue}*/}
-        {/*                </span>*/}
-        {/*              </div>*/}
-        {/*              <Input*/}
-        {/*                type="text"*/}
-        {/*                placeholder={getPolyglotText(*/}
-        {/*                  '검색어를 입력하세요.',*/}
-        {/*                  'cmm-cfl-검색어'*/}
-        {/*                )}*/}
-        {/*                value={write}*/}
-        {/*                onClick={() => {*/}
-        {/*                  setFocus(true);*/}
-        {/*                  togglePopup();*/}
-        {/*                }}*/}
-        {/*                onChange={(e) => {*/}
-        {/*                  setWrite(e.target.value);*/}
-        {/*                  setSearchInSearchInfo({*/}
-        {/*                    checkSearchInSearch:*/}
-        {/*                      searchInSearchInfo?.checkSearchInSearch || false,*/}
-        {/*                    parentSearchValue: queryId,*/}
-        {/*                    searchValue: e.target.value,*/}
-        {/*                  });*/}
-        {/*                }}*/}
-        {/*                onKeyDown={(e: any) => {*/}
-        {/*                  if (e.key === 'Enter') {*/}
-        {/*                    handleClose();*/}
-        {/*                    search(write);*/}
-        {/*                  }*/}
-        {/*                }}*/}
-        {/*              />*/}
-        {/*              <Icon*/}
-        {/*                className="clear link"*/}
-        {/*                onClick={() => setWrite('')}*/}
-        {/*              />*/}
-        {/*              /!* <Icon className="search_i"/> *!/*/}
-        {/*              <Button*/}
-        {/*                className="btn_sch"*/}
-        {/*                onClick={() => {*/}
-        {/*                  handleClose();*/}
-        {/*                  search(write);*/}
-        {/*                }}*/}
-        {/*              >*/}
-        {/*                <Icon className="search_i" />*/}
-        {/*              </Button>*/}
-        {/*            </div>*/}
-        {/*          }*/}
-        {/*        >*/}
-        {/*          <Popup.Content data-area={Area.SEARCH}>*/}
-        {/*            /!*<SearchHeaderFieldViewOld callback={searchSetting} />*!/*/}
-        {/*            <SearchHeaderFieldView callback={searchSetting} />*/}
-        {/*          </Popup.Content>*/}
-        {/*        </Popup>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*    <Checkbox*/}
-        {/*      className="again_chk"*/}
-        {/*      label={getPolyglotText('결과 내 재검색', '통검-필레팝-재검색')}*/}
-        {/*      checked={searchInSearchInfo?.checkSearchInSearch}*/}
-        {/*      onClick={() => {*/}
-        {/*        if (!searchInSearchInfo?.checkSearchInSearch) {*/}
-        {/*          setWrite('');*/}
-        {/*        }*/}
-        {/*        setSearchInSearchInfo({*/}
-        {/*          checkSearchInSearch: !searchInSearchInfo?.checkSearchInSearch,*/}
-        {/*          parentSearchValue: queryId,*/}
-        {/*          searchValue: write,*/}
-        {/*        });*/}
-        {/*      }}*/}
-        {/*    />*/}
-        {/*  </div>*/}
-        {/*</div>*/}
-      </div>
+      {/*<div className="top_search_area" data-area={Area.SEARCH}></div>*/}
 
       <div className="tab_search_inner">
         <Menu className="tab_search_title">
