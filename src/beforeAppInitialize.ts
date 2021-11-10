0366bd8b3bfdde3e44cdb5daa7d51faea7ca253b
jz <jz@yaman.io>
2021-11-10 10:36:04 +0900

    before initialize get bookmakrs

--- src/beforeAppInitialize.ts
+++ src/beforeAppInitialize.ts
@@ -1,7 +1,9 @@
 import { requestAvailablePageElements } from './shared/service/requestAvailablePageElements';
+import { reqeustBookmark } from './shared/service/requestBookmarks';
 import { initializeI18nResource } from './shared/viewmodel/PolyglotText';
 
 export async function beforeAppInitialize() {
   await initializeI18nResource();
   await requestAvailablePageElements();
+  await reqeustBookmark();
 }
