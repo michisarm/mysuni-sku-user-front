36dcca4a76289dd231602c40c411bdfe109a04aa
jz <jz@yaman.io>
2021-11-11 12:53:39 +0900

    pisAggrement 호출 로직 수정

--- src/beforeAppInitialize.ts
+++ src/beforeAppInitialize.ts
@@ -1,8 +1,10 @@
+import { SkProfileService } from './profile/stores';
 import { requestAvailablePageElements } from './shared/service/requestAvailablePageElements';
 import { reqeustBookmark } from './shared/service/requestBookmarks';
 import { initializeI18nResource } from './shared/viewmodel/PolyglotText';
 
 export async function beforeAppInitialize() {
+  await SkProfileService.instance.findSkProfile();
   await initializeI18nResource();
   await requestAvailablePageElements();
   await reqeustBookmark();
