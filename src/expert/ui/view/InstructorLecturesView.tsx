0be31c0aabef8af7282b45e879f1b1ab5c908583
sanggeun <tkdrms753@gmail.com>
2021-11-16 20:52:52 +0900

    instructor_Expert 강사페이지 변경된 카드렉쳐뷰로 변경

--- src/expert/ui/view/InstructorLecturesView.tsx
+++ src/expert/ui/view/InstructorLecturesView.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { reactAutobind, mobxHelper } from '@nara.platform/accent';
 import { observer, inject } from 'mobx-react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
@@ -11,6 +11,17 @@ import { CardWithCardRealtedCount } from '../../../lecture/model/CardWithCardRea
 import CardView from '../../../lecture/shared/Lecture/ui/view/CardVIew';
 import { Area } from 'tracker/model';
 import { getPolyglotText } from 'shared/ui/logic/PolyglotText';
+import {
+  LectureCardView,
+  CardProps,
+  parseCommunityLectureCards,
+} from '@sku/skuniv-ui-lecture-card';
+
+import { findCards } from '../../apis/instructorApi';
+import { parseLanguage } from 'shared/viewmodel/PolyglotString';
+import { SkProfileService } from 'profile/stores';
+import { hoverTrack } from 'tracker/present/logic/ActionTrackService';
+import { Segment } from 'semantic-ui-react';
 
 interface RequestMore {
   (): void;
@@ -25,32 +36,51 @@ interface Props {
 export function InstructorLecturesView(props: Props) {
   const { cardsTotalCount, cards, requestMore } = props;
   const needMore = cardsTotalCount > cards.length;
+  const [cardList, setCardList] = useState<CardProps[]>([]);
+  const [cardViewOn, setCardViewOn] = useState<number>(1);
+  const userLanguage = parseLanguage(
+    SkProfileService.instance.skProfile.language
+  );
+  const cardsIds = cards.map((c) => {
+    return c.card.id;
+  });
+
+  useEffect(() => {
+    findCards(cardsIds).then((c) => {
+      if (c === undefined) {
+        return;
+      }
+      setCardList(parseCommunityLectureCards(c, userLanguage));
+    });
+  }, [cards]);
+
   return (
     <div className="expert-cont">
-      {cards.length > 0 ? (
-        <>
-          <Lecture.Group type={Lecture.GroupType.Box}>
-            {cards.map(({ card, cardRelatedCount }) => (
-              <CardView
-                key={card.id}
-                cardId={card.id}
-                {...card}
-                {...cardRelatedCount}
-                dataArea={Area.EXPERT_LECTURE}
-              />
-            ))}
-          </Lecture.Group>
-
-          {needMore && <SeeMoreButton onClick={requestMore} />}
-        </>
-      ) : (
-        <NoSuchContentPanel
-          message={getPolyglotText(
-            '등록된 강의가 없습니다.',
-            '통검-강사소개-강의없음'
-          )}
-        />
-      )}
+      <div className="section">
+        {cardList.length > 0 ? (
+          <>
+            <Lecture.Group type={Lecture.GroupType.Box}>
+              {cardList.map((cards, i) => {
+                return (
+                  <LectureCardView
+                    {...cards}
+                    useBookMark
+                    hoverTrack={hoverTrack}
+                  />
+                );
+              })}
+            </Lecture.Group>
+            {needMore && <SeeMoreButton onClick={requestMore} />}
+          </>
+        ) : (
+          <NoSuchContentPanel
+            message={getPolyglotText(
+              '등록된 강의가 없습니다.',
+              '통검-강사소개-강의없음'
+            )}
+          />
+        )}
+      </div>
     </div>
   );
 }
