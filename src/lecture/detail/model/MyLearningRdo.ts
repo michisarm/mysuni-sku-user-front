bc9b08a5830e4e8b0dd4a4352bc701f7cdf1a7b8
박종유 <jypark@nextree.io>
2022-01-17 17:05:43 +0900

    Update: MyLearningRdo Filed Name Change

--- src/lecture/detail/model/MyLearningRdo.ts
+++ src/lecture/detail/model/MyLearningRdo.ts
@@ -1,13 +1,13 @@
 export interface MyLearningRdo {
   learningGoalHour: number;
-  stampCount: number;
+  obtainedStampCountForYear: number;
   totalStampCount: number;
 }
 
 export function initMyLearningRdo() {
   return {
     learningGoalHour: 0,
-    stampCount: 0,
+    obtainedStampCountForYear: 0,
     totalStampCount: 0,
   };
 }
