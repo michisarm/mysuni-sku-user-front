df47a281dbb01262bc8629d8ed02789b2b2b94a1
Hwang Jongock <jongock.dev@gamil.com>
2022-02-07 15:37:35 +0900

    주석 처리 해제

--- src/playlist/playlistRecommendPopUp/helper/textValidationCheck.ts
+++ src/playlist/playlistRecommendPopUp/helper/textValidationCheck.ts
@@ -5,29 +5,29 @@ import { getPolyglotText } from 'shared/ui/logic/PolyglotText';
 export function textValidationCheck(searchText: string) {
   const trimSearchText = trim(searchText);
 
-  // if (trimSearchText.length === 0) {
-  //   reactAlert({
-  //     title: getPolyglotText('구성원 검색하기', 'playlist-popup-구성원검색'),
-  //     message: getPolyglotText(
-  //       '이름 또는 이메일을 입력해주세요.',
-  //       'playlist-popup-구성원입력'
-  //     ),
-  //   });
+  if (trimSearchText.length === 0) {
+    reactAlert({
+      title: getPolyglotText('구성원 검색하기', 'playlist-popup-구성원검색'),
+      message: getPolyglotText(
+        '이름 또는 이메일을 입력해주세요.',
+        'playlist-popup-구성원입력'
+      ),
+    });
 
-  //   return false;
-  // }
+    return false;
+  }
 
-  // if (trimSearchText.length < 2) {
-  //   reactAlert({
-  //     title: getPolyglotText('구성원 검색하기', 'playlist-popup-구성원검색'),
-  //     message: getPolyglotText(
-  //       '두 글자 이상 검색해주세요.',
-  //       'playlist-popup-두글자검색'
-  //     ),
-  //   });
+  if (trimSearchText.length < 2) {
+    reactAlert({
+      title: getPolyglotText('구성원 검색하기', 'playlist-popup-구성원검색'),
+      message: getPolyglotText(
+        '두 글자 이상 검색해주세요.',
+        'playlist-popup-두글자검색'
+      ),
+    });
 
-  //   return false;
-  // }
+    return false;
+  }
 
   return true;
 }
