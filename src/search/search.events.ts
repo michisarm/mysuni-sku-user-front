e860d3874fd1048d5a6a6431ce411d172e9b453a
김동구 <bbonggu92@gmail.com>
2022-02-14 10:25:22 +0900

    fix: 마스킹처리 로직 수정

--- src/search/search.events.ts
+++ src/search/search.events.ts
@@ -878,17 +878,46 @@ export function getTitleHtmlSearchKeyword(title: string) {
 
 function escapeRegex(item: string, target: string): string {
   const ESCAPE_REGEX = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
-  if (item.match(ESCAPE_REGEX)) {
-    return target.replace(
-      item,
-      `<strong class="search_keyword">${item}</strong>`
-    );
-  } else {
-    const replacedItem = item.replace(ESCAPE_REGEX, '');
-    return target.replace(new RegExp(replacedItem, 'gi'), (match) => {
-      return `<strong class="search_keyword">${match}</strong>`;
-    });
-  }
+  const regExpItem = item.replace(ESCAPE_REGEX, '');
+  let replacedText = '';
+  const splitTags = target.split('</strong>');
+  const changedValues = splitTags.map((splitValue, index) => {
+    const splitTargetValue =
+      splitTags.length !== index + 1 ? splitValue + '</strong>' : splitValue;
+    if (splitTargetValue.includes('</strong>')) {
+      return splitTargetValue;
+    } else {
+      if (item.match(ESCAPE_REGEX)) {
+        return splitTargetValue.replace(new RegExp(item, 'gi'), (match) => {
+          return `<strong class="search_keyword">${match}</strong>`;
+        });
+      } else {
+        console.log('regExpItem : ', regExpItem);
+        return splitTargetValue.replace(
+          new RegExp(regExpItem, 'gi'),
+          (match) => {
+            return `<strong class="search_keyword">${match}</strong>`;
+          }
+        );
+      }
+    }
+  });
+
+  replacedText = changedValues.join('');
+
+  // if (item.match(ESCAPE_REGEX)) {
+  //   replacedText = target.replace(
+  //     item,
+  //     `<strong class="a_text">${item}</strong>`
+  //   );
+  // } else {
+  //   replacedText = target.replace(
+  //     new RegExp(regExpItem, 'gi'),
+  //     `<strong class="a_text">${regExpItem}</strong>`
+  //   );
+  // }
+
+  return replacedText;
 }
 
 export function getTagsHtml(tags: string) {
